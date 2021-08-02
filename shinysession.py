ab3a6950c44805362170917074a56e937e3f80e6
Winston Chang <winston@stdout.org>
2021-08-02 12:47:55 -0500

    Reorder code for type hinting

--- shinysession.py
+++ shinysession.py
@@ -1,8 +1,29 @@
 import react
 from reactives import ReactiveValues, Observer
 
+class ShinySession:
+    def __init__(self, server: callable) -> None:
+        self._message_queue = []
+
+        self.input = ReactiveValues()
+        self.output = Outputs(self)
+        self._server = server
+
+        self._server(self.input, self.output)
+
+    # Pending messages
+    def add_message(self, message):
+        self._message_queue.append(message)
+
+    def get_messages(self):
+        return self._message_queue
+
+    def clear_messages(self):
+        self._message_queue = []
+
+
 class Outputs:
-    def __init__(self, session):
+    def __init__(self, session: ShinySession):
         self._fns = {}
         self._session = session
 
@@ -24,23 +45,3 @@ class Outputs:
     def get(self, name):
         return self._fns[name]
 
-
-class ShinySession:
-    def __init__(self, server: callable) -> None:
-        self._message_queue = []
-
-        self.input = ReactiveValues()
-        self.output = Outputs(self)
-        self._server = server
-
-        self._server(self.input, self.output)
-
-    # Pending messages
-    def add_message(self, message):
-        self._message_queue.append(message)
-
-    def get_messages(self):
-        return self._message_queue
-
-    def clear_messages(self):
-        self._message_queue = []
