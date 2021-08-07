d1a1cd2b21ec7abd561b0a67d26bd5eb8365983b
Winston Chang <winston@stdout.org>
2021-08-06 23:00:37 -0500

    Remove unused function

--- ui.py
+++ ui.py
@@ -1,13 +1,6 @@
 # Temporary placeholders for UI-generating functions. These currently don't do
 # anything.
 
-def render_text(fn):
-    def wrapper():
-        print("render_text")
-        fn()
-        print("done")
-    return wrapper
-
 def fluid_page(*args, **kwargs):
     return args
 
