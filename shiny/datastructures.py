584ffb638544c6aaf1ca1e1c69684a1647fd4f47
Winston Chang <winston@stdout.org>
2021-09-16 18:55:54 -0500

    Add PriorityQueueFIFO

---
+++ shiny/datastructures.py
@@ -0,0 +1,34 @@
+from typing import TypeVar, Generic
+from queue import PriorityQueue
+
+
+T = TypeVar('T')
+
+class PriorityQueueFIFO(Generic[T]):
+    """
+    Similar to queue.PriorityQueue, except that if two elements have the same
+    priority, they are returned in the order they were inserted. Also, the item
+    is kept separate from the priority value (with PriorityQueue, the priority
+    is part of the item).
+    """
+    def __init__(self) -> None:
+        self._pq: PriorityQueue[tuple[int, int, T]] = PriorityQueue()
+        self._counter: int = 0
+
+    def put(self, priority: int, item: T) -> None:
+        """
+        Add an item to the queue.
+
+        Parameters:
+           priority (int): The priority of the item. Higher priority items will
+                           come out of the queue before lower priority items.
+           item (T): The item to put in the queue.
+        """
+        self._counter += 1
+        self._pq.put((-priority, self._counter, item))
+
+    def get(self) -> T:
+        return self._pq.get()[2]
+
+    def empty(self) -> bool:
+        return self._pq.empty()
---
+++ tests/test_datastructures.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""Tests for `shiny.datastructures`."""
+
+from shiny.datastructures import *
+
+
+def test_priority_queue_fifo():
+    q: PriorityQueueFIFO[str] = PriorityQueueFIFO()
+
+    # The random-seeming items are here to ensure that the value of the items
+    # do not affect the order that they go into the queue.
+    q.put(1, '9')
+    q.put(1, '8')
+    q.put(2, '6')
+    q.put(2, '7')
+
+
+    assert q.get() == '6'
+    assert q.get() == '7'
+    assert q.get() == '9'
+    assert q.get() == '8'
