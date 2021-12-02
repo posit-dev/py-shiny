4ea9286c178a13bc526242da5c7807bb3fb33fe8
Joe Cheng <joe@rstudio.com>
2021-12-01 16:15:29 -0800

    Reformat shiny/connmanager.py

--- shiny/connmanager.py
+++ shiny/connmanager.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Optional
-from asgiref.typing import ASGIReceiveCallable, ASGISendCallable, Scope, WebSocketAcceptEvent, WebSocketCloseEvent, WebSocketSendEvent
+from asgiref.typing import (
+    ASGIReceiveCallable,
+    ASGISendCallable,
+    Scope,
+    WebSocketAcceptEvent,
+    WebSocketCloseEvent,
+    WebSocketSendEvent,
+)
+
 
 class Connection(ABC):
     """Abstract class to serve a session and send/receive messages to the
@@ -18,9 +26,12 @@ class Connection(ABC):
     async def close(self, code: int, reason: Optional[str]) -> None:
         ...
 
+
 # Adapts ASGI call (scope, receive, send) to a Connection object
 class ASGIConnection(Connection):
-    def __init__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable):
+    def __init__(
+        self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ):
         self.scope: Scope = scope
         self._rcv: ASGIReceiveCallable = receive
         self._snd: ASGISendCallable = send
@@ -32,16 +43,24 @@ class ASGIConnection(Connection):
         """
         event = await self._rcv()
         if event["type"] == "websocket.connect":
-            await self._snd(WebSocketAcceptEvent(type = "websocket.accept", subprotocol = None, headers = []))
+            await self._snd(
+                WebSocketAcceptEvent(
+                    type="websocket.accept", subprotocol=None, headers=[]
+                )
+            )
         else:
-            await self._snd({
-                "type": "websocket.close",
-                "code": 403,
-                "reason": "Unexpected event received before handshake"
-            })
+            await self._snd(
+                {
+                    "type": "websocket.close",
+                    "code": 403,
+                    "reason": "Unexpected event received before handshake",
+                }
+            )
 
     async def send(self, message: str) -> None:
-        await self._snd(WebSocketSendEvent(type = "websocket.send", bytes = None, text = message))
+        await self._snd(
+            WebSocketSendEvent(type="websocket.send", bytes=None, text=message)
+        )
 
     async def receive(self) -> str:
         event = await self._rcv()
@@ -50,17 +69,31 @@ class ASGIConnection(Connection):
                 return event["text"]
             else:
                 # TODO: Log this?
-                await self._snd(WebSocketCloseEvent(type = "websocket.close", code = 1003, reason = "Can't understand non-text messages at this time"))
+                await self._snd(
+                    WebSocketCloseEvent(
+                        type="websocket.close",
+                        code=1003,
+                        reason="Can't understand non-text messages at this time",
+                    )
+                )
                 raise ConnectionClosed()
         elif event["type"] == "websocket.disconnect":
             raise ConnectionClosed()
         else:
             # TODO: Log the event["type"] we didn't recognize
-            await self._snd(WebSocketCloseEvent(type = "websocket.close", code = 1002, reason = "Unknown event type received"))
+            await self._snd(
+                WebSocketCloseEvent(
+                    type="websocket.close",
+                    code=1002,
+                    reason="Unknown event type received",
+                )
+            )
             raise ConnectionClosed()
 
     async def close(self, code: int, reason: Optional[str]) -> None:
-        await self._snd(WebSocketCloseEvent(type = "websocket.close", code = code, reason = reason))
+        await self._snd(
+            WebSocketCloseEvent(type="websocket.close", code=code, reason=reason)
+        )
 
 
 class ConnectionClosed(Exception):
