fe2e899fc1bfc923809a94dbcd91a3808dac1877
Joe Cheng <joe@rstudio.com>
2021-12-15 10:43:20 -0800

    Stabilize Starlette WebSocket
    Most WebSocket implementations no-op send/close calls on closed websockets.
    Starlette seems to be more fragile, and throws if things are not perfectly
    in the right state. This commit tries to make them more robust; without
    these changes, we can easily get errors at bad times. Furthermore, I
    neglected to translate their WebSocketDisconnect exception to our
    ConnectionClosed exception, which was preventing closed websockets from
    having their sessions removed from the active sessions list.

--- shiny/connmanager.py
+++ shiny/connmanager.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 import starlette.websockets
+from starlette.websockets import WebSocketState
 
 
 class Connection(ABC):
@@ -28,13 +29,31 @@ class StarletteConnection(Connection):
         await self.conn.accept(subprotocol)  # type: ignore
 
     async def send(self, message: str) -> None:
+        if self._is_closed():
+            return
+
         await self.conn.send_text(message)
 
     async def receive(self) -> str:
-        return await self.conn.receive_text()
+        if self._is_closed():
+            raise ConnectionClosed()
+
+        try:
+            return await self.conn.receive_text()
+        except starlette.websockets.WebSocketDisconnect:
+            raise ConnectionClosed()
 
     async def close(self, code: int, reason: Optional[str]) -> None:
+        if self._is_closed():
+            return
+
         await self.conn.close(code)
+
+    def _is_closed(self) -> bool:
+        return (
+            self.conn.application_state == WebSocketState.DISCONNECTED  # type: ignore
+            or self.conn.client_state == WebSocketState.DISCONNECTED  # type: ignore
+        )
 
 
 class ConnectionClosed(Exception):
