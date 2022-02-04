be27e9711f23ee69caa18ff1d17c8a1b8d0edc82
Winston Chang <winston@stdout.org>
2022-02-04 16:50:15 -0600

    Fix InputsProxy and add tests

--- examples/moduleapp/app.py
+++ examples/moduleapp/app.py
@@ -1,20 +1,6 @@
-# To run this app:
-#   python3 app.py
-
-# Then point web browser to:
-#   http://localhost:8000/
-
-# Add parent directory to path, so we can find the prism module.
-# (This is just a temporary fix)
-import os
-import sys
-
-# This will load the shiny module dynamically, without having to install it.
-# This makes the debug/run cycle quicker.
-shiny_module_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-sys.path.insert(0, shiny_module_dir)
-
+import shiny.ui_toolkit as st
 from shiny import *
+
 
 # =============================================================================
 # Counter module
@@ -23,22 +9,24 @@ def counter_module_ui(
     ns: Callable[[str], str], label: str = "Increment counter"
 ) -> TagChildArg:
     return TagList(
-        input_action_button(id=ns("button"), label=label),
-        output_text_verbatim(id=ns("out")),
+        st.input_action_button(id=ns("button"), label=label),
+        st.output_text_verbatim(id=ns("out")),
     )
 
 
-def counter_module_server(session: SessionProxy):
-    count: ReactiveVal[int] = ReactiveVal(0)
+def counter_module_server(
+    input: InputsProxy, output: OutputsProxy, session: SessionProxy
+):
+    count: reactive.Value[int] = reactive.Value(0)
 
-    @observe()
+    @reactive.effect()
+    @event(session.input.button)
     def _():
-        session.input["button"]
-        with isolate():
-            count(count() + 1)
+        count.set(count() + 1)
 
-    @session.output("out")
-    def _() -> str:
+    @output()
+    @render_text()
+    def out() -> str:
         return f"Click count is {count()}"
 
 
@@ -48,19 +36,15 @@ counter_module = ShinyModule(counter_module_ui, counter_module_server)
 # =============================================================================
 # App that uses module
 # =============================================================================
-ui = page_fluid(
+ui = st.page_fluid(
     counter_module.ui("counter1", "Counter 1"),
     counter_module.ui("counter2", "Counter 2"),
 )
 
 
-def server(session: Session):
+def server(input: Inputs, output: Outputs, session: Session):
     counter_module.server("counter1")
     counter_module.server("counter2")
 
 
 app = App(ui, server)
-
-
-if __name__ == "__main__":
-    app.run()
