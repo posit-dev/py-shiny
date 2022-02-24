d32027937efade94cd4903022b06cecba05dc7d1
Carson <cpsievert1@gmail.com>
2022-02-24 16:17:08 -0600

    Close #84: use wildcard import of htmltools to appease VSCode

--- shiny/ui/__init__.py
+++ shiny/ui/__init__.py
@@ -23,27 +23,79 @@ from ._notification import *
 from ._output import *
 from ._page import *
 from ._progress import *
-from htmltools import TagList, Tag, TagChildArg, TagAttrArg, tags, HTML, head_content
 
-# "Re-export" top-level tags
-# Note this should match https://github.com/rstudio/py-htmltools/blob/dcebb4/htmltools/tags.py#L8-L26
-# but we hard-code it here so
-from htmltools import (
-    p,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    a,
-    br,
-    div,
-    span,
-    pre,
-    code,
-    img,
-    strong,
-    em,
-    hr,
+from ._bootstrap import __all__ as _bootstrap_all
+from ._download_button import __all__ as _download_button_all
+from ._input_action_button import __all__ as _input_action_button_all
+from ._input_check_radio import __all__ as _input_check_radio_all
+from ._input_date import __all__ as _input_date_all
+from ._input_file import __all__ as _input_file_all
+from ._input_numeric import __all__ as _input_numeric_all
+from ._input_password import __all__ as _input_password_all
+from ._input_select import __all__ as _input_select_all
+from ._input_slider import __all__ as _input_slider_all
+from ._input_text import __all__ as _input_text_all
+from ._input_update import __all__ as _input_update_all
+from ._insert import __all__ as _insert_all
+from ._modal import __all__ as _modal_all
+from ._navs import __all__ as _navs_all
+from ._notification import __all__ as _notification_all
+from ._output import __all__ as _output_all
+from ._page import __all__ as _page_all
+from ._progress import __all__ as _progress_all
+
+# For reasons I can't explain, if we explicitly import each function/class
+# from htmltools (e.g., from htmltools import div, span, etc.) then
+# VSCode will flag them as private???
+from htmltools import *
+
+_htmltools_all = (
+    "TagList",
+    "Tag",
+    "TagChildArg",
+    "TagAttrArg",
+    "tags",
+    "HTML",
+    "head_content",
+    "p",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "a",
+    "br",
+    "div",
+    "span",
+    "pre",
+    "code",
+    "img",
+    "strong",
+    "em",
+    "hr",
+)
+
+
+__all__ = (
+    _bootstrap_all
+    + _download_button_all
+    + _input_action_button_all
+    + _input_check_radio_all
+    + _input_date_all
+    + _input_file_all
+    + _input_numeric_all
+    + _input_password_all
+    + _input_select_all
+    + _input_slider_all
+    + _input_text_all
+    + _input_update_all
+    + _insert_all
+    + _modal_all
+    + _navs_all
+    + _notification_all
+    + _output_all
+    + _page_all
+    + _progress_all
+    + _htmltools_all
 )
