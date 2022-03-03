7cd15d60b5eb1c6f70384b5d68eacec7cae34a69
Carson Sievert <cpsievert1@gmail.com>
2022-03-03 14:02:57 -0600

    Make @add_example() a no-op unless examples are requested (#95)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,16 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -57,3 +57,8 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- Shiny specific options --------------------------------------------------
+
+# Tell @shiny._docstring.add_example() to actually add examples to the docstrings.
+os.environ["SHINY_ADD_EXAMPLES"] = "true"
