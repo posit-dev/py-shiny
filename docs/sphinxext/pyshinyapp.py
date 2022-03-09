77a4e8f231e85f743ee42aeb23894bccbda8958c
Carson <cpsievert1@gmail.com>
2022-03-09 16:16:55 -0600

    Add basic support for all prism-experiments components; include HTML dependencies only once per page

--- docs/sphinxext/pyshinyapp.py
+++ docs/sphinxext/pyshinyapp.py
@@ -1,15 +1,27 @@
-"""Sphinx extension for embedding a purely client-side Shiny app via WASM
+"""Sphinx extensions for running python code and/or Shiny apps in the browser via WASM
 Usage::
-   .. pyshinyapp::
+    .. shinyapp::
+        :width: 100%
+        :height: 500px
+
+   .. shinyeditor::
+        :width: 100%
+        :height: 500px
+
+    .. cell::
+        :width: 100%
+        :height: 500px
+
+    .. terminal::
+        :width: 100%
+        :height: 500px
 """
 
 # N.B. place subclasses of Element in here (not conf.py)...because Sphinx!
-# Boy, this was a fun one to track down
 # https://github.com/sphinx-doc/sphinx/pull/6754
 
 import os
 import shutil
-
 
 from docutils.nodes import SkipNode, Element
 from docutils.parsers.rst import directives
@@ -31,50 +43,27 @@ if not os.path.exists(SHINYLIVE_DIR):
     )
 
 
-class PyShinyApp(Element):
-    """
-    Renders contents of the ``.. pyshinyapp::`` directive according to
-    https://rstudio.github.io/prism-experiments/embedded-app.html
-    """
-
+class ShinyElement(Element):
     def html(self):
         style = css(height=self["height"], width=self["width"])
-        # TODO: Include <script> only once per page
+        type = self["type"]
+        # right now pyshiny gives you the editor, but that'll probably change
+        if type == "shinyeditor":
+            type = "shiny"
         return (
-            """
-            <script type="module">
-              const serviceWorkerPath = "/serviceworker.js";
-
-              // Start the service worker as soon as possible, to maximize the
-              // resources it will be able to cache on the first run.
-              if ("serviceWorker" in navigator) {
-                navigator.serviceWorker
-                  .register(serviceWorkerPath)
-                  .then(() => console.log("Service Worker registered"))
-                  .catch(() => console.log("Service Worker registration failed"));
-
-                navigator.serviceWorker.ready.then(() => {
-                  if (!navigator.serviceWorker.controller) {
-                    // For Shift+Reload case; navigator.serviceWorker.controller will
-                    // never appear until a regular (not Shift+Reload) page load.
-                    window.location.reload();
-                  }
-                });
-              }
-            </script>
-            <link rel="stylesheet" href="/shinylive/Components/App.css">
-            <script src="/shinylive/run-python-blocks.js" type="module"></script>
-            """
-            f'<pre class="pyshinyapp" style="{style}">{self["code"]}</pre>'
+            f'<pre class="py{type}" style="{style}"><code>{self["code"]}</code></pre>'
         )
 
 
-class PyShinyDirective(SphinxDirective):
-    """
-    The ``.. pyshinyapp::`` directive which places the contents into a code block
-    via :class:`PyShinyAppElement`.
-    """
+def _run(self: SphinxDirective, type: str):
+    code = "\n".join(self.content)
+    width = self.options.pop("width", "100%")
+    height = self.options.pop("height", "500px")
 
+    return [ShinyElement(type=type, code=code, height=height, width=width)]
+
+
+class BaseDirective(SphinxDirective):
     has_content = True
     required_arguments = 0
     option_spec = {
@@ -82,48 +71,61 @@ class PyShinyDirective(SphinxDirective):
         "height": directives.unchanged,
     }
 
+
+class ShinyAppDirective(BaseDirective):
     def run(self):
-        code = "\n".join(self.content)
-        width = self.options.pop("width", "100%")
-        height = self.options.pop("height", "500px")
-
-        return [PyShinyApp(code=code, height=height, width=width)]
+        return _run(self, "shinyapp")
 
 
-def skip(self, node: Element):
-    raise SkipNode
+class ShinyEditorDirective(BaseDirective):
+    def run(self):
+        return _run(self, "shinyeditor")
 
 
-def append_element_html(self, node: Element):
-    self.body.append(node.html())
-    raise SkipNode
+class CellDirective(BaseDirective):
+    def run(self):
+        return _run(self, "cell")
 
 
-def after_build(app: Sphinx, error):
-    shinylive = os.path.join(app.outdir, "shinylive")
-    if os.path.exists(shinylive):
-        shutil.rmtree(shinylive)
-    shutil.copytree(SHINYLIVE_DIR, shinylive)
-    shutil.copy(
-        os.path.join(SHINYLIVE_DIR, "../serviceworker.js"),
-        os.path.join(app.outdir, "serviceworker.js"),
-    )
+class TerminalDirective(BaseDirective):
+    def run(self):
+        return _run(self, "terminal")
 
 
 def setup(app: Sphinx):
     # After the build is finished, copy over the necessary shinylive static files
     # the usual _static/ directory won't work given the limitations in it's current
     # design
+    def after_build(app: Sphinx, error):
+        shinylive = os.path.join(app.outdir, "shinylive")
+        if os.path.exists(shinylive):
+            shutil.rmtree(shinylive)
+        shutil.copytree(SHINYLIVE_DIR, shinylive)
+        shutil.copy(
+            os.path.join(SHINYLIVE_DIR, "../serviceworker.js"),
+            os.path.join(app.outdir, "serviceworker.js"),
+        )
+
     app.connect("build-finished", after_build)
 
+    def append_element_html(self, node: Element):
+        self.body.append(node.html())
+        raise SkipNode
+
+    def skip(self, node: Element):
+        raise SkipNode
+
     app.add_node(
-        PyShinyApp,
+        ShinyElement,
         html=(append_element_html, None),
         latex=(skip, None),
         textinfo=(skip, None),
         text=(skip, None),
         man=(skip, None),
     )
-    app.add_directive("pyshinyapp", PyShinyDirective)
+    app.add_directive("shinyapp", ShinyAppDirective)
+    app.add_directive("shinyeditor", ShinyEditorDirective)
+    app.add_directive("cell", CellDirective)
+    app.add_directive("terminal", TerminalDirective)
 
     return {"version": "0.1"}
--- shiny/_docstring.py
+++ shiny/_docstring.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable, Any, TypeVar, Literal
+from typing import Callable, Any, TypeVar, Literal, List
 
 ex_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "examples")
 
@@ -7,7 +7,32 @@ FuncType = Callable[..., Any]
 F = TypeVar("F", bound=FuncType)
 
 
-def add_example(directive: Literal["pyshinyapp"] = "pyshinyapp") -> Callable[[F], F]:
+def add_example(
+    directive: Literal[
+        "shinyapp::", "shinyeditor::", "code-block:: python", "cell::", "terminal::"
+    ] = "shinyapp::",
+    **options: str,
+) -> Callable[[F], F]:
+    """
+    Add an example to the docstring of a function, method, or class.
+
+    This decorator must, at the moment, be used on a function, method, or class whose
+    ``__name__`` matches the name of directory under ``shiny/examples/``, and must
+    also contain a ``app.py`` file in that directory.
+
+    Parameters
+    ----------
+    directive
+        A directive for rendering the example. This can be one of:
+            - ``shinyapp``: A live shiny app (statically served via wasm).
+            - ``code``: A python code snippet.
+            - ``shinyeditor``: A live shiny app (statically served via wasm).
+            - ``cell``: A executable Python cell.
+            - ``terminal``: A minimal Python IDE
+    **options
+        Options for the directive. See docs/source/sphinxext/pyshinyapp.py for details.
+    """
+
     def _(func: F) -> F:
 
         # To avoid a performance hit on `import shiny`, we only add examples to the
@@ -30,6 +55,19 @@ def add_example(directive: Literal["pyshinyapp"] = "pyshinyapp") -> Callable[[F]
         with open(example_file) as f:
             example = indent.join([" " * 4 + x for x in f.readlines()])
 
+        # When rendering a standalone app, put the code above it (maybe this should be
+        # handled by the directive itself?)
+        example_prefix: List[str] = []
+        if directive == "shinyapp::":
+            example_prefix.extend(
+                [
+                    ".. code-block:: python",
+                    "",
+                    example,
+                    "",
+                ]
+            )
+
         example_section = ("\n" + indent).join(
             [
                 "",
@@ -37,11 +75,9 @@ def add_example(directive: Literal["pyshinyapp"] = "pyshinyapp") -> Callable[[F]
                 "Example",
                 "-------",
                 "",
-                ".. code-block:: python",
-                "",
-                example,
-                "",
-                f".. {directive}::",
+                *example_prefix,
+                f".. {directive}",
+                *[f"{indent}:{k}: {v}" for k, v in options.items()],
                 "",
                 example,
             ]
