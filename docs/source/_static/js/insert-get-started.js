8e150eb7f20fe7954048cd93b666c94743bf2c87
Winston Chang <winston@stdout.org>
2022-03-16 17:54:20 -0500

    Fix links to get-started page

--- docs/source/_static/js/insert-get-started.js
+++ docs/source/_static/js/insert-get-started.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var a = document.createElement("a");
   a.textContent = "Get Started";
-  a.href = "https://rstudio.github.io/prism-experiments/guide/site/get-started.html";
+  a.href = "https://rstudio.github.io/pyshiny-site/get-started.html";
   a.style = "float:right; color:#5a5a5a; margin-right:5px; margin-top:5px; font-weight:600; text-decoration:none;";
   a.setAttribute("data-toggle", "tooltip");
   a.setAttribute("data-placement", "bottom");
