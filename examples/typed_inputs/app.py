bd46743f6f01653877b595a8ae6e71a516f0d73e
Winston Chang <winston@stdout.org>
2022-04-13 16:48:35 -0500

    Add typed input example

---
+++ examples/typed_inputs/app.py
@@ -0,0 +1,65 @@
+# pyright: reportUnusedFunction=false
+
+from shiny import *
+import typing
+
+app_ui = ui.page_fluid(
+    ui.input_numeric("n", "N", 20),
+    ui.input_numeric("n2", "N2", 50),
+    ui.input_checkbox("checkbox", "Checkbox", True),
+    ui.output_text_verbatim("txt", placeholder=True),
+    ui.output_text_verbatim("txt2", placeholder=True),
+    ui.output_text_verbatim("txt3", placeholder=True),
+)
+
+
+# By default the type of any input value, like input.n(), is Any, so no type checking
+# will be used.
+#
+# But it is possible to specify the type of the input value, by creating a subclass of
+# Inputs. We'll do that for input.n2() and input.checkbox():
+class ShinyInputs(Inputs):
+    n2: reactive.Value[int]
+    check: reactive.Value[bool]
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+
+    # Cast `input` to our ShinyInputs class. This just tells the static type checker
+    # that we want it treated as a ShinyInputs object for type checking; it has no
+    # run-time effect.
+    input = typing.cast(ShinyInputs, input)
+
+    # The type checker knows that r() returns an int, which you can see if you hover
+    # over it.
+    @reactive.Calc()
+    def r():
+        if input.n() is None:
+            return 0
+        return input.n() * 2
+
+    # Because we did NOT add a type for input.n (we did input.n2), the type checker
+    # thinks the return type of input.n() is Any, so we don't get type checking here.
+    # The function is returning the wrong value here: it returns an int instead of a
+    # string, but this error is not flagged.
+    @output()
+    @render_text()
+    async def txt():
+        return input.n() * 2
+
+    # In contrast, input.n2() is declared to return an int, so the type check does flag
+    # this error -- the `render_text()` is underlined in red.
+    @output()
+    @render_text()
+    async def txt2():
+        return input.n2() * 2
+
+    # This is a corrected version of the function above. It returns a string, and is not
+    # marked in red.
+    @output()
+    @render_text()
+    async def txt3():
+        return str(input.n2() * 2)
+
+
+app = App(app_ui, server)
