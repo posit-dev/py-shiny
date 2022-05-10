c0fd328fda1249b5719caf075af85749973f47a0
Carson <cpsievert1@gmail.com>
2022-05-10 17:14:11 -0500

    pull in latest shiny.js in order to simplify example

--- shiny/examples/dynamic_route/app.py
+++ shiny/examples/dynamic_route/app.py
@@ -1,7 +1,4 @@
-import random
-
 from shiny import *
-from htmltools import HTMLDependency
 from starlette.responses import JSONResponse
 from starlette.requests import Request
 
@@ -22,20 +19,14 @@ def server(input: Inputs, output: Outputs, session: Session):
         print("Serving at: ", path)
 
         ui.insert_ui(
-            # TODO: inserting a raw <script> doesn't work here, this might
-            # motivation for head_content(dedup=False)
-            HTMLDependency(
-                name=str(random.randint(0, 1000)),
-                version="1.0",
-                head=ui.tags.script(
-                    f"""
-                    fetch('{path}')
-                      .then(r => r.json())
-                      .then(x => {{ $('#messages').text(`Clicked ${{x.n_clicks}} times`); }});
-                    """
-                ),
+            ui.tags.script(
+                f"""
+                fetch('{path}')
+                  .then(r => r.json())
+                  .then(x => {{ $('#messages').text(`Clicked ${{x.n_clicks}} times`); }});
+                """
             ),
-            selector="head",
+            selector="body",
         )
 
 
