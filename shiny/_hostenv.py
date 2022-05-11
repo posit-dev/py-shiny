f3939d66b131eaac5a7b00ed816615c161f7bc2e
Joe Cheng <joe@rstudio.com>
2022-05-11 13:00:59 -0700

    Fix type errors

--- shiny/_hostenv.py
+++ shiny/_hostenv.py
@@ -6,13 +6,17 @@ from subprocess import run
 from typing import Dict
 from urllib.parse import urlparse
 
+
 def is_workbench() -> bool:
     return bool(os.getenv("RS_SERVER_URL") and os.getenv("RS_SESSION_URL"))
+
 
 def is_proxy_env() -> bool:
     return is_workbench()
 
+
 port_cache: Dict[int, str] = {}
+
 
 def get_proxy_url(url: str) -> str:
     if not is_workbench():
@@ -38,7 +42,15 @@ def get_proxy_url(url: str) -> str:
     server_url = re.sub("/$", "", server_url)
     session_url = re.sub("^/", "", session_url)
 
-    port = parts.port if parts.port else 80 if parts.scheme in ["ws", "http"] else 443 if parts.scheme in ["wss", "https"] else 0
+    port = (
+        parts.port
+        if parts.port
+        else 80
+        if parts.scheme in ["ws", "http"]
+        else 443
+        if parts.scheme in ["wss", "https"]
+        else 0
+    )
     if port == 0:
         return url
 
@@ -46,10 +58,11 @@ def get_proxy_url(url: str) -> str:
         ptoken = port_cache[port]
     else:
         try:
-            res = run(["/usr/lib/rstudio-server/bin/rserver-url",
-                str(port)],
+            res = run(
+                ["/usr/lib/rstudio-server/bin/rserver-url", str(port)],
                 capture_output=True,
-                encoding="ascii")
+                encoding="ascii",
+            )
         except FileNotFoundError:
             return url
         if res.returncode != 0:
@@ -59,7 +72,11 @@ def get_proxy_url(url: str) -> str:
 
     return f"{server_url}/{session_url}p/{ptoken}{path}{'?' if parts.query else ''}{parts.query}"
 
-pat_local_url = re.compile("https?://(127.0.0.1|localhost)(:\\d+)?([-\+=&;%@.\w_]*)", re.IGNORECASE)
+
+pat_local_url = re.compile(
+    "https?://(127.0.0.1|localhost)(:\\d+)?([-\\+=&;%@.\\w_]*)", re.IGNORECASE
+)
+
 
 class ProxyUrlFilter:
     def __init__(self):
@@ -68,12 +85,14 @@ class ProxyUrlFilter:
     def filter(self, record: logging.LogRecord) -> int:
         record.msg = pat_local_url.sub(self.url_replacement, record.getMessage())
         if hasattr(record, "color_message"):
-            color_msg = str(record.color_message)
+            color_msg = str(record.color_message)  # type: ignore
             if record.args:
                 color_msg = color_msg % record.args
-            record.color_message = pat_local_url.sub(self.url_replacement, color_msg)
+            record.color_message = pat_local_url.sub(  # type: ignore
+                self.url_replacement, color_msg
+            )
         record.args = ()
         return 1
 
-    def url_replacement(self, match: re.Match) -> str:
+    def url_replacement(self, match: re.Match[str]) -> str:
         return get_proxy_url(match.group(0))
