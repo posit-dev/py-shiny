2ef6646a690e75d5ca67ac9367fabdf35b185c47
Joe Cheng <joe@rstudio.com>
2022-05-12 10:13:12 -0700

    Prevent run_elsewhere from being called on pyodide

--- shiny/_connection.py
+++ shiny/_connection.py
@@ -57,7 +57,7 @@ class MockConnection(Connection):
         return self._http_conn
 
 
-class StarletteConnection(Connection):
+class ThreadedStarletteConnection(Connection):
     def __init__(self, conn: starlette.websockets.WebSocket):
         self.conn: starlette.websockets.WebSocket = conn
         self.event_loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
@@ -66,28 +66,7 @@ class StarletteConnection(Connection):
         if self._is_closed():
             return
 
-        # In general, we want to be able to call this send() method from synchronous
-        # functions.
-        if is_pyodide:
-            # The create_task() here isn't strictly necessary for things to work in
-            # pyodide. The emulated websocket's .send() method is a proxy object for a
-            # JS function, and when it's invoked, it returns a Future which will execute
-            # even if we don't await it or create a new task, since it's running outside
-            # of Python (similar to if a Future is run in a separate process).
-            #
-            # We can't wrap the Future in run_coro_sync(), because with pyodide's
-            # implementation of async wrapper functions, it does yield, and so
-            # run_coro_sync() will throw an error.
-            #
-            # If we just call conn.send_text() without either awaiting it or creating a
-            # Task, then Python will raise a warning about the Future not being awaited.
-            # So even though the code would work without create_task(), it would raise a
-            # warning. We call create_task() to avoid that warning.
-            asyncio.create_task(self.conn.send_text(message))
-        else:
-            asyncio.run_coroutine_threadsafe(
-                self.conn.send_text(message), self.event_loop
-            )
+        asyncio.run_coroutine_threadsafe(self.conn.send_text(message), self.event_loop)
 
     async def receive(self) -> str:
         if self._is_closed():
@@ -113,6 +92,62 @@ class StarletteConnection(Connection):
 
     def get_http_conn(self) -> HTTPConnection:
         return self.conn
+
+
+class PyodideStarletteConnection(Connection):
+    def __init__(self, conn: starlette.websockets.WebSocket):
+        self.conn: starlette.websockets.WebSocket = conn
+
+    def send(self, message: str) -> None:
+        if self._is_closed():
+            return
+
+        # The create_task() here isn't strictly necessary for things to work in
+        # pyodide. The emulated websocket's .send() method is a proxy object for a
+        # JS function, and when it's invoked, it returns a Future which will execute
+        # even if we don't await it or create a new task, since it's running outside
+        # of Python (similar to if a Future is run in a separate process).
+        #
+        # We can't wrap the Future in run_coro_sync(), because with pyodide's
+        # implementation of async wrapper functions, it does yield, and so
+        # run_coro_sync() will throw an error.
+        #
+        # If we just call conn.send_text() without either awaiting it or creating a
+        # Task, then Python will raise a warning about the Future not being awaited.
+        # So even though the code would work without create_task(), it would raise a
+        # warning. We call create_task() to avoid that warning.
+        asyncio.create_task(self.conn.send_text(message))
+
+    async def receive(self) -> str:
+        if self._is_closed():
+            raise ConnectionClosed()
+
+        try:
+            return await self.conn.receive_text()
+        except starlette.websockets.WebSocketDisconnect:
+            raise ConnectionClosed()
+
+    async def close(self, code: int, reason: Optional[str]) -> None:
+        if self._is_closed():
+            return
+
+        # TODO: Figure out how to await this
+        await self.conn.close(code)
+
+    def _is_closed(self) -> bool:
+        return (
+            self.conn.application_state == WebSocketState.DISCONNECTED  # type: ignore
+            or self.conn.client_state == WebSocketState.DISCONNECTED  # type: ignore
+        )
+
+    def get_http_conn(self) -> HTTPConnection:
+        return self.conn
+
+
+if is_pyodide:
+    StarletteConnection = PyodideStarletteConnection
+else:
+    StarletteConnection = ThreadedStarletteConnection
 
 
 class ConnectionClosed(Exception):
