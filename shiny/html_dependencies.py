3d1bc36ba234c990c2a2d0e0679499c594cf21c9
Carson <cpsievert1@gmail.com>
2022-05-23 14:59:55 -0500

    Revert inclusion of shiny.js changes; improve comment

--- shiny/html_dependencies.py
+++ shiny/html_dependencies.py
@@ -20,11 +20,16 @@ def jquery_deps() -> HTMLDependency:
     )
 
 
-# N.B. py-shiny has requirejs as a 'core' dependency since it's currently a pretty
-# fundamental dependency for any Jupyter widget project (i.e., ipyshiny) and we need a
-# custom setup for it to work decently with HTMLDependency() model (i.e., loading JS via
-# <script> tags). At the moment, we're just setting window.define.amd=false after loading
-# requirejs so that the typical UMD pattern won't result in an anonymous define() error.
+# Shiny doesn't (currently) use requirejs directly, but it does include it because a
+# custom requirejs setup is need to get HTMLDependency()s (i.e., loading JS via <script>
+# tags) to be usable. At the moment, we're just setting `window.define.amd=false` after
+# loading requirejs so that the typical UMD pattern won't result in an anonymous
+# define() error.
+# https://requirejs.org/docs/errors.html#mismatch
+# https://github.com/umdjs/umd
+#
+# Someday, we may want the same/similar thing in R, but this definitely more of an
+# immediate issue for Python since many Jupyter extensions use requirejs.
 def require_deps() -> HTMLDependency:
     return HTMLDependency(
         name="requirejs",
