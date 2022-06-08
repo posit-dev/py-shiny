fff04e91c02befd918881b940974c23169951560
Winston Chang <winston@stdout.org>
2022-06-08 09:05:15 -0500

    Add docstrings for deprecated functions

--- shiny/_deprecated.py
+++ shiny/_deprecated.py
@@ -29,20 +29,24 @@ def _warn_deprecated(message: str):
 # Render functions
 # ======================================================================================
 def render_text():
+    """Deprecated. Please use render.text() instead of render_text()."""
     _warn_deprecated("render_text() is deprecated. Use render.text() instead.")
     return render.text()
 
 
 def render_ui():
+    """Deprecated. Please use render.ui() instead of render_ui()."""
     _warn_deprecated("render_ui() is deprecated. Use render.ui() instead.")
     return render.ui()
 
 
 def render_plot(*args: Any, **kwargs: Any):
+    """Deprecated. Please use render.plot() instead of render_plot()."""
     _warn_deprecated("render_plot() is deprecated. Use render.plot() instead.")
     return render.plot(*args, **kwargs)
 
 
 def render_image(*args: Any, **kwargs: Any):
+    """Deprecated. Please use render.image() instead of render_image()."""
     _warn_deprecated("render_image() is deprecated. Use render.image() instead.")
     return render.image(*args, **kwargs)
