e1ba46142420d6ea06c729fc5ce5f08dea4ac1de
Winston Chang <winston@stdout.org>
2022-07-12 12:34:56 -0500

    Fix progress example for pyodide

--- shiny/examples/Progress/app.py
+++ shiny/examples/Progress/app.py
@@ -1,4 +1,5 @@
 from shiny import *
+import asyncio
 
 app_ui = ui.page_fluid(
     ui.input_action_button("button", "Compute"),
@@ -10,15 +11,15 @@ def server(input: Inputs, output: Outputs, session: Session):
     @output
     @render.text
     @reactive.event(input.button)
-    def compute():
+    async def compute():
         with ui.Progress(min=1, max=15) as p:
             p.set(message="Calculation in progress", detail="This may take a while...")
 
-            import time
-
             for i in range(1, 15):
                 p.set(i, message="Computing")
-                time.sleep(0.1)
+                await asyncio.sleep(0.1)
+                # Normally use time.sleep() instead, but it doesn't yet work in Pyodide.
+                # https://github.com/pyodide/pyodide/issues/2354
 
         return "Done computing!"
 
