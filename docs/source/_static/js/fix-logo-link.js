9efcda8353dccbc84ac29ec5b9aa85794c62fb1e
Winston Chang <winston@stdout.org>
2022-07-24 17:55:00 -0500

    Point logo in API docs to main website

---
+++ docs/source/_static/js/fix-logo-link.js
@@ -0,0 +1,8 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    let a = document.querySelector(".bd-sidebar a.navbar-brand");
+    a.href = "https://shiny.rstudio.com/py/";
+  },
+  false
+);
