35eeb960bd74990ed211373e281f78067128dd84
Joe Cheng <joe@rstudio.com>
2022-08-25 11:19:40 -0700

    Increase timeout on playwright test

--- e2e/test_playwright.py
+++ e2e/test_playwright.py
@@ -7,14 +7,14 @@ from playwright.sync_api import Page, expect
 from conftest import ShinyAppProc
 
 
-def test_cpuinfo(page: Page, airmass_app: ShinyAppProc):
+def test_airmass(page: Page, airmass_app: ShinyAppProc):
     page.goto(airmass_app.url)
     plot = page.locator("#plot")
     expect(plot).to_have_class(re.compile(r"\bshiny-bound-output\b"))
 
     img = plot.locator(">img")
-    expect(img).to_have_attribute("src", re.compile(".{1000}"))
-    # img.screenshot(path="sshot.png")
+    expect(img).to_have_attribute("src", re.compile(".{1000}"), timeout=30)
+    # img.screenshot(path="airmass.png")
 
     page.reload()
     page.pause()
@@ -22,13 +22,13 @@ def test_cpuinfo(page: Page, airmass_app: ShinyAppProc):
     expect(plot).to_have_class(re.compile(r"\bshiny-bound-output\b"))
 
 
-def test_cpuinfo2(page: Page, cpuinfo_app: ShinyAppProc):
+def test_cpuinfo(page: Page, cpuinfo_app: ShinyAppProc):
     page.goto(cpuinfo_app.url)
     plot = page.locator("#plot")
     expect(plot).to_have_class(re.compile(r"\bshiny-bound-output\b"))
     img = plot.locator(">img")
     expect(img).to_have_attribute("src", re.compile(".{1000}"))
-    # img.screenshot(path="sshot.png")
+    # img.screenshot(path="cpuinfo.png")
 
     page.reload()
     page.pause()
