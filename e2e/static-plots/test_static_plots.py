fc7d3e46c330ba8b3df23b03060ff7636045e325
MadhulikaTanuboddi <mdhlk88@gmail.com>
2022-10-18 09:20:01 -0700

    Remove screenshots from the static plots app Misc clean up

--- e2e/static-plots/test_static_plots.py
+++ e2e/static-plots/test_static_plots.py
@@ -12,9 +12,12 @@ def test_static_plots(page: Page, app: ShinyAppProc):
 
       # Two steps to check plot is shown
       #1. checking the plot has correct output classes
-      #2. Take a screenshot and compare
+      #2. Take a screenshot and compare?
           #a. https://github.com/kumaraditya303/pytest-playwright-snapshot
           #b. https://github.com/symon-storozhenko/pytest-playwright-visual
+      #3. If not screenshot, check base64 image code?
+
+    #TODO: Navigation - Nav-pill-list. Add a Class and use in the test
 
     # PLOTNINE
     page.locator(".nav.nav-pills .nav-item a[data-value='Plotnine']")
@@ -23,57 +26,41 @@ def test_static_plots(page: Page, app: ShinyAppProc):
     plot1.hover()
     expect(plot1).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    page.screenshot(path="e2e/static-plots/screenshot_plot_1.png", full_page=True)
-
-
     # SEABORN
     page.locator(".nav.nav-pills .nav-item a[data-value='Seaborn']").click()
     plot2 = page.locator("#seaborn")
-    # Adding this step to make sure plot loads
     plot2.hover()
     expect(plot2).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_2.png", full_page=True)
 
     # PANDAS
     page.locator(".nav.nav-pills .nav-item a[data-value='Pandas']").click()
     plot3 = page.locator("#pandas")
-    # Adding this step to make sure plot loads
     plot3.hover()
     expect(plot3).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_3.png", full_page=True)
 
     # HOLOVIEWS
     page.locator(".nav.nav-pills .nav-item a[data-value='Holoviews']").click()
     plot4 = page.locator("#holoviews")
-    # Adding this step to make sure plot loads
     plot4.hover()
     expect(plot4).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_4.png", full_page=True)
 
     # XARRAY
     page.locator(".nav.nav-pills .nav-item a[data-value='xarray']").click()
     plot5 = page.locator("#xarray")
-    # Adding this step to make sure plot loads
     plot5.hover()
-    expect(plot4).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_5.png", full_page=True)
+    expect(plot5).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
     # GEOPANDAS
     page.locator(".nav.nav-pills .nav-item a[data-value='geopandas']").click()
     plot6 = page.locator("#geopandas")
-    # Adding this step to make sure plot loads
     plot6.hover()
     expect(plot6).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_6.png", full_page=True)
 
     # Missingno
     page.locator(".nav.nav-pills .nav-item a[data-value='missingno']").click()
     plot7 = page.locator("#missingno")
-    # Adding this step to make sure plot loads
     plot7.hover()
-    expect(plot6).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
-    page.screenshot(path="e2e/static-plots/screenshot_plot_7.png", full_page=True)
-
+    expect(plot7).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
 
 
