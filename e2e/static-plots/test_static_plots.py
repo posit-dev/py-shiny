4abdac0a45d0232ae111bf662b448af181fa9ea9
MadhulikaTanuboddi <mdhlk88@gmail.com>
2022-10-18 22:01:30 -0700

    Static plots: Check the plot display by waiting for image instead of hover

--- e2e/static-plots/test_static_plots.py
+++ e2e/static-plots/test_static_plots.py
@@ -1,5 +1,6 @@
 from conftest import ShinyAppProc, create_example_fixture
 from playwright.sync_api import Page, expect
+import re
 
 from controls import NavControls
 
@@ -10,67 +11,51 @@ app = create_example_fixture("static_plots")
 def test_static_plots(page: Page, app: ShinyAppProc):
     page.goto(app.url)
 
-    # Click on each plot library type from the navset_pill card menu on the left
-    # and parallely verify plot is rendered
-
-      # Two steps to check plot is shown
-      #1. checking the plot has correct output classes
-      #2. Take a screenshot and compare?
-          #a. https://github.com/kumaraditya303/pytest-playwright-snapshot
-          #b. https://github.com/symon-storozhenko/pytest-playwright-visual
-      #3. If not screenshot, check base64 image code?
-
-    #TODO: Navigation - Nav-pill-list. Add a Class and use in the test
-    #TODO: Check the src attribute data and verify (instead of hover)
-
-    # PLOTNINE
-    nav_plotnine = NavControls(page, "nav-pills", "Plotnine")
     plot1 = page.locator("#plotnine")
-
-    # Adding this step to make sure plot loads and visible
-    plot1.hover()
+    image1 = plot1.locator("img")
+    expect(image1).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot1).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # SEABORN
     nav_seaborn = NavControls(page, "nav-pills", "Seaborn")
     nav_seaborn.loc.click()
     plot2 = page.locator("#seaborn")
-    plot2.hover()
+    image2 = plot2.locator("img")
+    expect(image2).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot2).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # PANDAS
     nav_pandas = NavControls(page, "nav-pills", "Pandas")
     nav_pandas.loc.click()
     plot3 = page.locator("#pandas")
-    plot3.hover()
+    image3 = plot3.locator("img")
+    expect(image3).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot3).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # HOLOVIEWS
     nav_holoviews = NavControls(page, "nav-pills", "Holoviews")
     nav_holoviews.loc.click()
     plot4 = page.locator("#holoviews")
-    plot4.hover()
+    image4 = plot4.locator("img")
+    expect(image4).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot4).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # XARRAY
     nav_xarray = NavControls(page, "nav-pills", "xarray")
     nav_xarray.loc.click()
     plot5 = page.locator("#xarray")
-    plot5.hover()
+    image5 = plot5.locator("img")
+    expect(image5).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot5).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # GEOPANDAS
     nav_geopandas = NavControls(page, "nav-pills", "geopandas")
     nav_geopandas.loc.click()
     plot6 = page.locator("#geopandas")
-    plot6.hover()
+    image6 = plot6.locator("img")
+    expect(image6).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot6).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
-    # Missingno
     nav_missingno = NavControls(page, "nav-pills", "missingno")
     nav_missingno.loc.click()
     plot7 = page.locator("#missingno")
-    plot7.hover()
+    image7 = plot7.locator("img")
+    expect(image7).to_have_attribute("src", re.compile(r"^data:image/png;base64,iVBOR"))
     expect(plot7).to_have_class("shiny-image-output shiny-plot-output shiny-bound-output")
 
 
