52937d6d5f9c99d65ed51ec780123e0fe315582b
MadhulikaTanuboddi <mdhlk88@gmail.com>
2022-10-18 23:44:34 -0700

    File upload: Parametrize the test to verify multiple file types uploading

--- e2e/file-upload/test_file_upload.py
+++ e2e/file-upload/test_file_upload.py
@@ -1,11 +1,12 @@
+
 import controls
 from conftest import ShinyAppProc
 from playwright.sync_api import Page, expect
+import pytest
 
-def test_file_upload(page: Page, local_app: ShinyAppProc) -> None:
+@pytest.mark.parametrize("input_file", [("mtcars.csv"), ("sample.rtf"), ("shiny-ui-editor.mov"), ("hello.json")])
+def test_file_upload(page: Page, local_app: ShinyAppProc, input_file: str) -> None:
     page.goto(local_app.url)
-
-    input_file = "mtcars.csv"
 
     file_input = controls.FileInput(page, "file1-label")
     file_input.upload_file(input_file)
@@ -14,8 +15,8 @@ def test_file_upload(page: Page, local_app: ShinyAppProc) -> None:
     assert upload_status == "Upload complete"
 
     file_output = page.locator("#file_content.shiny-bound-output")
+    expect(file_output).to_be_visible()
     expect(file_output).to_contain_text(input_file)
-    expect(file_output).to_contain_text("MIME type: text/csv")
 
-
-
+    # TODO: Further assertions in the binary text; for ex: expect(file_output).to_contain_text("MIME type: text/csv")
+    # TODO: Add more file types?
