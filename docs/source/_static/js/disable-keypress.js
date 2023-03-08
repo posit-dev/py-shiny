b309e69842952ed9f8f5d3a1f6c3bbc6085738f9
Carson Sievert <cpsievert1@gmail.com>
2023-03-08 10:02:55 -0600

    Update shinylive editor CSS class

--- docs/source/_static/js/disable-keypress.js
+++ docs/source/_static/js/disable-keypress.js
@@ -1,6 +1,6 @@
 // Don't allow keypresses that happen in the pyshiny editor
 // bubble up to the parent since Sphinx will, in that case, want
 // to navigate to another page on <- or ->.
-$(document).on("keydown", ".shinylive-container", function (e) {
+$(document).on("keydown", ".shinylive-wrapper", function (e) {
   e.stopPropagation();
 });
