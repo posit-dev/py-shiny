79703fae1dfc7b5b2b6092340b54ff9838141d96
Barret Schloerke <barret@posit.co>
2023-03-08 12:58:31 -0500

    Add flags to disable unused imports; Consolidate imports by version

--- shiny/_typing_extensions.py
+++ shiny/_typing_extensions.py
@@ -1,3 +1,8 @@
+# # Within file flags to ignore unused imports
+# flake8: noqa: F401
+# pyright: reportUnusedImport=false
+
+
 import sys
 
 if sys.version_info >= (3, 8):
@@ -6,36 +11,14 @@ else:
     from typing_extensions import Literal, Protocol, runtime_checkable
 
 if sys.version_info >= (3, 10):
-    from typing import TypeGuard
+    from typing import Concatenate, ParamSpec, TypeGuard
 else:
-    from typing_extensions import TypeGuard
+    from typing_extensions import Concatenate, ParamSpec, TypeGuard
 
 # Even though TypedDict is available in Python 3.8, because it's used with NotRequired,
 # they should both come from the same typing module.
 # https://peps.python.org/pep-0655/#usage-in-python-3-11
 if sys.version_info >= (3, 11):
-    from typing import NotRequired, TypedDict
+    from typing import NotRequired, TypedDict, assert_type
 else:
-    from typing_extensions import NotRequired, TypedDict
-
-if sys.version_info >= (3, 11):
-    from typing import assert_type
-else:
-    from typing_extensions import assert_type
-
-if sys.version_info < (3, 10):
-    from typing_extensions import Concatenate, ParamSpec
-else:
-    from typing import Concatenate, ParamSpec
-
-
-if False:
-    TypeGuard[object]
-    TypedDict[object, object]
-    Literal["False"]
-    Protocol
-    assert_type[object]
-    Concatenate[object, object]
-    ParamSpec[object]
-    NotRequired[object]
-    runtime_checkable[object]
+    from typing_extensions import NotRequired, TypedDict, assert_type
