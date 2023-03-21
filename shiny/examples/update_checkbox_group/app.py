e4c4c301d1327cbd2ceb2176d697a8e7b14504fb
Barret Schloerke <barret@posit.co>
2023-03-21 15:12:45 -0400

    No need to type convert when the input value is a tuple

--- shiny/examples/update_checkbox_group/app.py
+++ shiny/examples/update_checkbox_group/app.py
@@ -16,13 +16,6 @@ def server(input: Inputs, output: Outputs, session: Session):
     def _():
         x = input.inCheckboxGroup()
 
-        if x is None:
-            x = []
-        elif isinstance(x, str):
-            x = [x]
-        elif isinstance(x, tuple):
-            x = list(x)
-
         # Can also set the label and select items
         ui.update_checkbox_group(
             "inCheckboxGroup2",
