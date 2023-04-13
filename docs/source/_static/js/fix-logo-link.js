df895751e8392970b35586c8712c60d3d63b71e0
Winston Chang <winston@stdout.org>
2023-04-13 12:24:28 -0500

    Remove docs dir and build target

--- docs/source/_static/js/fix-logo-link.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    let a = document.querySelector(".bd-sidebar a.navbar-brand");
-    a.href = "https://shiny.rstudio.com/py/";
-  },
-  false
-);
