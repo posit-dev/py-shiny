48e56089a87510ec8b87a5fa8870cb4057c76eeb
Barret Schloerke <barret@posit.co>
2023-04-26 14:36:21 -0400

    Update _fill.py

--- shiny/experimental/ui/_fill.py
+++ shiny/experimental/ui/_fill.py
@@ -10,6 +10,14 @@ from ._htmldeps import fill_dependencies
 # TODO-future: Find a way to allow users to pass `class_` within `**kwargs`, rather than
 # manually handling it so that it can override the classes added by `bind_fill_role()`.
 # Ex: `card_body()`, `card_image()`, `card()`, `layout_column_wrap()` and by extension `value_box()` or any method that calls the first four
+# TODO-future:
+# From @wch:
+# > For functions like this, which modify the original object, I think the Pythonic way
+# > of doing things is to return None, to make it clearer that the object is modified in
+# > place.
+# From @schloerke:
+# > It makes for a very clunky interface. Keeping as is for now.
+# > Should we copy the tag before modifying it? (If we are not doing that elsewhere, then I am hesitant to start here.)
 def bind_fill_role(
     tag: Tag,
     *,
