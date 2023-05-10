1a81112462d9efea45a63720e1311812cf0e17c9
Barret Schloerke <barret@posit.co>
2023-05-10 10:55:52 -0400

    More updates to sidebar(); Add deprecated `panel_sidebar()` and `panel_main()`

--- shiny/experimental/ui/_sidebar.py
+++ shiny/experimental/ui/_sidebar.py
@@ -4,16 +4,19 @@ import numbers
 import random
 from typing import Optional
 
-from htmltools import Tag, TagAttrs, TagChild, css, div
+from htmltools import HTML, Tag, TagAttrs, TagAttrValue, TagChild, TagList, css, div
 from htmltools import svg as svgtags
 from htmltools import tags
 
+from shiny._deprecated import warn_deprecated
 from shiny._typing_extensions import Literal
+from shiny.session import Session, require_active_session
 
-from ._color import get_color_contrast
+# from ._color import get_color_contrast
 from ._css import CssUnit, trinary, validate_css_unit
 from ._fill import bind_fill_role
 from ._htmldeps import sidebar_dependency
+from ._utils import consolidate_attrs
 
 
 class Sidebar:
@@ -23,7 +26,7 @@ class Sidebar:
         collapse_tag: Optional[Tag],
         position: Literal["left", "right"],
         open: Literal["desktop", "open", "closed", "always"],
-        width: int,
+        width: CssUnit,
         max_height_mobile: Optional[str | float],
     ):
         self.tag = tag
@@ -33,10 +36,15 @@ class Sidebar:
         self.width = width
         self.max_height_mobile = max_height_mobile
 
+    # # This does not contain the `collapse_tag`
+    # # The `Sidebar` class should use it's fields, not this method
+    # def tagify(self) -> Tag:
+    #     return self.tag.tagify()
+
 
 def sidebar(
     *args: TagChild | TagAttrs,
-    width: int = 250,
+    width: CssUnit = 250,
     position: Literal["left", "right"] = "left",
     open: Literal["desktop", "open", "closed", "always"] = "desktop",
     id: Optional[str] = None,
@@ -46,17 +54,17 @@ def sidebar(
     class_: Optional[str] = None,  # TODO-future; Consider using `**kwargs` instead
     max_height_mobile: Optional[str | float] = None,
 ) -> Sidebar:
-    # TODO: validate `open`, bg, fg, class_, max_height_mobile
-    # TODO: Add type annotations
+    # TODO-future; validate `open`, bg, fg, class_, max_height_mobile
 
     if id is None and open != "always":
         # but always provide id when collapsible for accessibility reasons
         id = f"bslib-sidebar-{random.randint(1000, 10000)}"
 
-    if fg is None and bg is not None:
-        fg = get_color_contrast(bg)
-    if bg is None and fg is not None:
-        bg = get_color_contrast(fg)
+    # TODO-future; implement
+    # if fg is None and bg is not None:
+    #     fg = get_color_contrast(bg)
+    # if bg is None and fg is not None:
+    #     bg = get_color_contrast(fg)
 
     if isinstance(title, str) or isinstance(title, numbers.Number):
         title = div(str(title), class_="sidebar-title")
@@ -93,6 +101,100 @@ def sidebar(
     )
 
 
+def layout_sidebar(
+    sidebar: Sidebar,
+    *args: TagChild | TagAttrs,
+    fillable: bool = False,
+    fill: bool = True,
+    bg: Optional[str] = None,
+    fg: Optional[str] = None,
+    border: Optional[bool] = None,
+    border_radius: Optional[bool] = None,
+    border_color: Optional[str] = None,
+    height: Optional[CssUnit] = None,
+    **kwargs: TagAttrValue,
+) -> Tag:
+    # TODO-future; implement
+    # if fg is None and bg is not None:
+    #     fg = get_color_contrast(bg)
+    # if bg is None and fg is not None:
+    #     bg = get_color_contrast(fg)
+
+    main = div(
+        {"role": "main", "class": "main", "style": css(background_color=bg, color=fg)},
+        *args,
+        **kwargs,
+    )
+
+    main = bind_fill_role(main, container=fillable)
+
+    contents = [main, sidebar.tag, sidebar.collapse_tag]
+
+    right = sidebar.position == "right"
+
+    max_height_mobile = sidebar.max_height_mobile or (
+        "250px" if height is None else "50%"
+    )
+
+    res = div(
+        {"class": "bslib-sidebar-layout"},
+        {"class": "sidebar-right"} if right else None,
+        {"class": "sidebar-collapsed"} if sidebar.open == "closed" else None,
+        *contents,
+        sidebar_dependency(),
+        sidebar_init_js(),
+        data_sidebar_init_auto_collapse="true" if sidebar.open == "desktop" else None,
+        data_bslib_sidebar_init="true" if sidebar.open != "always" else None,
+        data_bslib_sidebar_open=sidebar.open,
+        data_bslib_sidebar_border=trinary(border),
+        data_bslib_sidebar_border_radius=trinary(border_radius),
+        style=css(
+            __bslib_sidebar_width=validate_css_unit(sidebar.width),
+            __bs_card_border_color=border_color,
+            height=validate_css_unit(height),
+            __bslib_sidebar_max_height_mobile=validate_css_unit(max_height_mobile),
+        ),
+    )
+
+    res = bind_fill_role(res, item=fill)
+
+    return res
+
+
+# @describeIn sidebar Toggle a `sidebar()` state during an active Shiny user
+#   session.
+# @param session A Shiny session object (the default should almost always be
+#   used).
+# @export
+def sidebar_toggle(
+    id: str,
+    open: Literal["toggle", "open", "closed", "always"] | bool | None = None,
+    session: Session | None = None,
+) -> None:
+    session = require_active_session(session)
+
+    method: Literal["toggle", "open", "close"] = "toggle"
+    if open is None or open == "toggle":
+        method = "toggle"
+    elif open is True or open == "open":
+        method = "open"
+    elif open is False or open == "closed":
+        method = "close"
+    else:
+        if open == "always" or open == "desktop":
+            raise ValueError(
+                f"`open = '{open}'` is not supported by `sidebar_toggle()`"
+            )
+        raise ValueError(
+            "open must be NULL (or 'toggle'), TRUE (or 'open'), or FALSE (or 'closed')"
+        )
+
+    def callback() -> None:
+        session.send_input_message(id, {"method": method})
+
+    session.on_flush(callback, once=True)
+
+
 def collapse_icon() -> Tag:
     return tags.svg(
         svgtags.path(
@@ -108,98 +210,48 @@ def collapse_icon() -> Tag:
     )
 
 
-def layout_sidebar(
-    sidebar: Sidebar,
-    *args: TagChild | TagAttrs,
-    fillable: bool = False,
-    fill: bool = True,
-    bg: Optional[str] = None,
-    fg: Optional[str] = None,
-    border: Optional[bool] = None,
-    border_radius: Optional[bool] = None,
-    border_color: Optional[str] = None,
-    height: Optional[CssUnit] = None,
-) -> Tag:
-    if fg is None and bg is not None:
-        fg = get_color_contrast(bg)
-    if bg is None and fg is not None:
-        bg = get_color_contrast(fg)
-
-    main = div(
-        *args,
-        role="main",
-        class_="main",
-        style=css(background_color=bg, color=fg),
-    )
-
-    main = bind_fill_role(main, container=fillable)
-
-    contents = [main, sidebar.tag, sidebar.collapse_tag]
-
-    right = sidebar.position == "right"
-
-    max_height_mobile = sidebar.max_height_mobile or (
-        "250px" if height is None else "50%"
-    )
-
-    res = div(
-        sidebar_dependency(),
-        sidebar_js_init(),
-        {"class": "bslib-sidebar-layout"},
-        {"class": "sidebar-right"} if right else None,
-        {"class": "sidebar-collapsed"} if sidebar.open == "closed" else None,
-        *contents,
-        data_sidebar_init_auto_collapse="true" if sidebar.open == "desktop" else None,
-        data_bslib_sidebar_border=trinary(border),
-        data_bslib_sidebar_border_radius=trinary(border_radius),
-        style=css(
-            __bslib_sidebar_width=validate_css_unit(sidebar.width),
-            __bs_card_border_color=border_color,
-            height=validate_css_unit(height),
-            __bslib_sidebar_max_height_mobile=validate_css_unit(max_height_mobile),
-        ),
-    )
-
-    res = bind_fill_role(res, item=fill)
-
-    # res <- as.card_item(res)
-    # as_fragment(
-    #     tag_require(res, version = 5, caller = "layout_sidebar()")
-    # )
-    return res
-
-
-def sidebar_js_init() -> Tag:
+def sidebar_init_js() -> Tag:
+    # Note: if we want to avoid inline `<script>` tags in the future for
+    # initialization code, we might be able to do so by turning the sidebar layout
+    # container into a web component
     return tags.script(
-        {"data_bslib_sidebar_init": True},
-        """
-        var thisScript = document.querySelector('script[data-bslib-sidebar-init]');
-        thisScript.removeAttribute('data-bslib-sidebar-init');
-
-        // If this layout is the innermost layout, then allow it to add CSS
-        // variables to it and its ancestors (counting how parent layouts there are)
-        var thisLayout = $(thisScript).parent();
-        var noChildLayouts = thisLayout.find('.bslib-sidebar-layout').length === 0;
-        if (noChildLayouts) {
-        var parentLayouts = thisLayout.parents('.bslib-sidebar-layout');
-        // .add() sorts the layouts in DOM order (i.e., innermost is last)
-        var layouts = thisLayout.add(parentLayouts);
-        var ctrs = {left: 0, right: 0};
-        layouts.each(function(i, x) {
-            $(x).css('--bslib-sidebar-counter', i);
-            var right = $(x).hasClass('sidebar-right');
-            $(x).css('--bslib-sidebar-overlap-counter', right ? ctrs.right : ctrs.left);
-            right ? ctrs.right++ : ctrs.left++;
-        });
-        }
-
-        // If sidebar is marked open='desktop', collapse sidebar if on mobile
-        if (thisLayout.data('sidebarInitAutoCollapse')) {
-        var initCollapsed = thisLayout.css('--bslib-sidebar-js-init-collapsed');
-        if (initCollapsed === 'true') {
-            thisLayout.addClass('sidebar-collapsed');
-            thisLayout.find('.collapse-toggle').attr('aria-expanded', 'false');
-        }
-        }
-        """,
+        {"data-bslib-sidebar-init": True},
+        HTML("bslib.Sidebar.initCollapsibleAll()"),
     )
+
+
+########################################################
+
+
+def panel_sidebar(
+    *args: TagChild | TagAttrs,
+    width: int = 4,
+    **kwargs: TagAttrValue,
+) -> Sidebar:
+    """Deprecated. Please use `ui.sidebar()` instead of `ui.panel_sidebar()`."""
+    warn_deprecated("`panel_sidebar()` is deprecated. Please use `sidebar()` instead.")
+    return sidebar(
+        *args,
+        # id="bslib-panel-sidebar",
+        width=f"{int(width / 12 * 100)}%",
+        **kwargs,
+    )
+
+
+def panel_main(
+    *args: TagChild | TagAttrs,
+    width: int = 8,
+    **kwargs: TagAttrValue,
+) -> TagList:
+    """Deprecated. Please supply `*args` directly to `layout_sidebar()` instead."""
+    warn_deprecated(
+        "`panel_main()` is deprecated. Please supply `*args` directly to `layout_sidebar()` instead."
+    )
+    # warn if keys are being ignored
+    attrs, children = consolidate_attrs(*args, **kwargs)
+    if len(attrs) > 0:
+        warn_deprecated(
+            "`*args: TagAttrs` or `**kwargs: TagAttrValue` values supplied to `panel_main()` are being ignored"
+        )
+
+    return TagList(*children)
