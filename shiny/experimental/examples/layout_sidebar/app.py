68b89972d71645d686feef44232fb3d746d174d5
Winston Chang <winston@stdout.org>
2023-06-16 08:37:41 -0500

    Revert layout_sidebar example

--- shiny/experimental/examples/layout_sidebar/app.py
+++ shiny/experimental/examples/layout_sidebar/app.py
@@ -1,30 +1,14 @@
 from __future__ import annotations
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 import shiny.experimental as x
-from shiny import App, Inputs, Outputs, Session, render, ui
+from shiny import App, ui
 
 app_ui = ui.page_fluid(
     x.ui.layout_sidebar(
-        ui.output_plot("p"),
-        sidebar=ui.input_slider(
-            "n", "input_slider()", min=10, max=100, value=50, step=5, animate=True
-        ),
+        "Main content",
+        sidebar="Sidebar content",
     ),
 )
 
 
-def server(input: Inputs, output: Outputs, session: Session):
-    @output
-    @render.plot
-    def p():
-        np.random.seed(19680801)
-        x_rand = 100 + 15 * np.random.randn(437)
-        fig, ax = plt.subplots()
-        ax.hist(x_rand, int(input.n()), density=True)
-        return fig
-
-
-app = App(app_ui, server)
+app = App(app_ui, server=None)
