63c802323ab00e199149babe16f15c5903687907
Winston Chang <winston@stdout.org>
2023-06-16 08:25:06 -0500

    Fix uses of as_fill_carrier()

--- shiny/experimental/examples/layout_sidebar/app.py
+++ shiny/experimental/examples/layout_sidebar/app.py
@@ -1,14 +1,30 @@
 from __future__ import annotations
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 import shiny.experimental as x
-from shiny import App, ui
+from shiny import App, Inputs, Outputs, Session, render, ui
 
 app_ui = ui.page_fluid(
     x.ui.layout_sidebar(
-        "Main content",
-        sidebar="Sidebar content",
+        ui.output_plot("p"),
+        sidebar=ui.input_slider(
+            "n", "input_slider()", min=10, max=100, value=50, step=5, animate=True
+        ),
     ),
 )
 
 
-app = App(app_ui, server=None)
+def server(input: Inputs, output: Outputs, session: Session):
+    @output
+    @render.plot
+    def p():
+        np.random.seed(19680801)
+        x_rand = 100 + 15 * np.random.randn(437)
+        fig, ax = plt.subplots()
+        ax.hist(x_rand, int(input.n()), density=True)
+        return fig
+
+
+app = App(app_ui, server)
