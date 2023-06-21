e8e030932f1f3bcd3745ad2e8fedfefecdd36afc
Winston Chang <winston@stdout.org>
2023-06-20 21:57:04 -0500

    Fix missing import

--- shiny/examples/input_checkbox_group/app.py
+++ shiny/examples/input_checkbox_group/app.py
@@ -1,4 +1,4 @@
-from shiny import App, Inputs, Outputs, Session, render, ui
+from shiny import App, Inputs, Outputs, Session, render, req, ui
 
 app_ui = ui.page_fluid(
     ui.input_checkbox_group(
