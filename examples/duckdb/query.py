4db4c19f95ab0a2317df70c3a94917ca299031e7
Gordon Shotwell <gordon.shotwell@posit.co>
2023-06-26 16:07:04 -0300

    Initial application

---
+++ examples/duckdb/query.py
@@ -0,0 +1,40 @@
+import duckdb
+import pandas as pd
+
+from shiny import module, reactive, render, ui
+
+
+@module.ui
+def query_output_ui():
+    out = ui.row(
+        {"style": "border: 1px solid gray; border-radius: 5px; margin:10px"},
+        ui.column(
+            3,
+            ui.input_text_area(
+                "sql_query",
+                "Prompt",
+                value="SELECT * FROM weather LIMIT 10",
+                width="100%",
+                height="100%",
+            ),
+        ),
+        ui.column(9, ui.output_data_frame("results")),
+    )
+
+    return out
+
+
+@module.server
+def query_output_server(input, output, session, con: duckdb.DuckDBPyConnection):
+    @reactive.Calc
+    def response_table():
+        if input.sql_query() == "":
+            return pd.DataFrame()
+        qry = input.sql_query().replace("\n", "")
+
+        return con.query(qry).to_df()
+
+    @output
+    @render.data_frame
+    def results():
+        return response_table()
