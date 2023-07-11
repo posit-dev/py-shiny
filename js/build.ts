59c9c0492bb5f1e5838a3564f1602b1f467e034f
Winston Chang <winston@stdout.org>
2023-07-10 23:33:58 -0500

    Update JS build script

--- js/build.ts
+++ js/build.ts
@@ -1,27 +1,59 @@
-import { BuildOptions, build } from "esbuild";
+import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import * as fs from "node:fs";
 
-async function bundle() {
-  try {
-    const options: BuildOptions = {
-      entryPoints: { dataframe: "dataframe/index.tsx" },
-      format: "esm",
-      bundle: true,
-      outdir: "../shiny/www/shared/dataframe",
-      minify: true,
-      sourcemap: true,
-      plugins: [sassPlugin({ type: "css-text", sourceMap: false })],
-      metafile: true,
-    };
+// Set a boolean value of watch to true if the flag --watch is provided when the file is run from the command line.
+// E.g. tsx scripts/build.ts --watch
+const watch: boolean = process.argv.includes("--watch");
+const minify: boolean = process.argv.includes("--minify");
+const metafile: boolean = process.argv.includes("--metafile");
 
-    const result = await build(options);
-    console.log("Build completed successfully!");
-    // console.log("Output:", result);
-    fs.writeFileSync("esbuild-metadata.json", JSON.stringify(result.metafile));
-  } catch (error) {
-    console.error("Build failed:", error);
-  }
-}
+const rebuildLoggerPlugin = {
+  name: "rebuild-logger",
+  setup(build: esbuild.PluginBuild) {
+    build.onStart(() => {
+      console.log(`[${new Date().toISOString()}] Rebuilding JS files...`);
+    });
+  },
+};
 
-bundle();
+const metafilePlugin = {
+  name: "metafile",
+  setup(build: esbuild.PluginBuild) {
+    build.onEnd((result) => {
+      if (metafile) {
+        fs.writeFileSync(
+          "esbuild-metadata.json",
+          JSON.stringify(result.metafile)
+        );
+      }
+    });
+  },
+};
+
+esbuild
+  .context({
+    entryPoints: {
+      "dataframe/dataframe": "dataframe/index.tsx",
+    },
+    format: "esm",
+    bundle: true,
+    outdir: "../shiny/www/shared",
+    minify: minify,
+    sourcemap: true,
+    metafile: true,
+    plugins: [
+      sassPlugin({ type: "css-text", sourceMap: false }),
+      rebuildLoggerPlugin,
+      metafilePlugin,
+    ],
+  })
+  .then((context) => {
+    if (watch) {
+      context.watch();
+    } else {
+      context.rebuild();
+      context.dispose();
+    }
+  })
+  .catch(() => process.exit(1));
