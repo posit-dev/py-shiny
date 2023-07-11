859863e70c68306280d506033d75069fddd8d2c8
Winston Chang <winston@stdout.org>
2023-07-11 11:57:28 -0500

    Update app layout

--- examples/ml/app.py
+++ examples/ml/app.py
@@ -3,28 +3,34 @@ import json
 from shiny import App, Inputs, Outputs, Session, render, ui
 
 app_ui = ui.page_fluid(
-    ui.h3("Dynamic output"),
-    ui.ml.output_classification_label("label1"),
-    ui.input_slider("lion", "Lion value:", min=0, max=100, value=60, step=1),
-    ui.h3("Static output", style="margin-top: 3rem;"),
-    ui.ml.output_classification_label(
-        "label2",
-        value={
-            "Tigers": 32,
-            "Lions": 60,
-            "Bears": 15,
-        },
-    ),
-    ui.h3("Static output, sort=False"),
-    ui.ml.output_classification_label(
-        "label3",
-        value={
-            "Tigers": 32,
-            "Lions": 60,
-            "Bears": 15,
-        },
-        sort=False,
-    ),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_slider("lion", "Lion value:", min=0, max=100, value=60, step=1),
+        ),
+        ui.panel_main(
+            ui.h3("Dynamic output"),
+            ui.ml.output_classification_label("label1"),
+            ui.h3("Static output", style="margin-top: 3rem;"),
+            ui.ml.output_classification_label(
+                "label2",
+                value={
+                    "Tigers": 32,
+                    "Lions": 60,
+                    "Bears": 15,
+                },
+            ),
+            ui.h3("Static output, sort=False"),
+            ui.ml.output_classification_label(
+                "label3",
+                value={
+                    "Tigers": 32,
+                    "Lions": 60,
+                    "Bears": 15,
+                },
+                sort=False,
+            ),
+        ),
+    )
 )
 
 
