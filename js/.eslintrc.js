49b5845261432e67e0ef075f986ffdd5cd20379e
Winston Chang <winston@stdout.org>
2023-07-10 23:11:00 -0500

    Update eslint rules

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -19,8 +19,39 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
+    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "default",
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      {
+        selector: "variable",
+        format: ["camelCase", "UPPER_CASE"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      { selector: "typeLike", format: ["PascalCase"] },
+      {
+        selector: ["objectLiteralProperty", "typeProperty"],
+        format: null,
+      },
+      {
+        selector: "function",
+        // Allow PascalCase for React components.
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      { selector: "enumMember", format: ["PascalCase"] },
+    ],
   },
   settings: {
     react: {
