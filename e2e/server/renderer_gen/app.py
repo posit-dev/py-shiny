52b77bdc8c3fe9bae190e3bc06b3c1be81167718
Barret Schloerke <barret@posit.co>
2023-07-13 15:51:44 -0400

    Create e2e test app that tests renderer_gen in sync/async and with/without args

---
+++ e2e/server/renderer_gen/app.py
@@ -0,0 +1,71 @@
+from typing import Optional
+
+from shiny import App, Inputs, Outputs, Session, ui
+from shiny.render._render import RenderFunctionMeta, renderer_gen
+
+
+@renderer_gen
+def render_test_text(
+    meta: RenderFunctionMeta,
+    value: str,
+    *,
+    extra_txt: Optional[str] = None,
+) -> str:
+    value = str(value)
+    value += "; "
+    value += "async" if meta.is_async else "sync"
+    if extra_txt:
+        value = value + "; " + str(extra_txt)
+    return value
+
+
+app_ui = ui.page_fluid(
+    ui.code("t1:"),
+    ui.output_text_verbatim("t1"),
+    ui.code("t2:"),
+    ui.output_text_verbatim("t2"),
+    ui.code("t3:"),
+    ui.output_text_verbatim("t3"),
+    ui.code("t4:"),
+    ui.output_text_verbatim("t4"),
+    ui.code("t5:"),
+    ui.output_text_verbatim("t5"),
+    ui.code("t6:"),
+    ui.output_text_verbatim("t6"),
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @output
+    @render_test_text
+    def t1():
+        return "t1; no call"
+        # return "hello"
+
+    @output
+    @render_test_text
+    async def t2():
+        return "t2; no call"
+
+    @output
+    @render_test_text()
+    def t3():
+        return "t3; call"
+
+    @output
+    @render_test_text()
+    async def t4():
+        return "t4; call"
+
+    @output
+    @render_test_text(extra_txt="w/ extra_txt")
+    def t5():
+        return "t5; call"
+
+    @output
+    @render_test_text(extra_txt="w/ extra_txt")
+    async def t6():
+        return "t6; call"
+
+
+app = App(app_ui, server)
