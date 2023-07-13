dd7830b39a9581bde3e4256fb1953d4c8846452d
Karan Gathani <karan.gathani@posit.co>
2023-07-12 22:15:14 -0700

    Use the expect_height to verify autoresize functionality

--- e2e/experimental/test_autoresize.py
+++ e2e/experimental/test_autoresize.py
@@ -4,6 +4,13 @@ from playwright.sync_api import Page
 
 app = x_create_doc_example_fixture("input_text_area")
 
+initial_height = "36px"
+resize_number = 6
+
+
+def resized_height(value: int) -> str:
+    return str(36 + (24 * value)) + "px"
+
 
 def test_autoresize(page: Page, app: ShinyAppProc) -> None:
     page.goto(app.url)
@@ -12,10 +19,10 @@ def test_autoresize(page: Page, app: ShinyAppProc) -> None:
     input_area.expect_height(None)
     input_area.expect_width(None)
     input_area.set("test value")
+    input_area.expect_height(initial_height)
     OutputTextVerbatim(page, "value").expect_value("test value")
-    for _ in range(6):
+    for _ in range(resize_number):
         input_area.loc.press("Enter")
     input_area.loc.type("end value")
     OutputTextVerbatim(page, "value").expect_value("test value\n\n\n\n\n\nend value")
-    # uncomment after bug is fixed -
-    # input_area.expect_rows("6")
+    input_area.expect_height(resized_height(resize_number))
