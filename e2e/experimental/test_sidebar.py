9219b84d2bb0619bc9cf67a3bbecccb7804c517b
Karan Gathani <karan.gathani@posit.co>
2023-07-19 22:32:33 -0700

    Add sidebar tests

--- e2e/experimental/test_sidebar.py
+++ e2e/experimental/test_sidebar.py
@@ -16,3 +16,28 @@ def test_autoresize(page: Page, app: ShinyAppProc) -> None:
     left_sidebar.loc_toggle.click()
     left_sidebar.expect_toggle_to_be_false()
     output_txt_left.expect_value("input.sidebar_left(): False")
+
+    right_sidebar = Sidebar(page, "sidebar_right")
+    output_txt_right = OutputTextVerbatim(page, "state_right")
+    output_txt_right.expect_value("input.sidebar_right(): True")
+    right_sidebar.expect_toggle_button(True)
+    right_sidebar.expect_toggle_to_be_true()
+    right_sidebar.loc_toggle.click()
+    right_sidebar.expect_toggle_to_be_false()
+    output_txt_right.expect_value("input.sidebar_right(): False")
+
+    closed_sidebar = Sidebar(page, "sidebar_closed")
+    output_txt_closed = OutputTextVerbatim(page, "state_closed")
+    output_txt_closed.expect_value("input.sidebar_closed(): False")
+    closed_sidebar.expect_toggle_button(True)
+    closed_sidebar.expect_toggle_to_be_false()
+    closed_sidebar.loc_toggle.click()
+    closed_sidebar.expect_toggle_to_be_true()
+    output_txt_closed.expect_value("input.sidebar_closed(): True")
+
+    always_sidebar = Sidebar(page, "sidebar_always")
+    output_txt_always = OutputTextVerbatim(page, "state_always")
+    output_txt_always.expect_value("input.sidebar_always(): False")
+    always_sidebar.expect_toggle_button(False)
+
+
