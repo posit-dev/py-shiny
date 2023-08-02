913b40301b2af7b53d480ba595f364052a371611
Barret Schloerke <barret@posit.co>
2023-08-02 17:10:25 -0400

    better comment about which variables are found during imports

--- shiny/render/__init__.py
+++ shiny/render/__init__.py
@@ -3,13 +3,20 @@ Tools for reactively rendering output for the user interface.
 """
 
 from ._render import (  # noqa: F401
-    # Exported. Give autocomplete hints for `shiny.render.FOO`
+    # Values declared in `__all__` will give autocomplete hints / resolve.
+    # E.g. `from shiny import render; render.text` but not `render.RenderMeta`
+    # These values do not need ` as FOO` as the variable is used in `__all__`
     text,
     plot,
     image,
     table,
     ui,
-    # Manual imports. Do not give autocomplete hints.
+    # Renamed values (in addition to the __all__values) are exposed when importing
+    # directly from `render` module just like a regular variable.
+    # E.g `from shiny.render import RenderMeta, RenderFnAsync, renderer_components`
+    # These values need ` as FOO` as the variable is not used in `__all__` and causes an
+    # reportUnusedImport error from pylance.
+    # Using the same name is allowed.
     RenderMeta as RenderMeta,
     RenderFnAsync as RenderFnAsync,
     RendererParams as RendererParams,
@@ -21,10 +28,10 @@ from ._render import (  # noqa: F401
 )
 
 from ._dataframe import (  # noqa: F401
-    # Manual imports
+    # Renamed values
     DataGrid as DataGrid,
     DataTable as DataTable,
-    # Exported
+    # Values declared in `__all__`
     data_frame,
 )
 
