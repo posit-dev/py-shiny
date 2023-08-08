8bb6aa69308de0e2cb2931f6d854d760fe3db3e3
Barret Schloerke <barret@posit.co>
2023-08-08 16:08:26 -0400

    Restore `shiny.render.__init__` import structure and import transformer module

--- shiny/render/__init__.py
+++ shiny/render/__init__.py
@@ -2,45 +2,39 @@
 Tools for reactively rendering output for the user interface.
 """
 
-from ._render import (  # noqa: F401
-    # Values declared in `__all__` will give autocomplete hints / resolve.
-    # E.g. `from shiny import render; render.text` but not `render.RenderMeta`
-    # These values do not need ` as FOO` as the variable is used in `__all__`
+
+from . import (  # noqa: F401
+    transformer,  # pyright: ignore[reportUnusedImport]
+)
+
+from ._deprecated import (  # noqa: F401
+    RenderFunction,  # pyright: ignore[reportUnusedImport]
+    RenderFunctionAsync,  # pyright: ignore[reportUnusedImport]
+)
+
+from ._render import (
     text,
     plot,
     image,
     table,
     ui,
-    # Renamed values (in addition to the __all__values) are exposed when importing
-    # directly from `render` module just like a regular variable.
-    # E.g `from shiny.render import RenderMeta, RenderFnAsync, renderer_components`
-    # These values need ` as FOO` as the variable is not used in `__all__` and causes an
-    # reportUnusedImport error from pylance.
-    # Using the same name is allowed.
-    TransformerMetadata as TransformerMetadata,
-    ValueFnAsync as ValueFnAsync,
-    TransformerParams as TransformerParams,
-    OutputTransformer as OutputTransformer,
-    output_transformer as output_transformer,
-    # Deprecated / legacy classes
-    RenderFunction as RenderFunction,
-    RenderFunctionAsync as RenderFunctionAsync,
 )
 
-from ._dataframe import (  # noqa: F401
-    # Renamed values
-    DataGrid as DataGrid,
-    DataTable as DataTable,
-    # Values declared in `__all__`
+from ._dataframe import (
+    DataGrid,
+    DataTable,
     data_frame,
 )
 
 
 __all__ = (
+    # TODO-future: Document which variables are exposed via different import approaches
     "data_frame",
     "text",
     "plot",
     "image",
     "table",
     "ui",
+    "DataGrid",
+    "DataTable",
 )
