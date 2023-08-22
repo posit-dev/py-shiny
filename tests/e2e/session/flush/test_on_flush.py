06e707052b787b262704e9f6691d5d9c5d27ebc0
Barret Schloerke <barret@posit.co>
2023-08-22 16:08:01 -0400

    Add e2e test of async server flush and flushed events

---
+++ tests/e2e/session/flush/test_on_flush.py
@@ -0,0 +1,22 @@
+from conftest import ShinyAppProc
+from controls import OutputTextVerbatim
+from playwright.sync_api import Page
+
+
+def test_output_image_kitchen(page: Page, local_app: ShinyAppProc) -> None:
+    page.goto(local_app.url)
+
+    OutputTextVerbatim(page, "all_txt").expect_value(
+        "('a-3-flush', 'bx-3-first-flush', 'by-3-first-flush', 'bx-3-second-flush', "
+        "'by-3-second-flush', 'c-3-flush', 'a-3-flushed', 'bx-3-first-flushed', "
+        "'by-3-first-flushed', 'bx-3-second-flushed', 'by-3-second-flushed', "
+        "'c-3-flushed')"
+    )
+    OutputTextVerbatim(page, "flush_txt").expect_value(
+        "('a-3-flush', 'bx-3-first-flush', 'by-3-first-flush', 'bx-3-second-flush', "
+        "'by-3-second-flush', 'c-3-flush')"
+    )
+    OutputTextVerbatim(page, "flushed_txt").expect_value(
+        "('a-3-flushed', 'bx-3-first-flushed', 'by-3-first-flushed', "
+        "'bx-3-second-flushed', 'by-3-second-flushed', 'c-3-flushed')"
+    )
