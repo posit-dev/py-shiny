b3ca04304829465c8e5d73bfb4aa36f4cf2793c7
Karan Gathani <karan.gathani@posit.co>
2023-08-28 11:15:11 -0700

    add location of the original app

--- tests/deploys/apps/plotly_app/app.py
+++ tests/deploys/apps/plotly_app/app.py
@@ -1,4 +1,4 @@
-import pandas  # noqa: F401 (this line needed for Shinylive to load plotly.express)
+# App altered from: https://github.com/rstudio/py-shiny/blob/main/shiny/api-examples/data_frame/app.py
 import plotly.express as px
 import plotly.graph_objs as go
 from shinywidgets import output_widget, render_widget
