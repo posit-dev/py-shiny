bcb461bce29825830e6af3ab8c88cad67187b9d3
Karan Gathani <karan.gathani@posit.co>
2023-09-05 20:44:00 -0700

    remove unused methods

--- tests/e2e/experimental/popover/test_popover.py
+++ tests/e2e/experimental/popover/test_popover.py
@@ -2,11 +2,6 @@ from conftest import ShinyAppProc
 from controls import InputActionButton, Popover
 from playwright.sync_api import Page
 
-# def get_popover_id(page: Page) -> str | None:
-#     return page.locator(
-#         "div > bslib-popover#{id} > button[data-bs-toggle='popover']"
-#     ).get_attribute("aria-describedby")
-
 
 def test_popover(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
--- tests/e2e/experimental/tooltip/test_tooltip.py
+++ tests/e2e/experimental/tooltip/test_tooltip.py
@@ -3,12 +3,6 @@ from controls import InputActionButton, Tooltip
 from playwright.sync_api import Page
 
 
-def get_tooltip_id(page: Page) -> str | None:
-    return page.locator(
-        "div > bslib-tooltip > button[data-bs-toggle='tooltip']"
-    ).get_attribute("aria-describedby")
-
-
 def test_tooltip(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
     input_action_button = InputActionButton(page, "btn_w_tooltip")
