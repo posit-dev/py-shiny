7c31267ebe30b77cd02f021ae3ce2ec20c06ebc0
Karan Gathani <karan.gathani@posit.co>
2023-09-05 18:23:57 -0700

    address liniting issues

--- tests/e2e/experimental/popover/test_popover.py
+++ tests/e2e/experimental/popover/test_popover.py
@@ -2,7 +2,6 @@ from conftest import ShinyAppProc
 from controls import InputActionButton, Popover
 from playwright.sync_api import Page
 
-
 # def get_popover_id(page: Page) -> str | None:
 #     return page.locator(
 #         "div > bslib-popover#{id} > button[data-bs-toggle='popover']"
@@ -14,6 +13,6 @@ def test_popover(page: Page, local_app: ShinyAppProc) -> None:
 
     input_action_button = InputActionButton(page, "btn_w_popover")
     input_action_button.click()
-    popover = Popover(page, 'popover_id')
+    popover = Popover(page, "popover_id")
     popover.expect_active()
     popover.expect_body("A message")
--- tests/e2e/experimental/tooltip/test_tooltip.py
+++ tests/e2e/experimental/tooltip/test_tooltip.py
@@ -2,6 +2,7 @@ from conftest import ShinyAppProc
 from controls import InputActionButton, Tooltip
 from playwright.sync_api import Page
 
+
 def get_tooltip_id(page: Page) -> str | None:
     return page.locator(
         "div > bslib-tooltip > button[data-bs-toggle='tooltip']"
@@ -12,6 +13,6 @@ def test_tooltip(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
     input_action_button = InputActionButton(page, "btn_w_tooltip")
     input_action_button.click()
-    tooltip = Tooltip(page, 'tooltip_id')
+    tooltip = Tooltip(page, "tooltip_id")
     tooltip.expect_active()
     tooltip.expect_body("A message")
