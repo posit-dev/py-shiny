35ab1595871f0dc149a17d875a9b2a937aaa10a2
Joe Cheng <joe@posit.co>
2023-10-30 12:48:35 -0700

    Fix pyright error

--- tests/pytest/test_reactives.py
+++ tests/pytest/test_reactives.py
@@ -1095,10 +1095,6 @@ async def test_event_silent_exception_async():
 # ------------------------------------------------------------
 @pytest.mark.asyncio
 async def test_event_type_check():
-    conn = MockConnection()
-    session = App(ui.TagList(), None)._create_session(conn)
-    output = session.output
-
     with pytest.raises(TypeError):
         # Should complain about missing argument to @event().
         @event()
