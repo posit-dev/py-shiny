aad53a3b39022c1cebe32d6e286a9db16860ca21
Gordon Shotwell <gordon.shotwell@posit.co>
2023-11-16 11:16:03 -0400

    Add express template

--- shiny/templates/express/app.py
+++ shiny/templates/express/app.py
@@ -1,16 +1,17 @@
-from shiny import App, render, ui
+import random
 
-app_ui = ui.page_fluid(
-    ui.panel_title("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
-)
+import matplotlib.pyplot as plt
+
+from shiny import render, ui
+from shiny.express import input, layout
+
+random.seed(0)
+data = [random.gauss(0, 1) for _ in range(10000)]
+
+with layout.sidebar():
+    ui.input_slider("num_bins", "Number of Bins", min=1, max=50, value=30)
 
 
-def server(input, output, session):
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
-
-
-app = App(app_ui, server)
+@render.plot
+def plot():
+    plt.hist(data, bins=input.num_bins())
