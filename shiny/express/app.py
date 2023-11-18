d466195562d53607eb949c7aeb33833f18c039cb
Winston Chang <winston@posit.co>
2023-11-17 22:50:56 -0600

    Use path to app as part of entrypoint for Express apps (#816)

--- shiny/express/app.py
+++ shiny/express/app.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-from .._app import App
+from pathlib import Path
+
 from ._run import wrap_express_app
+from ._utils import unescape_from_var_name
 
-app: App
 
-
+# If someone requests shiny.express.app:_2f_path_2f_to_2f_app_2e_py, then we will call
+# wrap_express_app(Path("/path/to/app.py")) and return the result.
 def __getattr__(name: str):
-    if name == "app":
-        return wrap_express_app()
-    raise AttributeError(f"Module 'shiny.express.app' has no attribute '{name}'")
+    name = unescape_from_var_name(name)
+    return wrap_express_app(Path(name))
