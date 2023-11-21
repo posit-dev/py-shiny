e760b44cacc26bb651ee4be6b15c9c27cab69477
Carson <cpsievert1@gmail.com>
2023-11-21 17:29:47 -0600

    Markup update for https://github.com/rstudio/bslib/pull/907

--- shiny/ui/_sidebar.py
+++ shiny/ui/_sidebar.py
@@ -269,7 +269,7 @@ def sidebar(
         {"class": "bslib-sidebar-input"} if resolved_id is not None else None,
         {
             "class": "sidebar",
-            "hidden": True if (open == "closed") else None,
+            "hidden": True if (open in ["closed", "desktop"]) else None,
         },
         id=resolved_id,
         class_=class_,
@@ -412,7 +412,7 @@ def layout_sidebar(
                 "--_main-bg": bg,
                 "--bs-card-border-color": border_color,
                 "height": as_css_unit(height),
-                "--_max-height-mobile": as_css_unit(max_height_mobile),
+                "--_mobile-max-height": as_css_unit(max_height_mobile),
             },
         ),
     )
