1fe3ee0ca35d21c0207d72a04c917b697380459d
Garrick Aden-Buie <garrick@adenbuie.com>
2023-12-06 10:21:17 -0500

    feat(layout_columns): Works and accepts `col_widths`

---
+++ examples/layout_columns/app.py
@@ -0,0 +1,115 @@
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+import numpy as np
+
+kid_phrases = [
+    "Why is the sky blue?",
+    "I want ice cream for breakfast!",
+    "Can I have a pet dinosaur?",
+    "I have imaginary friends!",
+    "Let's build a treehouse!",
+    "Why do bees buzz?",
+    "I can do a cartwheel!",
+    "Can we have a giant pillow fight?",
+    "What if I could fly like a bird?",
+    "I love playing in mud!",
+    "Let's have a picnic on the moon!",
+    "Can we have a sleepover every day?",
+    "Why do I have to go to bed so early?",
+    "One day, I'll be a superhero!",
+    "I want to learn magic tricks!",
+    "Can I paint the walls with glitter?",
+    "Let's have a dance party in the living room!",
+    "I wish I could talk to animals!",
+    "What if the clouds were made of cotton candy?",
+    "I want to be a dinosaur when I grow up!",
+    "Can I have chocolate for dinner?",
+    "What if toys came to life when we're not looking?",
+    "I want to explore the deep ocean like a mermaid!",
+    "Can we have a pet elephant?",
+]
+
+hipster_names = [
+    "Hazel",
+    "Felix",
+    "Luna",
+    "Jasper",
+    "Olive",
+    "Atticus",
+    "Clementine",
+    "August",
+    "Ivy",
+    "Ezra",
+    "Wren",
+    "Arlo",
+    "Juniper",
+    "Finn",
+    "Aurora",
+    "Milo",
+    "Harper",
+    "Theo",
+    "Poppy",
+    "Otto",
+    "Willow",
+    "Silas",
+    "Maisie",
+    "Oscar",
+]
+
+
+def random_cards(n=3):
+    base_classes = [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+    ]
+    classes = [f"bg-{c}-subtle text-{c}-emphasis" for c in base_classes]
+
+    # Randomly sample, with replacement, from kid_phrases, hipster_name and classes
+    # to create an array of cards
+    phrases = np.random.choice(kid_phrases, n)
+    names = np.random.choice(hipster_names, n)
+    classes = np.random.choice(classes, n)
+
+    return [
+        ui.card(
+            ui.h5(f"{names[i]} says", class_="card-title"),
+            phrases[i],
+            class_=classes[i],
+        )
+        for i in range(n)
+    ]
+
+
+app_ui = ui.page_fluid(
+    ui.panel_title(ui.h2("Layout Columns")),
+    ui.input_slider("n_cards", label="Number of Cards", min=1, max=24, value=4),
+    ui.input_action_button("new_cards", "New Cards"),
+    ui.output_ui("layout_columns_example").add_class("mt-3"),
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @reactive.calc
+    @reactive.event(input.new_cards, ignore_none=False)
+    def cards():
+        return random_cards(24)
+
+    @render.ui
+    def layout_columns_example():
+        return ui.layout_columns(
+            cards()[: input.n_cards()],
+            col_widths={
+                "md": [4, 2, 3, 3],
+                "lg": [3, 4, 2, 3],
+                "xl": [3, 3, 4, 2],
+                "xxl": [2, 3, 3, 4],
+            },
+        )
+
+
+app = App(app_ui, server)
---
+++ shiny/ui/_layout_columns.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from typing import Literal, Optional, Dict, Union, List
+
+from htmltools import Tag, TagAttrs, TagAttrValue, TagChild, css
+
+from ..types import MISSING, MISSING_TYPE
+from ._html_deps_shinyverse import web_component_dependency
+from ._tag import consolidate_attrs
+from .css import CssUnit, as_css_unit
+from .fill import as_fill_item, as_fillable_container
+
+
+from json import dumps as toJSON
+from warnings import warn as warn
+
+ColWidthsUserSpec = Optional[Dict[str, Union[List[int], None]]]
+
+
+def layout_columns(
+    *args: TagChild | TagAttrs,
+    col_widths: ColWidthsUserSpec = None,
+    # row_heights: CssUnit | None | MISSING_TYPE = MISSING,
+    fill: bool = True,
+    fillable: bool = True,
+    gap: Optional[CssUnit] = None,
+    class_: Optional[str] = None,
+    height: Optional[CssUnit] = None,
+    **kwargs: TagAttrValue,
+) -> Tag:
+    attrs, children = consolidate_attrs(*args, class_=class_, **kwargs)
+
+    col_widths = validate_col_spec(col_widths, len(children))
+
+    # Create the bslib-layout-columns element
+    tag = Tag(
+        "bslib-layout-columns",
+        *children,
+        attrs,
+        web_component_dependency(),
+        class_=f"bslib-grid grid {class_ or ''}",
+        col_widths=json_col_spec(col_widths),
+        style=css(
+            gap=as_css_unit(gap),
+            height=as_css_unit(height),
+            # row_heights=as_css_unit(row_heights),
+        ),
+    )
+
+    # Apply fill and fillable
+    if fill:
+        tag = as_fill_item(tag)
+    if fillable:
+        tag = as_fillable_container(tag)
+
+    return tag
+
+
+Breakpoints = Literal["xs", "sm", "md", "lg", "xl"]
+BreakpointsDict = Dict[Union[Breakpoints, str], Union[List[int], None]]
+
+
+def validate_col_spec(
+    col_widths: BreakpointsDict | List[int] | None,
+    n_kids: int,
+) -> BreakpointsDict | None:
+    if col_widths is None:
+        return None
+
+    if isinstance(col_widths, list):
+        col_widths = {"md": col_widths}
+
+    for break_name, bk in col_widths.items():
+        if bk is None:
+            continue
+
+        if any(b == 0 for b in bk):
+            raise ValueError(
+                "Column values must be greater than 0 to indicate width, or negative to indicate a column offset."
+            )
+
+        if len(bk) > 1 and any(b is None for b in bk):
+            raise ValueError(
+                "Cannot mix widths and None values. All column widths must be specified, or choose auto widths using a single None value."
+            )
+
+        if not any(b > 0 for b in bk):
+            raise ValueError(
+                "Column values must include at least one positive integer width."
+            )
+
+        if len(bk) > n_kids:
+            warn(
+                f"More column widths than children at breakpoint '{break_name}', extra widths will be ignored."
+            )
+
+    return col_widths
+
+
+def json_col_spec(col_widths: ColWidthsUserSpec) -> Optional[str]:
+    if col_widths is None:
+        return None
+
+    return toJSON(col_widths, default=lambda x: "null" if x is None else x)
