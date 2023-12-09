4b40b9a311152d412703f8eeaede56cddd368c24
Karan <karangattu@users.noreply.github.com>
2023-12-08 22:53:30 -0500

    Fix failing express tests (#887)

--- tests/playwright/shiny/shiny-express/page_default/test_page_default.py
+++ tests/playwright/shiny/shiny-express/page_default/test_page_default.py
@@ -3,6 +3,6 @@ from playwright.sync_api import Page
 from utils.express_utils import verify_express_page_default
 
 
-def test_express_accordion(page: Page, local_app: ShinyAppProc) -> None:
+def test_page_default(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
     verify_express_page_default(page)
