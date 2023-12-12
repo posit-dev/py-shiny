576793b580b307a2eec368cda610921fe417e03d
Garrick Aden-Buie <garrick@adenbuie.com>
2023-12-12 17:39:54 -0500

    docs(layout_columns): Add example app

---
+++ shiny/api-examples/layout_columns/app.py
@@ -0,0 +1,33 @@
+from model_plots import *  # model plots and cards
+
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+
+app_ui = ui.page_fluid(
+    ui.panel_title(ui.h2("Model Dashboard")),
+    ui.div("Using", ui.code("ui.layout_columns()"), ".", class_="mb-2"),
+    ui.layout_columns(
+        card_loss,
+        card_acc,
+        card_feat,
+        col_widths=(5, 7, 12),
+        # row_heights=(2, 3),
+        # height="700px",
+    ),
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @render.plot
+    def loss_over_time():
+        return plot_loss_over_time()
+
+    @render.plot
+    def accuracy_over_time():
+        return plot_accuracy_over_time()
+
+    @render.plot
+    def feature_importance():
+        return plot_feature_importance()
+
+
+app = App(app_ui, server)
