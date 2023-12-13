7602eb62d9dbd80e56029e5ea86fae52264a80c6
Nick Strayer <nick.strayer@posit.co>
2023-12-13 11:13:43 -0500

    Custom component template updates (#874)

--- shiny/templates/package-templates/js-react/srcts/index.tsx
+++ shiny/templates/package-templates/js-react/srcts/index.tsx
@@ -1,17 +1,16 @@
 import { SketchPicker } from "react-color";
-import type { ColorResult } from "react-color";
 import React from "react";
 
-import { makeReactInput } from "@shiny-helpers/react";
+import { makeReactInput } from "@posit-dev/shiny-bindings-react";
 
 // Generates a new input binding that renders the supplied react component
 // into the root of the webcomponent.
 makeReactInput({
   tagName: "custom-component",
   initialValue: "#fff",
-  renderComp: ({ onNewValue }) => (
+  renderComp: ({ initialValue, onNewValue }) => (
     <ColorPickerReact
-      initialValue="#fff"
+      initialValue={initialValue}
       onNewValue={(color) => onNewValue(color)}
     />
   ),
@@ -25,12 +24,12 @@ function ColorPickerReact({
   initialValue: string;
   onNewValue: (x: string) => void;
 }) {
-  const [currentColor, setCurrentColor] = React.useState<string>(initialValue);
+  const [currentColor, setCurrentColor] = React.useState(initialValue);
 
   return (
     <SketchPicker
       color={currentColor}
-      onChange={(color: ColorResult) => {
+      onChange={(color) => {
         setCurrentColor(color.hex);
         onNewValue(color.hex);
       }}
