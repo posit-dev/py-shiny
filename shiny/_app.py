42fc70f8b47bf20b6ac10ccf7a020a089fe3102d
Barret Schloerke <barret@posit.co>
2023-12-19 17:18:35 -0500

    feat: Allow for `App` `server=` to take `input` only (#920)
    Co-authored-by: Winston Chang <winston@stdout.org>

--- shiny/_app.py
+++ shiny/_app.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import copy
 import os
 import secrets
+from inspect import signature
 from pathlib import Path
 from typing import Any, Callable, Optional, cast
 
@@ -101,19 +102,25 @@ class App:
     def __init__(
         self,
         ui: Tag | TagList | Callable[[Request], Tag | TagList] | Path,
-        server: Optional[Callable[[Inputs, Outputs, Session], None]],
+        server: Callable[[Inputs], None]
+        | Callable[[Inputs, Outputs, Session], None]
+        | None,
         *,
         static_assets: Optional["str" | "os.PathLike[str]" | dict[str, Path]] = None,
         debug: bool = False,
     ) -> None:
         if server is None:
-
-            def _server(inputs: Inputs, outputs: Outputs, session: Session):
-                pass
-
-            server = _server
-
-        self.server = server
+            self.server = noop_server_fn
+        elif len(signature(server).parameters) == 1:
+            self.server = wrap_server_fn_with_output_session(
+                cast(Callable[[Inputs], None], server)
+            )
+        elif len(signature(server).parameters) == 3:
+            self.server = cast(Callable[[Inputs, Outputs, Session], None], server)
+        else:
+            raise ValueError(
+                "`server` must have 1 (Inputs) or 3 parameters (Inputs, Outputs, Session)"
+            )
 
         self._debug: bool = debug
 
@@ -446,3 +453,17 @@ def create_static_asset_route(
             file_response_handler,
             name="shiny-app-static-assets-" + mount_point,
         )
+
+
+def noop_server_fn(input: Inputs, output: Outputs, session: Session) -> None:
+    pass
+
+
+def wrap_server_fn_with_output_session(
+    server: Callable[[Inputs], None]
+) -> Callable[[Inputs, Outputs, Session], None]:
+    def _server(input: Inputs, output: Outputs, session: Session):
+        # Only has 1 parameter, ignore output, session
+        server(input)
+
+    return _server
