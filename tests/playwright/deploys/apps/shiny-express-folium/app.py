1c18794176ee665e776a69edeaf9a6cd85567924
Karan Gathani <karan.gathani@posit.co>
2023-12-19 10:35:12 -0800

    Add rsconnect json files and folium tests

---
+++ tests/playwright/deploys/apps/shiny-express-folium/app.py
@@ -0,0 +1,30 @@
+import folium
+
+from shiny import render, ui
+from shiny.express import input, layout
+
+locations_coords = {
+    "San Francisco": (37.79554, -122.39348),
+    "Los Angeles": (34.05026, -118.25768),
+    "New York": (40.71222, -74.00490),
+}
+layout.set_page(layout.page_fixed())
+
+with layout.card(id="card"):
+    "Static Map"
+    folium.Map(
+        location=locations_coords["San Francisco"], tiles="USGS.USTopo", zoom_start=12
+    )
+    ui.input_radio_buttons(
+        "location", "Location", ["San Francisco", "New York", "Los Angeles"]
+    )
+
+    @render.display
+    def folium_map():
+        "Map inside of render display call"
+        folium.Map(
+            location=locations_coords[input.location()],
+            tiles="cartodb positron",
+            zoom_start=12,
+        )
+        input.location()
--- tests/playwright/deploys/tests/test_folium.py
+++ tests/playwright/deploys/tests/test_folium.py
@@ -3,15 +3,12 @@ import os
 import pytest
 from playwright.sync_api import Page
 from utils.deploy_utils import deploy
-from utils.express_utils import verify_express_page_default
+from utils.express_utils import verify_express_folium_render
 
-APP_DIR = "shiny-express-page-default"
-APP_NAME = "shiny_express_page_default"
-# reqd since the app on connect takes a while to load
+APP_DIR = "shiny-express-folium"
+APP_NAME = "shiny-express-folium"
 PAGE_TIMEOUT = 120 * 1000
 EXPECT_TIMEOUT = 30 * 1000
-
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 app_file_path = os.path.join(os.path.dirname(current_dir), "apps", APP_DIR)
 
@@ -19,7 +16,7 @@ app_file_path = os.path.join(os.path.dirname(current_dir), "apps", APP_DIR)
 @pytest.mark.integrationtest
 @pytest.mark.only_browser("chromium")
 @pytest.mark.parametrize("location", ["connect", "shinyapps"])
-def test_express_page_default(page: Page, location: str) -> None:
+def test_folium_map(page: Page, location: str) -> None:
     page_url = deploy(location, APP_NAME, app_file_path)
     page.goto(page_url, timeout=PAGE_TIMEOUT)
-    verify_express_page_default(page)
+    verify_express_folium_render(page)
