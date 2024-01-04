edb73e34e3e2cbc308d14cbbc902eda66abb5aaa
Barret Schloerke <barret@posit.co>
2024-01-04 15:45:53 -0500

    Add `WrapAsync` utility class to expose common info about a now async function

--- shiny/_utils.py
+++ shiny/_utils.py
@@ -11,7 +11,7 @@ import random
 import secrets
 import socketserver
 import tempfile
-from typing import Any, Awaitable, Callable, Optional, TypeVar, cast
+from typing import Any, Awaitable, Callable, Generic, Optional, TypeVar, cast
 
 from ._typing_extensions import ParamSpec, TypeGuard
 
@@ -221,35 +221,86 @@ random.setstate(current_random_state)
 # Async-related functions
 # ==============================================================================
 
-T = TypeVar("T")
+R = TypeVar("R")  # Return type
 P = ParamSpec("P")
 
 
 def wrap_async(
-    fn: Callable[P, T] | Callable[P, Awaitable[T]]
-) -> Callable[P, Awaitable[T]]:
+    fn: Callable[P, R] | Callable[P, Awaitable[R]]
+) -> Callable[P, Awaitable[R]]:
     """
-    Given a synchronous function that returns T, return an async function that wraps the
+    Given a synchronous function that returns R, return an async function that wraps the
     original function. If the input function is already async, then return it unchanged.
     """
 
     if is_async_callable(fn):
         return fn
 
-    fn = cast(Callable[P, T], fn)
+    fn = cast(Callable[P, R], fn)
 
     @functools.wraps(fn)
-    async def fn_async(*args: P.args, **kwargs: P.kwargs) -> T:
+    async def fn_async(*args: P.args, **kwargs: P.kwargs) -> R:
         return fn(*args, **kwargs)
 
     return fn_async
 
 
+# TODO-barret; Q: Expose in quartodoc file?
+class WrapAsync(Generic[P, R]):
+    """
+    Make a function asynchronous.
+
+    Parameters
+    ----------
+    fn
+        Function to make asynchronous.
+
+    Returns
+    -------
+    :
+        Asynchronous function (within the `WrapAsync` instance)
+    """
+
+    def __init__(self, fn: Callable[P, R] | Callable[P, Awaitable[R]]):
+        self._is_async = is_async_callable(fn)
+        self._fn = wrap_async(fn)
+
+    async def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        """
+        Call the asynchronous function.
+        """
+        return await self._fn(*args, **kwargs)
+
+    @property
+    def is_async(self) -> bool:
+        """
+        Was the original function asynchronous?
+
+        Returns
+        -------
+        :
+            Whether the original function is asynchronous.
+        """
+        return self._is_async
+
+    @property
+    def fn(self) -> Callable[P, R] | Callable[P, Awaitable[R]]:
+        """
+        Retrieve the original function
+
+        Returns
+        -------
+        :
+            Original function supplied to the `WrapAsync` constructor.
+        """
+        return self._fn
+
+
 # This function should generally be used in this code base instead of
 # `iscoroutinefunction()`.
 def is_async_callable(
-    obj: Callable[P, T] | Callable[P, Awaitable[T]]
-) -> TypeGuard[Callable[P, Awaitable[T]]]:
+    obj: Callable[P, R] | Callable[P, Awaitable[R]]
+) -> TypeGuard[Callable[P, Awaitable[R]]]:
     """
     Determine if an object is an async function.
 
@@ -263,6 +314,7 @@ def is_async_callable(
         Returns True if `obj` is an `async def` function, or if it's an object with a
         `__call__` method which is an `async def` function.
     """
+
     if inspect.iscoroutinefunction(obj):
         return True
     if hasattr(obj, "__call__"):  # noqa: B004
@@ -282,7 +334,7 @@ def is_async_callable(
 # of how this stuff works.
 # For a more in-depth explanation, see
 # https://snarky.ca/how-the-heck-does-async-await-work-in-python-3-5/.
-def run_coro_sync(coro: Awaitable[T]) -> T:
+def run_coro_sync(coro: Awaitable[R]) -> R:
     """
     Run a coroutine that is in fact synchronous. Given a coroutine (which is
     returned by calling an `async def` function), this function will run the
@@ -310,7 +362,7 @@ def run_coro_sync(coro: Awaitable[T]) -> T:
     )
 
 
-def run_coro_hybrid(coro: Awaitable[T]) -> "asyncio.Future[T]":
+def run_coro_hybrid(coro: Awaitable[R]) -> "asyncio.Future[R]":
     """
     Synchronously runs the given coro up to its first yield, then runs the rest of the
     coro by scheduling it on the current event loop, as per normal. You can think of
@@ -325,7 +377,7 @@ def run_coro_hybrid(coro: Awaitable[T]) -> "asyncio.Future[T]":
     asyncio Task implementation, this is a hastily assembled hack job; who knows what
     unknown unknowns lurk here.
     """
-    result_future: asyncio.Future[T] = asyncio.Future()
+    result_future: asyncio.Future[R] = asyncio.Future()
 
     if not inspect.iscoroutine(coro):
         raise TypeError("run_coro_hybrid requires a Coroutine object.")
