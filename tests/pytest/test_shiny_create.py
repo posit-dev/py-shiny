d952d7181a828c5b1c2f8026c74532f801e32e67
Barret Schloerke <barret@posit.co>
2024-01-09 10:39:35 -0500

    Add future testing comment

--- tests/pytest/test_shiny_create.py
+++ tests/pytest/test_shiny_create.py
@@ -36,6 +36,7 @@ def subprocess_create(app_template: str, mode: str = "core", package_name: str =
     shutil.rmtree(dest_dir)
 
 
+# TODO-karan; Integrate all tests below with _examples_ testing and check for JS errors / output errors. 
 @pytest.mark.parametrize("app_template", ["basic-app", "dashboard", "multi-page"])
 def test_create_core(app_template: str):
     subprocess_create(app_template)
