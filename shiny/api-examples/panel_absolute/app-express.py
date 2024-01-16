930759c16480ca3c1a2bd3d69b1445f1a0fc61f7
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-16 12:16:43 -0500

    expressify: api-examples/panel_absolute

---
+++ shiny/api-examples/panel_absolute/app-express.py
@@ -0,0 +1,8 @@
+from shiny.express import ui
+
+ui.h2("A basic absolute panel example")
+
+with ui.panel_absolute(draggable=True, width="300px", right="50px", top="25%"):
+    with ui.panel_well():
+        "Drag me around!"
+        ui.input_slider("n", "N", min=0, max=100, value=20)
