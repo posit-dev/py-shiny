1f7765020c4b7e7b8bb21f027158e997282928b5
Karan <karangattu@users.noreply.github.com>
2024-01-16 12:59:54 -0800

    Truncate the requirements.txt file before deploys (#998)
    Co-authored-by: Barret Schloerke <barret@posit.co>

--- tests/playwright/examples/test_api_examples.py
+++ tests/playwright/examples/test_api_examples.py
@@ -3,7 +3,8 @@ from example_apps import get_apps, reruns, validate_example
 from playwright.sync_api import Page
 
 
+@pytest.mark.examples
 @pytest.mark.flaky(reruns=reruns, reruns_delay=1)
-@pytest.mark.parametrize("ex_app_path", get_apps("examples"))
-def test_examples(page: Page, ex_app_path: str) -> None:
+@pytest.mark.parametrize("ex_app_path", get_apps("shiny/api-examples"))
+def test_api_examples(page: Page, ex_app_path: str) -> None:
     validate_example(page, ex_app_path)
