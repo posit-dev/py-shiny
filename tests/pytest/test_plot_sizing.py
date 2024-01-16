c87ef09a13186c72225b527598d2aba14b94deaa
Winston Chang <winston@stdout.org>
2024-01-16 11:12:11 -0600

    Disable ui_kwargs tests

--- tests/pytest/test_plot_sizing.py
+++ tests/pytest/test_plot_sizing.py
@@ -26,38 +26,38 @@ def test_decorator_plot_default():
     assert rendered == str(ui.output_plot("foo"))
 
 
-def test_decorator_ui_kwargs():
-    """@ui_kwargs is respected"""
+# def test_decorator_ui_kwargs():
+#     """@ui_kwargs is respected"""
 
-    @ui_kwargs(width="640px", height="480px")
-    @render.plot()
-    def foo():
-        ...
+#     @ui_kwargs(width="640px", height="480px")
+#     @render.plot()
+#     def foo():
+#         ...
 
-    rendered = str(foo.tagify())
-    assert rendered == str(ui.output_plot("foo", width="640px", height="480px"))
-
-
-def test_decorator_ui_kwargs_priority():
-    """@ui_kwargs should override render.plot width/height"""
-
-    @ui_kwargs(width="640px", height=480)
-    @render.plot(width=1280, height=960)
-    def foo():
-        ...
-
-    rendered = str(foo.tagify())
-    # Note "640px" => 640 and 480 => "480px"
-    assert rendered == str(ui.output_plot("foo", width=640, height="480px"))
+#     rendered = str(foo.tagify())
+#     assert rendered == str(ui.output_plot("foo", width="640px", height="480px"))
 
 
-def test_decorator_ui_kwargs_MISSING():
-    """Not saying we support this, but test how MISSING interacts"""
+# def test_decorator_ui_kwargs_priority():
+#     """@ui_kwargs should override render.plot width/height"""
 
-    @ui_kwargs(width=MISSING)
-    @render.plot(width=1280, height=MISSING)
-    def foo():
-        ...
+#     @ui_kwargs(width="640px", height=480)
+#     @render.plot(width=1280, height=960)
+#     def foo():
+#         ...
 
-    rendered = str(foo.tagify())
-    assert rendered == str(ui.output_plot("foo", width="1280px"))
+#     rendered = str(foo.tagify())
+#     # Note "640px" => 640 and 480 => "480px"
+#     assert rendered == str(ui.output_plot("foo", width=640, height="480px"))
+
+
+# def test_decorator_ui_kwargs_MISSING():
+#     """Not saying we support this, but test how MISSING interacts"""
+
+#     @ui_kwargs(width=MISSING)
+#     @render.plot(width=1280, height=MISSING)
+#     def foo():
+#         ...
+
+#     rendered = str(foo.tagify())
+#     assert rendered == str(ui.output_plot("foo", width="1280px"))
