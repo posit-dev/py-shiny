6fccc372c8f219525db4ea46012f7abbf7ff75eb
Winston Chang <winston@stdout.org>
2024-01-16 22:59:03 -0600

    Update output_args

--- shiny/express/_output.py
+++ shiny/express/_output.py
@@ -8,8 +8,6 @@ from typing import Callable, Generator, TypeVar, overload
 from .. import ui
 from .._typing_extensions import ParamSpec
 from ..render.renderer import RendererBase, RendererBaseT
-from ..render.transformer import OutputRenderer
-from ..render.transformer._transformer import OT
 
 __all__ = ("suspend_display",)
 
@@ -18,24 +16,22 @@ R = TypeVar("R")
 CallableT = TypeVar("CallableT", bound=Callable[..., object])
 
 
+# TODO-barret-future; quartodoc entry?
 def output_args(
-    *args: object,
     **kwargs: object,
-) -> Callable[[OutputRenderer[OT]], OutputRenderer[OT]]:
+) -> Callable[[RendererBaseT], RendererBaseT]:
     """
     Sets default UI arguments for a Shiny rendering function.
 
     Each Shiny render function (like :func:`~shiny.render.plot`) can display itself when
     declared within a Shiny inline-style application. In the case of
     :func:`~shiny.render.plot`, the :func:`~shiny.ui.output_plot` function is called
-    implicitly to display the plot. Use the `@output_args` decorator to specify
-    arguments to be passed to `output_plot` (or whatever the corresponding UI function
-    is) when the render function displays itself.
+    implicitly to display the plot. Use the `@ui_kwargs` decorator to specify arguments
+    to be passed to `output_plot` (or whatever the corresponding UI function is) when
+    the render function displays itself.
 
     Parameters
     ----------
-    *args
-        Positional arguments to be passed to the UI function.
     **kwargs
         Keyword arguments to be passed to the UI function.
 
@@ -45,16 +41,9 @@ def output_args(
         A decorator that sets the default UI arguments for a Shiny rendering function.
     """
 
-    def wrapper(renderer: OutputRenderer[OT]) -> OutputRenderer[OT]:
-        if not isinstance(renderer, OutputRenderer):
-            raise TypeError(
-                f"Expected an OutputRenderer, but got {type(renderer).__name__}."
-                "\nIf you are trying to set default UI arguments for a `Renderer`, use"
-                " `@ui_kwargs` instead."
-            )
-        renderer._auto_output_ui_args = args
+    def wrapper(renderer: RendererBaseT) -> RendererBaseT:
+        # renderer._default_ui_args = args
         renderer._auto_output_ui_kwargs = kwargs
-
         return renderer
 
     return wrapper
