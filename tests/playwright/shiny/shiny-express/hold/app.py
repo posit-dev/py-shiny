91417f9a2758e7775852813a6bc6add7d379caf7
Winston Chang <winston@posit.co>
2024-01-19 16:29:24 -0600

    Update test app

--- tests/playwright/shiny/shiny-express/hold/app.py
+++ tests/playwright/shiny/shiny-express/hold/app.py
@@ -1,22 +1,27 @@
-from shiny import render, ui
-from shiny.express import hold, input
+from shiny.express import input, render, ui
 
 with ui.card(id="card"):
     ui.input_slider("s1", "A", 1, 100, 20)
 
-    @hold
-    @render.code
-    def hidden():
-        return input.s1()
+    with ui.hold():
+
+        @render.code
+        def hidden():
+            return input.s1()
 
     ui.input_slider("s2", "B", 1, 100, 40)
 
     # from shiny.express import ui_kwargs
     # @ui_kwargs(placeholder=False)
     # @ui_kwargs(placeholder=True)
-    @render.code()
-    def visible():
-        # from shiny import req
+    with ui.hold() as held:
 
-        # req(False)
-        return input.s2()
+        @render.code()
+        def visible():
+            # from shiny import req
+
+            # req(False)
+            return input.s2()
+
+    "Some text here"
+    held  # pyright: ignore[reportUnusedExpression]
