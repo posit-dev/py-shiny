87bf3ddd91b8fbc9d76552a37af3e08517fc2257
Winston Chang <winston@posit.co>
2024-01-19 16:17:34 -0600

    Tell flake8 to ignore f-string

--- examples/express/expressify_app.py
+++ examples/express/expressify_app.py
@@ -4,7 +4,7 @@ from shiny.express import expressify, ui
 # @expressify converts a function to work with Express syntax
 @expressify
 def expressified1(s: str):
-    f"Expressified function 1: {s}"
+    f"Expressified function 1: {s}"  # noqa: B021
     ui.br()
 
 
@@ -19,7 +19,7 @@ ui.br()
 # @expressify() also works with parens
 @expressify()
 def expressified2(s: str):
-    f"Expressified function 2: {s}"
+    f"Expressified function 2: {s}"  # noqa: B021
     ui.br()
 
 
