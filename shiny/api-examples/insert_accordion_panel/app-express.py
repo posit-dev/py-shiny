b1b329874f9030db85a486e4686ebbcaa4216c80
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-25 13:57:51 -0400

    Update shiny/api-examples/insert_accordion_panel/app-express.py
    Co-authored-by: Carson Sievert <cpsievert1@gmail.com>

--- shiny/api-examples/insert_accordion_panel/app-express.py
+++ shiny/api-examples/insert_accordion_panel/app-express.py
@@ -4,21 +4,24 @@ from shiny import reactive
 from shiny.express import expressify, input, ui
 
 
-@expressify
-def make_panel(id: str):
-    with ui.accordion_panel(f"Section {id}"):
-        f"Some narrative for section {id}"
-
-
 with ui.sidebar():
     ui.input_action_button("add_panel", "Add random panel", class_="mt-3 mb-3"),
 
-with ui.accordion(multiple=False, id="acc"):
+with ui.accordion(id="acc", multiple=True):
     for letter in "ABCDE":
-        make_panel(letter)
+        with ui.accordion_panel(f"Section {letter}"):
+            f"Some narrative for section {letter} "
+            "more narrative"
 
 
-@reactive.Effect
+@reactive.effect
 @reactive.event(input.add_panel)
+@expressify
 def _():
-    ui.insert_accordion_panel("acc", make_panel("foo"))
+    letter = str(random.randint(0, 10000))
+    with ui.hold() as panel:
+        with ui.accordion_panel(f"Section {letter}"):
+            f"Some narrative for section {letter} "
+            "more narrative"
+
+    ui.insert_accordion_panel("acc", panel[0])
