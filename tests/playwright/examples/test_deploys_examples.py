756f213fedc671158b8f3e133d4b29cf1387ab35
Barret Schloerke <barret@posit.co>
2024-01-25 12:45:09 -0500

    Move deploys apps

--- tests/playwright/examples/test_deploys_examples.py
+++ tests/playwright/examples/test_deploys_examples.py
@@ -4,6 +4,6 @@ from playwright.sync_api import Page
 
 
 @pytest.mark.flaky(reruns=reruns, reruns_delay=1)
-@pytest.mark.parametrize("ex_app_path", get_apps("tests/playwright/shiny/deploys"))
+@pytest.mark.parametrize("ex_app_path", get_apps("tests/playwright/deploys"))
 def test_deploy_examples(page: Page, ex_app_path: str) -> None:
     validate_example(page, ex_app_path)
