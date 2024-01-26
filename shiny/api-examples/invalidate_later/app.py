f3cfd43e492af84805f62faf3234d20dfaeee106
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-26 09:26:56 -0400

    invalidate_later

--- shiny/api-examples/invalidate_later/app.py
+++ shiny/api-examples/invalidate_later/app.py
@@ -2,16 +2,11 @@ import random
 
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
-app_ui = ui.page_fluid(ui.output_ui("value"))
+app_ui = ui.page_fluid(ui.output_text("value"))
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    @reactive.Effect
-    def _():
-        reactive.invalidate_later(0.5)
-        print("Random int: ", random.randint(0, 10000))
-
-    @render.ui
+    @render.text
     def value():
         reactive.invalidate_later(0.5)
         return "Random int: " + str(random.randint(0, 10000))
