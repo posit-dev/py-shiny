d1107ed5905577e580978c444c388198e875e7ca
Karan Gathani <karan.gathani@posit.co>
2024-01-30 11:47:33 -0800

    remove fastapi from app_requirements.txt

--- tests/playwright/deploys/plotly/test_plotly_app.py
+++ tests/playwright/deploys/plotly/test_plotly_app.py
@@ -4,6 +4,7 @@ from utils.deploy_utils import (
     skip_if_not_python_310_or_chrome,
 )
 
+TIMEOUT = 2 * 60 * 1000
 app_url = create_deploys_app_url_fixture(__file__, "example_deploy_app_A")
 
 
@@ -12,8 +13,10 @@ def test_deploys(page: Page, app_url: str) -> None:
     page.goto(app_url)
 
     COUNTRY = "Afghanistan"
-    expect(page.get_by_text(COUNTRY)).to_have_count(1, timeout=120 * 1000)
+    expect(page.get_by_text(COUNTRY)).to_have_count(1, timeout=TIMEOUT)
     page.get_by_role("cell", name=COUNTRY).click()
-    expect(page.locator("#country_detail_pop")).to_contain_text(COUNTRY)
+    expect(page.locator("#country_detail_pop")).to_contain_text(
+        COUNTRY, timeout=TIMEOUT
+    )
     expect(page.locator("#country_detail_percap")).to_contain_text(COUNTRY)
     expect(page.get_by_text(COUNTRY)).to_have_count(3)
