9d98231598feb4acd405943e2cf37c7119223667
Gordon Shotwell <gordon.shotwell@posit.co>
2024-02-14 13:30:46 -0400

    Apply suggestions from code review
    Co-authored-by: Barret Schloerke <barret@posit.co>

--- shiny/api-examples/data_frame/app-express.py
+++ shiny/api-examples/data_frame/app-express.py
@@ -66,10 +66,12 @@ with ui.layout_columns(col_widths=[12, 6, 6]):
 
 @reactive.calc
 def filtered_df():
+    req(summary_data.input_selected_rows())
+
     # summary_data.input_selected_rows() is a tuple, so we must convert it to list,
     # as that's what Pandas requires for indexing.
 
-    selected_idx = list(req(summary_data.input_selected_rows()))
+    selected_idx = list(summary_data.input_selected_rows())
     countries = summary_df.iloc[selected_idx]["country"]
     # Filter data for selected countries
     return df[df["country"].isin(countries)]
