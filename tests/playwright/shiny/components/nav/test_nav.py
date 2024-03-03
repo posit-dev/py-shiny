2c7c1d869043de4b754b0af4ce9a807e0fd886e6
Barret Schloerke <barret@posit.co>
2024-03-02 23:40:44 -0500

    Add typing support; Use dataclass + array to make easier typing. Remove soft assertions

--- tests/playwright/shiny/components/nav/test_nav.py
+++ tests/playwright/shiny/components/nav/test_nav.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
 import pytest
 from conftest import ShinyAppProc
 from controls import (
@@ -20,56 +24,37 @@ def test_nav(page: Page, local_app: ShinyAppProc) -> None:
     # Update the page size to be wider
     page.set_viewport_size({"width": 1500, "height": 800})
 
-    nav_data = {
-        "navset_tab": {
-            "control": LayoutNavsetTab,
-            "verify": "navset_tab()"
-        },
-        "navset_pill": {
-            "control": LayoutNavSetPill,
-            "verify": "navset_pill()"
-        },
-        "navset_underline": {
-            "control": LayoutNavSetUnderline,
-            "verify": "navset_underline()"
-        },
-        "navset_card_tab": {
-            "control": LayoutNavSetCardTab,
-            "verify": "navset_card_tab()"
-        },
-        "navset_card_pill": {
-            "control": LayoutNavSetCardPill,
-            "verify": "navset_card_pill()"
-        },
-        "navset_card_underline": {
-            "control": LayoutNavSetCardUnderline,
-            "verify": "navset_card_underline()"
-        },
-        "navset_pill_list": {
-            "control": LayoutNavSetPillList,
-            "verify": "navset_pill_list()"
-        },
-        "page_navbar": {
-            "control": LayoutNavSetBar,
-            "verify": "page_navbar"
-        }
-    }
+    @dataclass
+    class LayoutInfo:
+        control: type[
+            LayoutNavSetBar
+            | LayoutNavSetCardPill
+            | LayoutNavSetCardTab
+            | LayoutNavSetCardUnderline
+            | LayoutNavSetPill
+            | LayoutNavSetPillList
+            | LayoutNavsetTab
+            | LayoutNavSetUnderline
+        ]
+        verify: str
 
-    def tester(el_name):
-        element = nav_data[el_name]['control'](page, el_name)
+    nav_data: list[LayoutInfo] = [
+        LayoutInfo(LayoutNavsetTab, "navset_tab()"),
+        LayoutInfo(LayoutNavSetPill, "navset_pill()"),
+        LayoutInfo(LayoutNavSetUnderline, "navset_underline()"),
+        LayoutInfo(LayoutNavSetCardTab, "navset_card_tab()"),
+        LayoutInfo(LayoutNavSetCardPill, "navset_card_pill()"),
+        LayoutInfo(LayoutNavSetCardUnderline, "navset_card_underline()"),
+        LayoutInfo(LayoutNavSetPillList, "navset_pill_list()"),
+        LayoutInfo(LayoutNavSetBar, "page_navbar()"),
+    ]
+
+    for nav_info in nav_data:
+        el_name = nav_info.verify.replace("()", "")
+        element = nav_info.control(page, el_name)
         element.expect_nav_values(["a", "b", "c"])
         element.expect_value("a")
-        element.expect_content(nav_data[el_name]['verify'] + ": tab a content")
+        element.expect_content(nav_info.verify + ": tab a content")
         element.set("b")
         element.expect_value("b")
-        element.expect_content(nav_data[el_name]['verify'] + ": tab b content")
-
-    failures = []
-    # soft assert
-    for nav in list(nav_data.keys()):
-        try:
-            tester(nav)
-        except Exception as e:
-            failures.append({"element": nav, "error": e})
-    assert failures == []
-
+        element.expect_content(nav_info.verify + ": tab b content")
