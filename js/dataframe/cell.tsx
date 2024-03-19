f94d9eafe58a13363725e037fd8e9c50006ca6f0
Barret Schloerke <barret@posit.co>
2024-03-19 17:16:43 -0400

    Autoresize height

--- js/dataframe/cell.tsx
+++ js/dataframe/cell.tsx
@@ -282,6 +282,26 @@ export const TableBodyCell: FC<TableBodyCellProps> = ({
     setValue(e.target.value);
   }
 
+  // https://medium.com/@oherterich/creating-a-textarea-with-dynamic-height-using-react-and-typescript-5ed2d78d9848
+  // Updates the height of a <textarea> when the value changes.
+  const useAutosizeTextArea = (
+    textAreaRef: HTMLTextAreaElement | null,
+    value: string
+  ) => {
+    useEffect(() => {
+      if (textAreaRef) {
+        // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+        textAreaRef.style.height = "0px";
+        const scrollHeight = textAreaRef.scrollHeight;
+
+        // We then set the height directly, outside of the render loop
+        // Trying to set this with state or a ref will product an incorrect value.
+        textAreaRef.style.height = scrollHeight + "px";
+      }
+    }, [textAreaRef, value]);
+  };
+  useAutosizeTextArea(inputRef.current, value as string);
+
   let onClick:
     | ((e: ReactMouseEvent<HTMLTableCellElement>) => void)
     | undefined = undefined;
