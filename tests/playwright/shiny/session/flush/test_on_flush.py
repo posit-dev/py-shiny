03e468fbd67f0b340ba32a4a1202c2d6117500cd
Karan Gathani <karan.gathani@posit.co>
2024-03-26 12:21:28 -0700

    unskip test from #1241

--- tests/playwright/shiny/session/flush/test_on_flush.py
+++ tests/playwright/shiny/session/flush/test_on_flush.py
@@ -1,25 +1,9 @@
-import os
-
-import pytest
 from conftest import ShinyAppProc
 from controls import OutputTextVerbatim
 from playwright.sync_api import Page
 
-on_ci = os.environ.get("CI", "False") == "true"
-
 
 def test_output_image_kitchen(page: Page, local_app: ShinyAppProc) -> None:
-
-    if on_ci:
-        # 2024-03-22: The tests pass locally, but do not pass on CI. It started with
-        # https://github.com/posit-dev/py-shiny/commit/2f75a9076c567690f2a6a647ec07cfa9e558ff9c
-        # , but the changes in that commit were unrelated. We had believe it was an
-        # update in uvicorn, but removing all other debug statements did not fix the
-        # issue.
-        # 2024-03-22 Barret: When inspecting the issue, we found that many log
-        # INFO entries have different port values. This is concerning.
-        # https://github.com/posit-dev/py-shiny/pull/1236
-        pytest.skip("Error with stdout / stderr on CI. Related #1236")
 
     page.goto(local_app.url)
 
