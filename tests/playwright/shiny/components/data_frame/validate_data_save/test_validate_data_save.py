78d233d5346ae72d8011c7d61a01c7beaac67f6a
Karan Gathani <karan.gathani@posit.co>
2024-04-08 12:52:12 -0700

    Add another app to slow down data save

---
+++ tests/playwright/shiny/components/data_frame/validate_data_save/test_validate_data_save.py
@@ -0,0 +1,12 @@
+from conftest import ShinyAppProc
+from controls import OutputDataFrame
+from playwright.sync_api import Page
+
+
+def test_validate_data_edit_mode(page: Page, local_app: ShinyAppProc) -> None:
+    page.goto(local_app.url)
+
+    data_frame = OutputDataFrame(page, "penguins_df")
+    data_frame.save_cell("Study0708_edited", row=1, col=1, save_key="Enter")
+    data_frame.expect_class_state("saving", row=1, col=1)
+    data_frame.expect_class_state("editing", row=2, col=1)
