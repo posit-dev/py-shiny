4efa58b32e762532b9310bad6893d1ed1db1773e
Carson <cpsievert1@gmail.com>
2024-04-30 11:03:45 -0500

    Add more commentary

--- js/busy-indicators/busy-indicators.ts
+++ js/busy-indicators/busy-indicators.ts
@@ -29,13 +29,17 @@ function isDownloadBinding(binding: OutputBinding): boolean {
   return binding.name === "shiny.downloadLink";
 }
 
-// Add BUSY_CLASS to output bindings that haven't yet received their value
+// Leverage shiny.js events to add BUSY_CLASS when output bindings are either
+// recalculating or calculating for the first time.
+// https://shiny.posit.co/r/articles/build/js-events/
 $(document).on("shiny:bound", function (x) {
   const e = x as BoundEvent;
 
   if (e.bindingType !== "output") return;
   if (isDownloadBinding(e.binding)) return;
 
+  // shiny:bound is always triggered on the output element
+  // https://github.com/rstudio/shiny/blob/950c63/srcts/src/shiny/bind.ts#L325
   e.target.classList.add(BUSY_CLASS);
 });
 
@@ -44,9 +48,16 @@ $(document).on("shiny:outputinvalidated", function (x) {
 
   if (isDownloadBinding(e.binding)) return;
 
+  // shiny:outputinvalidated is always triggered on the output element
+  // https://github.com/rstudio/shiny/blob/950c63/srcts/src/shiny/shinyapp.ts#L1422
   e.target.classList.add(BUSY_CLASS);
 });
 
+// shiny:value & shiny:error are usually triggered on the output element, but
+// can in theory, be triggered on the document. That's probably fine since we
+// probably don't need an output spinner in that case, and the UI will still get
+// a page-level pulse when shiny is busy.
+// https://github.com/rstudio/shiny/blob/950c63/srcts/src/shiny/shinyapp.ts#L471-L512
 $(document).on("shiny:value", function (e: JQuery.TriggeredEvent) {
   e.target.classList.remove(BUSY_CLASS);
 });
