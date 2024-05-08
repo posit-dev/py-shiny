350811e830522a54b6a9ec2485f706baf23b9bd2
Garrick Aden-Buie <garrick@adenbuie.com>
2024-05-08 09:54:38 -0400

    docs(Theme): Document class and add two new methods:
    * `Theme.available_presets()` returns the list of available theme presets
    * `.to_sass()` returns the Sass code for the custom theme

--- shiny/ui/_theme.py
+++ shiny/ui/_theme.py
@@ -22,6 +22,43 @@ T = TypeVar("T", bound="Theme")
 
 @no_example()
 class Theme:
+    """
+    Create a custom Shiny theme.
+
+    The `Theme` class allows you to create a custom Shiny theme by providing custom Sass
+    code. The theme can be based on one of the available presets, such as `"shiny"` or
+    `"bootstrap"`, or a Bootswatch theme. Use the `.add_*()` methods can be chained
+    together to add custom Sass functions, defaults, mixins, and rules.
+
+    Pass the `Theme` object directly to the `theme` argument of any Shiny page function,
+    such as :func:`~shiny.ui.page_sidebar` or :func:`~shiny.ui.page_navbar`. In Shiny
+    Express apps, use the `theme` argument of :func:`~shiny.express.ui.page_opts` to set
+    the app theme.
+
+    Customized themes require the [libsass package](https://pypi.org/project/libsass/),
+    and are compiled to CSS when the theme is used. The `Theme` class caches the
+    compiled CSS, but you can speed up app loading (and avoid the runtime `libsass`
+    dependency) by pre-compiling the theme CSS and saving it to a file. To do this, use
+    the `.to_css()` method to render the theme to a single minified CSS string. Once
+    saved to a file, the CSS can be used in any Shiny app by passing the file path to
+    the `theme` argument described above.
+
+    Parameters
+    ----------
+    preset
+        The name of the preset to use as a base. `"shiny"` is the default theme for
+        Shiny apps and `"bootstrap"` uses standard Bootstrap 5 styling. Bootswatch theme
+        presets are also available. Use `Theme.available_presets()` to see the full
+        list.
+    name
+        A custom name for the theme. If not provided, the preset name will be used.
+
+    Raises
+    ------
+    ValueError
+        If the `preset` is not a valid theme preset.
+    """
+
     def __init__(
         self,
         preset: ShinyThemePreset = "shiny",
@@ -48,6 +85,13 @@ class Theme:
         # so that we can re-use the already compiled CSS file.
         self._css_temp_srcdir: Optional[str] = None
 
+    @classmethod
+    def available_presets(cls) -> tuple[ShinyThemePreset, ...]:
+        """
+        Get a list of available theme presets.
+        """
+        return ShinyThemePresets
+
     @property
     def preset(self) -> ShinyThemePreset:
         return self._preset
@@ -62,11 +106,41 @@ class Theme:
         self._css_temp_srcdir = None
 
     def add_functions(self: T, *args: list[str]) -> T:
+        """
+        Add custom Sass functions to the theme.
+
+        Sass code added via this method will be placed **after** the function
+        declarations from the theme preset, allowing you to override or extend the
+        default functions.
+
+        Parameters
+        ----------
+        *args
+            The Sass functions to add as a single or multiple strings.
+        """
         self._css = ""
         self._functions.extend(dedent_array(*args))
         return self
 
     def add_defaults(self: T, *args: str, **kwargs: dict[str, str]) -> T:
+        """
+        Add custom default values to the theme.
+
+        Sass code added via this method will be placed **before** the default values of
+        the theme preset, allowing you to override or extend the default values.
+
+        Parameters
+        ----------
+        *args
+            Sass code, as a single or multiple strings, containing default value
+            declarations to add.
+        **kwargs
+            Keyword arguments containing default value declarations to add. The keys
+            should be Sass variable names using underscore casing that will be
+            transformed automatically to kebab-case. For example,
+            `.add_defaults(primary_color="#ff0000")` is equivalent to
+            `.add_defaults("$primary-color: #ff0000;")`.
+        """
         if len(args) > 0 and len(kwargs) > 0:
             raise ValueError("Cannot provide both positional and keyword arguments")
 
@@ -86,16 +160,75 @@ class Theme:
         return self
 
     def add_mixins(self: T, *args: str) -> T:
+        """
+        Add custom Sass mixins to the theme.
+
+        Sass code added via this method will be placed **after** the mixin declarations
+        from the theme preset, allowing you to override or extend the default mixins.
+
+        Parameters
+        ----------
+        *args
+            Sass code, as a single or multiple strings, containing mixins to add.
+        """
         self._mixins.extend(dedent_array(args))
         self._css = ""
         return self
 
     def add_rules(self: T, *args: str) -> T:
+        """
+        Add custom Sass rules to the theme.
+
+        Sass code added via this method will be placed **after** the rule declarations
+        from the theme preset, allowing you to override or extend the default rules.
+
+        Parameters
+        ----------
+        *args
+            Sass code, as a single or multiple strings, containing rules to add.
+        """
         self._rules.extend(dedent_array(args))
         self._css = ""
         return self
 
+    def to_sass(self) -> str:
+        """
+        Returns the custom theme as a single Sass string.
+
+        Returns
+        -------
+        :
+            The custom theme as a single Sass string.
+        """
+        path_functions = path_pkg_preset(self._preset, "_01_functions.scss")
+        path_defaults = path_pkg_preset(self._preset, "_02_defaults.scss")
+        path_mixins = path_pkg_preset(self._preset, "_03_mixins.scss")
+        path_rules = path_pkg_preset(self._preset, "_04_rules.scss")
+
+        sass_lines = [
+            f'@import "{path_functions}";',
+            *self._functions,
+            *self._defaults,
+            f'@import "{path_defaults}";',
+            f'@import "{path_mixins}";',
+            *self._mixins,
+            f'@import "{path_rules}";',
+            *self._rules,
+        ]
+
+        return "\n".join(sass_lines)
+
     def to_css(self) -> str:
+        """
+        Compile the theme to CSS and return the result as a string.
+
+        Returns
+        -------
+        :
+            The compiled CSS for the theme. The value is cached such that previously
+            compiled themes are returned immediately. Adding additional custom Sass code
+            or changing the preset will invalidate the cache.
+        """
         if self._css:
             if self._css == "precompiled":
                 return self._read_precompiled_css()
@@ -104,23 +237,7 @@ class Theme:
         check_libsass_installed()
         import sass
 
-        sass_lines = [
-            '@import "_01_functions";',
-            *self._functions,
-            *self._defaults,
-            '@import "_02_defaults";',
-            '@import "_03_mixins";',
-            *self._mixins,
-            '@import "_04_rules";',
-            *self._rules,
-        ]
-
-        sass_string = "\n".join(sass_lines)
-
-        self._css = sass.compile(
-            string=sass_string,
-            include_paths=[path_pkg_preset(self._preset)],
-        )
+        self._css = sass.compile(string=self.to_sass())
 
         return self._css
 
@@ -142,6 +259,17 @@ class Theme:
         )
 
     def html_dependency(self) -> HTMLDependency:
+        """
+        Create an `HTMLDependency` object from the theme.
+
+        Returns
+        -------
+        :
+            An :class:`~htmltools.HTMLDependency` object representing the theme. In
+            most cases, you should not need to call this method directly. Instead, pass
+            the `Theme` object directly to the `theme` argument of any Shiny page
+            function.
+        """
         if self._css == "precompiled":
             return self._html_dependency_precompiled()
 
@@ -175,7 +303,7 @@ class Theme:
 
     def tagify(self) -> HTMLDependency:
         """
-        Create an `HTMLDependency` object from the theme.
+        Create an :class:`~htmltools.HTMLDependency` object from the theme.
         """
         return self.html_dependency()
 
@@ -185,7 +313,18 @@ def dedent_array(x: list[str] | tuple[str, ...]) -> list[str]:
 
 
 def path_pkg_preset(preset: ShinyThemePreset, *args: str) -> str:
-    return path_pkg_www("sass", "preset", str(preset), *args)
+    """
+    Returns a path relative to the packaged directory for a given preset.
+
+    Examples
+    --------
+
+    ```python
+    path_pkg_preset("shiny", "preset.min.css")
+    #> "{shiny}/www/shared/sass/preset/shiny/preset.min.css"
+    ```
+    """
+    return os.path.realpath(path_pkg_www("sass", "preset", str(preset), *args))
 
 
 def check_is_valid_preset(preset: ShinyThemePreset) -> None:
