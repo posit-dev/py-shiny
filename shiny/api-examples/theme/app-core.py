2e8e009f76a4135552d8e035a29739ad9b20c778
Garrick Aden-Buie <garrick@adenbuie.com>
2024-05-09 17:18:38 -0400

    example: Update api-examples/theme

--- shiny/api-examples/theme/app-core.py
+++ shiny/api-examples/theme/app-core.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from shared import filler_text, my_theme
 
 from shiny import App, render, ui
 
@@ -11,32 +11,9 @@ app_ui = ui.page_sidebar(
     ),
     ui.h2("Output"),
     ui.output_text_verbatim("txt"),
-    ui.markdown(
-        """
-**AI-generated filler text.** In the world of exotic fruits, the durian stands out with its spiky exterior and strong odor. Despite its divisive smell, many people are drawn to its rich, creamy texture and unique flavor profile. This tropical fruit is often referred to as the "king of fruits" in various Southeast Asian countries.
-
-Durians are known for their large size and thorn-covered husk, which requires careful handling. The flesh inside can vary in color from pale yellow to deep orange, with a custard-like consistency that melts in your mouth. Some describe its taste as a mix of sweet, savory, and creamy, while others find it overpowering and pungent.
-"""
-    ),
+    ui.markdown(filler_text),
     title="Theme Example",
-    theme=(
-        ui.Theme("shiny", include_paths=Path(__file__).parent)
-        .add_defaults(
-            headings_color="red",
-            bar_color="purple",
-            select_color_text="green",
-            bslib_dashboard_design=True,
-        )
-        .add_rules(
-            """
-        strong { color: $primary; }
-        .sidebar-title { color: $danger; }
-        """
-        )
-        .add_rules('@import "css/rules.scss";')
-    ),
-    # theme="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-    # theme=Path(__file__).parent / "css" / "bootswatch-minty.min.css",
+    theme=my_theme,
 )
 
 
--- shiny/api-examples/theme/app-express.py
+++ shiny/api-examples/theme/app-express.py
@@ -1,14 +1,16 @@
-from pathlib import Path
+from shared import filler_text, my_theme
 
 from shiny.express import input, render, ui
 
 ui.page_opts(
     title="Theme Example",
-    theme=Path(__file__).parent / "css" / "bootswatch-minty.min.css",
+    theme=my_theme,
 )
 
 with ui.sidebar(title="Parameters"):
     ui.input_numeric("n", "N", min=0, max=100, value=20)
+    ui.input_slider("m", "M", min=0, max=100, value=50)
+    ui.input_selectize("letter", "Letter", choices="A B C D E".split())
 
 ui.h2("Output")
 
@@ -18,10 +20,4 @@ def txt():
     return f"n*2 is {input.n() * 2}"
 
 
-ui.markdown(
-    """
-**AI-generated filler text.** In the world of exotic fruits, the durian stands out with its spiky exterior and strong odor. Despite its divisive smell, many people are drawn to its rich, creamy texture and unique flavor profile. This tropical fruit is often referred to as the "king of fruits" in various Southeast Asian countries.
-
-Durians are known for their large size and thorn-covered husk, which requires careful handling. The flesh inside can vary in color from pale yellow to deep orange, with a custard-like consistency that melts in your mouth. Some describe its taste as a mix of sweet, savory, and creamy, while others find it overpowering and pungent.
-"""
-)
+ui.markdown(filler_text)
