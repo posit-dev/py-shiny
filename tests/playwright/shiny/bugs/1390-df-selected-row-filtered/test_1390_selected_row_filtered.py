3062ba91f6706d7c45976fcb76c091f0b47c6808
Barret Schloerke <barret@posit.co>
2024-05-22 16:39:21 -0400

    Make sure selection is updated on new selection update (when filtered)

--- tests/playwright/shiny/bugs/1390-df-selected-row-filtered/test_1390_selected_row_filtered.py
+++ tests/playwright/shiny/bugs/1390-df-selected-row-filtered/test_1390_selected_row_filtered.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import platform
 
 from conftest import ShinyAppProc
-from controls import OutputDataFrame, OutputCode
+from controls import OutputCode, OutputDataFrame
 from playwright.sync_api import Page, expect
 
 
@@ -25,11 +25,11 @@ def test_row_selection(page: Page, local_app: ShinyAppProc) -> None:
     selected_rows.expect_value("()")
 
     # Select row
-    my_df.select_rows([5])
+    my_df.select_rows([5, 7])
     my_df.expect_n_row(10)
-    expect_selected_count(my_df, 1)
-    selected_df.expect_n_row(1)
-    selected_rows.expect_value("(5,)")
+    expect_selected_count(my_df, 2)
+    selected_df.expect_n_row(2)
+    selected_rows.expect_value("(5, 7)")
 
     # # Filter to different row
     # Currently this causes an error
@@ -43,33 +43,45 @@ def test_row_selection(page: Page, local_app: ShinyAppProc) -> None:
     # Confirm the original data frame returns
     my_df.expect_n_row(10)
     # Confirm the previous row is still selected as no new selection has been made
-    expect_selected_count(my_df, 1)
-    selected_df.expect_n_row(1)
-    selected_rows.expect_value("(5,)")
+    expect_selected_count(my_df, 2)
+    selected_df.expect_n_row(2)
+    selected_rows.expect_value("(5, 7)")
 
     # Filter to non selected row
     # Currently this causes an error
-    my_df.set_column_filter(0, text="7")
+    my_df.set_column_filter(0, text="8")
     my_df.expect_n_row(1)
     expect_selected_count(my_df, 0)
     selected_df.expect_n_row(0)
     selected_rows.expect_value("()")
 
-    # Path forward: subset the selected rows
-
     # Remove the filter
     # Confirm the original data frame returns
     my_df.set_column_filter(0, text="")
     my_df.expect_n_row(10)
+    expect_selected_count(my_df, 2)
+    selected_df.expect_n_row(2)
+    selected_rows.expect_value("(5, 7)")
+
+    # Update selection with new selection while under a filter
+    my_df.set_column_filter(0, text="8")
+    my_df.expect_n_row(1)
+    expect_selected_count(my_df, 0)
+    selected_df.expect_n_row(0)
+    selected_rows.expect_value("()")
+    my_df.select_rows([0])
+    my_df.set_column_filter(0, text="")
+    my_df.expect_n_row(10)
     expect_selected_count(my_df, 1)
     selected_df.expect_n_row(1)
-    selected_rows.expect_value("(5,)")
+    selected_rows.expect_value("(8,)")
 
     # Remove selection
     modifier = "Meta" if platform.system() == "Darwin" else "Control"
-    my_df.cell_locator(row=5, col=0).click(
-        modifiers=[modifier]
-    )  # TODO-karan-test: Support unselect row method
+    for row in (8,):
+        my_df.cell_locator(row=row, col=0).click(
+            modifiers=[modifier]
+        )  # TODO-karan-test: Support unselect row method
     expect_selected_count(my_df, 0)
     selected_df.expect_n_row(0)
     selected_rows.expect_value("()")
