74a315297da7586e18e753e5f22a439236db0c2d
Barret Schloerke <barret@posit.co>
2024-06-04 17:20:02 -0400

    Make sure playwright is installed for `shiny.playwright` with suggestion to install `pytest-playwright`

--- shiny/playwright/__init__.py
+++ shiny/playwright/__init__.py
@@ -1,4 +1,16 @@
-from ._expect import expect_to_change
-from . import controls
+try:
+    import playwright  # noqa: F401 # pyright: ignore[reportUnusedImport, reportMissingTypeStubs]
+except ImportError:
+    raise ImportError(
+        "The shiny.playwright module requires the playwright package to be installed."
+        " Please install it with this command:"
+        "\n\n    pip install playwright"
+        "\n\n",
+        "If you are using pytest to test your code,"
+        " you can install the pytest-playwright shim package with this command:",
+        "\n\n    pip install pytest-playwright",
+    )
 
-__all__ = ["expect_to_change", "controls"]
+from . import controls, expect
+
+__all__ = ["expect", "controls"]
