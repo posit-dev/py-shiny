55a8ab7be512baca480eb167de518160180ab86a
Karan Gathani <karan.gathani@posit.co>
2024-06-06 11:53:30 -0700

    Check for playwright, pytest and pytest-playwright when users import shiny.playwright

--- shiny/playwright/__init__.py
+++ shiny/playwright/__init__.py
@@ -5,12 +5,21 @@ except ImportError:
         "The shiny.playwright module requires the playwright package to be installed."
         " Please install it with this command:"
         "\n\n    pip install playwright"
-        "\n\n",
-        "If you are using pytest to test your code,"
-        " you can install the pytest-playwright shim package with this command:",
-        "\n\n    pip install pytest-playwright",
     )
+try:
+    import pytest  # noqa: F401 # pyright: ignore[reportUnusedImport, reportMissingTypeStubs]
 
+    try:
+        import pytest_playwright  # noqa: F401 # pyright: ignore[reportUnusedImport, reportMissingTypeStubs]
+
+    except ImportError:
+        raise ImportError(
+            "If you are using pytest to test your app,"
+            " you can install the pytest-playwright shim package with this command:",
+            "\n\n    pip install pytest-playwright",
+        )
+except ImportError:
+    pass
 from . import controls, expect
 
 __all__ = ["expect", "controls"]
