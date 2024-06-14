bd64f3cdbbfdf9b4f541f6d6144e57b1b3418c41
Winston Chang <winston@posit.co>
2024-06-13 20:09:09 -0500

    Code cleanup

--- shiny/express/app.py
+++ shiny/express/app.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-import importlib
+from pathlib import Path
+
+from ._run import wrap_express_app
+from ._utils import unescape_from_var_name
 
 
 # If someone requests shiny.express.app:_2f_path_2f_to_2f_app_2e_py, then we will call
 # wrap_express_app(Path("/path/to/app.py")) and return the result.
 def __getattr__(name: str) -> object:
-    app_module = importlib.import_module("shiny_express_app" + name)
-    return app_module.app
+    name = unescape_from_var_name(name)
+    return wrap_express_app(Path(name))
