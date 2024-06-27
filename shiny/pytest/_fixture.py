4b78b17d1320f0a18a61d787384f1d4ebfad7c52
Karan Gathani <karan.gathani@posit.co>
2024-06-27 15:23:45 -0700

    dynamically determine the path to the shiny app

--- shiny/pytest/_fixture.py
+++ shiny/pytest/_fixture.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from pathlib import PurePath
+import inspect
+from pathlib import Path, PurePath
 from typing import Literal, Union
 
 import pytest
 
 from .._docstring import no_example
 from ..run._run import shiny_app_gen
+from ..types import MISSING, MISSING_TYPE
 
 __all__ = (
     "create_app_fixture",
@@ -21,6 +23,8 @@ ScopeName = Literal["session", "package", "module", "class", "function"]
 def create_app_fixture(
     app: Union[PurePath, str],
     scope: ScopeName = "module",
+    # *,
+    # start_dir: Union[PurePath, str, MISSING_TYPE, None] = MISSING,
 ):
     """
     Create a fixture for a local Shiny app directory.
@@ -29,13 +33,22 @@ def create_app_fixture(
     ----------
     app
         The path to the Shiny app file.
+
+        If `app` is a `Path` or `PurePath` instance and `.is_file()` returns `True`, then this value will be used directly.
+        Note, file paths will be checked from where `pytest` was called, not from the calling function.
+
+        Otherwise, all `app` paths will be considered to be relative paths from where the test function was collected.
+
+        To be sure that your app path is always relative, supply a `str` value.
     scope
         The scope of the fixture.
     """
 
     @pytest.fixture(scope=scope)
-    def fixture_func():
-        sa_gen = shiny_app_gen(app)
+    def fixture_func(request: pytest.FixtureRequest):
+        app_purepath_exists = isinstance(app, PurePath) and Path(app).is_file()
+        app_path = app if app_purepath_exists else request.path.parent / app
+        sa_gen = shiny_app_gen(app_path)
         yield next(sa_gen)
 
     return fixture_func
