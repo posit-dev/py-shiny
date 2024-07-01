1d7114b8432c0cf66d196ebbfb01a9888765d6a0
Barret Schloerke <barret@posit.co>
2024-07-01 15:29:28 -0400

    Fix broken lints

--- shiny/render/_data_frame_utils/_styles.py
+++ shiny/render/_data_frame_utils/_styles.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING, Callable, Literal, NotRequired, Required
 
 from ..._typing_extensions import TypedDict
 from ...types import Jsonifiable, ListOrTuple
-from ._utils import to_bool_tuple_or_none, to_int_tuple_or_none, to_intstr_tuple_or_none
 
 if TYPE_CHECKING:
     import pandas as pd
@@ -36,7 +35,9 @@ StyleInfoBody = TypedDict(
     {
         "location": Required[Literal["body"]],
         "rows": NotRequired[int | ListOrTuple[int] | ListOrTuple[bool] | None],
-        "cols": NotRequired[int | str | ListOrTuple[str | int] | None],
+        "cols": NotRequired[
+            str | int | ListOrTuple[str] | ListOrTuple[int] | ListOrTuple[bool] | None
+        ],
         "style": NotRequired[dict[str, Jsonifiable] | None],
         "class": NotRequired[str | None],
     },
@@ -44,7 +45,7 @@ StyleInfoBody = TypedDict(
 StyleInfo = StyleInfoBody
 
 BrowserStyleInfoBody = TypedDict(
-    "BrowserStyleInfo",
+    "BrowserStyleInfoBody",
     {
         "location": Required[Literal["body"]],
         "rows": Required[tuple[int, ...] | None],
@@ -56,78 +57,26 @@ BrowserStyleInfoBody = TypedDict(
 BrowserStyleInfo = BrowserStyleInfoBody
 
 
-def columns_to_browser_columns(
-    columns: ListOrTuple[int | str] | None,
-    browser_column_names: ListOrTuple[str],
-) -> tuple[int, ...] | None:
-
-    if columns is None:
-        return None
-
-    ret: list[int] = []
-    browser_column_names_set = set(browser_column_names)
-    ncol = len(browser_column_names)
-
-    for col in columns:
-        if isinstance(col, str):
-            if col not in browser_column_names_set:
-                raise ValueError(
-                    f"Style column '{col}' not found in data frame column names"
-                )
-            ret.append(browser_column_names.index(col))
-        else:
-            if col < 0:
-                raise ValueError(f"Style column index `{col}` must be non-negative")
-            if col >= ncol:
-                raise ValueError(
-                    f"Style column index `{col}` out of range (max: {ncol - 1})"
-                )
-            ret.append(col)
-
-    return tuple(ret)
-
-
 def style_info_to_browser_style_info(
     info: StyleInfo,
     *,
+    nrow: int,
     browser_column_names: ListOrTuple[str],
 ) -> BrowserStyleInfo:
     if not isinstance(info, dict):
-        raise ValueError("`StyleInfo` objects must be a dictionary. Received: ", info)
+        raise TypeError("`StyleInfo` objects must be a dictionary. Received: ", info)
 
     location = info.get("location", None)
     if location != "body":
         raise ValueError(f"Unsupported `StyleInfo` location: {location}")
 
-    rows = info.get("rows", None)
-    if rows is not None:
-        if isinstance(rows, (bool, int)):
-            rows = (rows,)
-        if not isinstance(rows, (list, tuple)):
-            raise ValueError(
-                "`StyleInfo` `rows` value must be a list, tuple, or scalar"
-            )
-        if len(rows) > 0:
-            if isinstance(rows[0], bool):
-                # Validate that all elements are bools
-                rows = to_bool_tuple_or_none(
-                    rows  # pyright: ignore[reportGeneralTypeIssues]
-                )
-            elif isinstance(rows[0], int):
-                # Validate that all elements are ints
-                rows = to_int_tuple_or_none(
-                    rows  # pyright: ignore[reportGeneralTypeIssues]
-                )
-
-    rows = to_int_tuple_or_none(info.get("rows", None))
-
-    cols = to_intstr_tuple_or_none(info.get("cols", None))
-    cols = columns_to_browser_columns(cols, browser_column_names)
+    rows = style_info_rows(info, nrow=nrow)
+    cols = style_info_cols(info, browser_column_names=browser_column_names)
 
     style = info.get("style", None)
     if style is not None:
         if isinstance(style, str):
-            raise ValueError(
+            raise TypeError(
                 "`StyleInfo` `style` value must be a dictionary of key-value pairs, not a style string"
             )
         assert isinstance(style, dict)
@@ -146,6 +95,114 @@ def style_info_to_browser_style_info(
         "style": style,
         "class": class_,
     }
+
+
+def style_info_cols(
+    info: StyleInfo,
+    *,
+    browser_column_names: ListOrTuple[str],
+) -> tuple[int, ...] | None:
+    cols = info.get("cols", None)
+    if cols is None:
+        return None
+    if isinstance(cols, (str, int)):
+        cols = (cols,)
+    if not isinstance(cols, (list, tuple)):
+        raise TypeError(
+            "`StyleInfo` `cols` value must be a list, tuple, int, or string"
+        )
+
+    # cols_tup = tuple(cols)
+    if len(cols) == 0:
+        return ()
+
+    if isinstance(cols[0], bool):
+        if len(cols) != len(browser_column_names):
+            raise ValueError(
+                f"Style column boolean list must have the same length as the data frame column names. Expected {len(browser_column_names)}, got {len(cols)}"
+            )
+        return tuple(i for i, col in enumerate(cols) if col)
+
+    if isinstance(cols[0], str):
+        ret: list[int] = []
+        col_names = set(browser_column_names)
+        for col in cols:
+            assert isinstance(
+                col, str
+            ), "All elements of `StyleInfo` `cols` must be of the same type: str, int, or bool."
+            if col not in col_names:
+                raise ValueError(
+                    f"`StyleInfo` `cols` value '{col}' not found in data frame column names"
+                )
+            ret.append(browser_column_names.index(col))
+        return tuple(ret)
+    elif isinstance(cols[0], int):
+        ret: list[int] = []
+        for col in cols:
+            assert isinstance(
+                col, int
+            ), "All elements of `StyleInfo` `cols` must be of the same type: str, int, or bool."
+            if col < 0:
+                raise ValueError(f"Style column index `{col}` must be non-negative")
+            if col >= len(browser_column_names):
+                raise ValueError(
+                    f"Style column index `{col}` out of range (max: {len(browser_column_names) - 1})"
+                )
+            ret.append(col)
+        return tuple(ret)
+    elif isinstance(cols[0], bool):
+        if all(isinstance(col, bool) for col in cols):
+            return tuple(i for i, val in enumerate(cols) if val)
+        raise TypeError(
+            "All elements of `StyleInfo` `cols` must be of the same type: str, int, or bool."
+        )
+    else:
+        raise TypeError(
+            "All elements of `StyleInfo` `cols` must be of the same type: str, int, or bool."
+        )
+
+
+def style_info_rows(
+    info: StyleInfo,
+    *,
+    nrow: int,
+) -> None | tuple[int, ...]:
+    rows = info.get("rows", None)
+    if rows is None:
+        return None
+    if isinstance(rows, (bool, int)):
+        return (rows,)
+    if not isinstance(rows, (list, tuple)):
+        raise TypeError("`StyleInfo` `rows` value must be a list, tuple, int or string")
+
+    rows_tup = tuple(rows)
+    if len(rows_tup) == 0:
+        return rows_tup
+
+    if isinstance(rows_tup[0], bool):
+        assert (
+            len(rows_tup) == nrow
+        ), "Length of `StyleInfo` `rows` must match the number of rows in the data frame when `rows` is a boolean list / tuple."
+        if all(isinstance(row, bool) for row in rows_tup):
+            # Turn into a tuple of indices
+            ret = tuple(i for i, val in enumerate(rows_tup) if val)
+            return ret
+
+        raise TypeError(
+            "All elements of `StyleInfo` `rows` must be of the same type: bool or int."
+        )
+
+    elif isinstance(rows_tup[0], int):
+        if all(isinstance(row, int) for row in rows_tup):
+            return rows_tup
+
+        raise TypeError(
+            "All elements of `StyleInfo` `rows` must be of the same type: bool or int."
+        )
+    else:
+        raise TypeError(
+            "`StyleInfo` `rows` value must be a list or tuple containing ints, bools, or str values."
+        )
 
 
 def as_style_infos(
@@ -181,10 +238,11 @@ def as_browser_style_infos(
 
     if not isinstance(style_infos, list):
         style_infos = [style_infos]
-
+    nrow = data.shape[0]
     return [
         style_info_to_browser_style_info(
             info,
+            nrow=nrow,
             browser_column_names=browser_column_names,
         )
         for info in style_infos
