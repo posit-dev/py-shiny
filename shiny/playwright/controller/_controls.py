0dc6a3f84ff620b06dda214f64171ee7d58da4fd
Karan Gathani <karan.gathani@posit.co>
2024-07-11 14:47:32 +0530

    Unskip selectize test

--- shiny/playwright/controller/_controls.py
+++ shiny/playwright/controller/_controls.py
@@ -1032,15 +1032,188 @@ class InputSelect(_InputSelectBase):
         )
 
 
-class InputSelectize(_InputSelectBase):
+class InputSelectize(
+    _UiWithLabel,
+    _UiBase,
+):
     """Controller for :func:`shiny.ui.input_selectize`."""
 
     def __init__(self, page: Page, id: str) -> None:
-        super().__init__(
-            page,
-            id=id,
-            select_class=".selectized",
+        super().__init__(page, id=id, loc=f"#{id} + .selectize-control")
+        # self.loc_something = self.loc.locator(
+        #     "> .selectize-dropdown > .selectize-dropdown-content"
+        # )
+        self.loc_items_group = self.loc.locator(
+            "> .selectize-dropdown > .selectize-dropdown-content > div.optgroup"
         )
+        self.loc_choices = self.loc_items_group.locator("> .option")
+        self.loc_selected = self.loc.locator("> div.selectize-input.items > .item")
+        self.loc_choice_groups = self.loc_choices.locator("> .optgroup")
+
+    def set(
+        self,
+        selected: str | list[str],
+        *,
+        timeout: Timeout = None,
+    ) -> None:
+        """
+        Sets the selected option(s) of the input selectize.
+
+        Parameters
+        ----------
+        selected
+            The value(s) of the selected option(s).
+        timeout
+            The maximum time to wait for the selection to be set. Defaults to `None`.
+        """
+        if isinstance(selected, str):
+            selected = [selected]
+        self.loc.click()
+        for value in selected:
+            self.loc_items_group.locator(f"> .option:has-text('{value}')").click(
+                timeout=timeout
+            )
+        self.loc.press("Escape")
+
+    def expect_choices(
+        self,
+        # TODO-future; support patterns?
+        choices: ListPatternOrStr,
+        *,
+        timeout: Timeout = None,
+    ) -> None:
+        """
+        Expect the available options of the input selectize to be an exact match.
+
+        Parameters
+        ----------
+        choices
+            The expected choices of the input select.
+        timeout
+            The maximum time to wait for the expectation to be fulfilled. Defaults to `None`.
+        """
+        self.loc.click()
+        self.loc.press("Escape")
+        # Playwright doesn't like lists of size 0. Instead, check for empty locator
+        if len(choices) == 0:
+            playwright_expect(self.loc_choices).to_have_count(0, timeout=timeout)
+            return
+
+        _MultipleDomItems.expect_locator_values_in_list(
+            page=self.page,
+            loc_container=self.loc,
+            el_type=self.page.locator(
+                "> .selectize-dropdown > .selectize-dropdown-content > div.optgroup > .option"
+            ),
+            arr_name="choices",
+            arr=choices,
+            key="data-value",
+            timeout=timeout,
+        )
+
+    def expect_selected(
+        self,
+        value: ListPatternOrStr,
+        *,
+        timeout: Timeout = None,
+    ) -> None:
+        """
+        Expect the selected option(s) of the input select to be an exact match.
+
+        Parameters
+        ----------
+        value
+            The expected value(s) of the selected option(s).
+        timeout
+            The maximum time to wait for the expectation to be fulfilled. Defaults to `None`.
+        """
+        # Playwright doesn't like lists of size 0
+        if isinstance(value, list) and len(value) == 0:
+            playwright_expect(self.loc_selected).to_have_count(0, timeout=timeout)
+            return
+
+        _MultipleDomItems.expect_locator_values_in_list(
+            page=self.page,
+            loc_container=self.loc,
+            el_type=self.page.locator("> div.selectize-input.items > .item"),
+            arr_name="value",
+            arr=value,
+            key="data-value",
+        )
+
+    def expect_choice_groups(
+        self,
+        # TODO-future; support patterns?
+        choice_groups: ListPatternOrStr,
+        *,
+        timeout: Timeout = None,
+    ) -> None:
+        """
+        Expect the choice groups of the input select to be an exact match.
+
+        Parameters
+        ----------
+        choice_groups
+            The expected choice groups of the input select.
+        timeout
+            The maximum time to wait for the expectation to be fulfilled. Defaults to `None`.
+        """
+
+        # Playwright doesn't like lists of size 0. Instead, use `None`
+        if len(choice_groups) == 0:
+            playwright_expect(self.loc_choice_groups).to_have_count(0, timeout=timeout)
+            return
+
+        _MultipleDomItems.expect_locator_values_in_list(
+            page=self.page,
+            loc_container=self.loc,
+            el_type=self.page.locator(
+                "> .selectize-dropdown > .selectize-dropdown-content > div.optgroup"
+            ),
+            arr_name="choice_groups",
+            arr=choice_groups,
+            timeout=timeout,
+            key="data-group",
+        )
+
+    def expect_choice_labels(
+        self,
+        value: ListPatternOrStr,
+        *,
+        timeout: Timeout = None,
+    ) -> None:
+        """
+        Expect the choice labels of the input selectize to be an exact match.
+
+        Parameters
+        ----------
+        value
+            The expected choice labels of the input select.
+        timeout
+            The maximum time to wait for the expectation to be fulfilled. Defaults to `None`.
+        """
+        # Playwright doesn't like lists of size 0. Instead, use `None`
+        if len(value) == 0:
+            playwright_expect(self.loc_choices).to_have_count(0, timeout=timeout)
+            return
+        playwright_expect(self.loc_choices).to_have_text(value, timeout=timeout)
+
+    # multiple: bool = False,
+    def expect_multiple(self, value: bool, *, timeout: Timeout = None) -> None:
+        """
+        Expect the input selectize to allow multiple selections.
+
+        Parameters
+        ----------
+        value
+            Whether the input select allows multiple selections.
+        timeout
+            The maximum time to wait for the expectation to be fulfilled. Defaults to `None`.
+        """
+        if value:
+            playwright_expect(self.loc).to_have_class(re.compile(r"multi"))
+        else:
+            playwright_expect(self.loc).not_to_have_class(re.compile(r"multi"))
 
 
 class _InputActionBase(_UiBase):
--- tests/playwright/shiny/inputs/test_input_selectize.py
+++ tests/playwright/shiny/inputs/test_input_selectize.py
@@ -15,30 +15,24 @@ def test_input_selectize_kitchen(page: Page, app: ShinyAppProc) -> None:
     expect(state.loc_label).to_have_text("Choose a state:")
     state.expect_label("Choose a state:")
 
-    # TODO: This test was being run against input_select, not input_selectize
-    # and none of these expectations pass. We need to add additional test methods
-    # for the InputSelectize class to test this behaviour.
-    # https://github.com/posit-dev/py-shiny/issues/1100
+    state.expect_choices(["NY", "NJ", "CT", "WA", "OR", "CA", "MN", "WI", "IA"])
+    state.expect_choice_labels(
+        [
+            "New York",
+            "New Jersey",
+            "Connecticut",
+            "Washington",
+            "Oregon",
+            "California",
+            "Minnesota",
+            "Wisconsin",
+            "Iowa",
+        ]
+    )
+    state.expect_choice_groups(["East Coast", "West Coast", "Midwest"])
 
-    # state.expect_choices(["NY", "NJ", "CT", "WA", "OR", "CA", "MN", "WI", "IA"])
-    # state.expect_choice_labels(
-    #     [
-    #         "New York",
-    #         "New Jersey",
-    #         "Connecticut",
-    #         "Washington",
-    #         "Oregon",
-    #         "California",
-    #         "Minnesota",
-    #         "Wisconsin",
-    #         "Iowa",
-    #     ]
-    # state.expect_choice_groups(["East Coast", "West Coast", "Midwest"])
+    state.expect_multiple(True)
 
-    # state.expect_multiple(True)
+    state.set(["Iowa", "California"])
 
-    # state.expect_width(None)
-
-    # state.set("Iowa")
-
-    # state.expect_selected("IA")
+    state.expect_selected(["IA", "CA"])
