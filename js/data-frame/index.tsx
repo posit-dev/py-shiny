78a1061cb7f478a0c3f010f3ec73c1e63c074814
Barret Schloerke <barret@posit.co>
2024-07-12 12:32:15 -0400

    Update param name

--- js/data-frame/index.tsx
+++ js/data-frame/index.tsx
@@ -281,7 +281,7 @@ const ShinyDataGrid: FC<ShinyDataGridProps<unknown>> = ({
     paddingStart: theadRef.current?.clientHeight ?? 0,
     // In response to https://github.com/posit-dev/py-shiny/pull/538/files#r1228352446
     // (the default scrollingDelay is 150)
-    scrollingDelay: 10,
+    isScrollingResetDelay: 10,
   });
 
   // Reset scroll when dataset changes
