201220987bf51361ad1262292e41716a82cc2be9
Garrick Aden-Buie <garrick@posit.co>
2024-07-16 16:06:31 -0400

    `shiny.experimental`: Remove deprecated functions (#1540)

--- shiny/experimental/api-examples/card_title/app.py
+++ shiny/experimental/api-examples/card_title/app.py
@@ -3,13 +3,13 @@ from shiny import App, ui
 
 app_ui = ui.page_fluid(
     x.ui.card(
-        x.ui.card_header("This is the header"),
+        ui.card_header("This is the header"),
         x.ui.card_body(
             x.ui.card_title("This is the title"),
             ui.p("This is the body."),
             ui.p("This is still the body."),
         ),
-        x.ui.card_footer("This is the footer"),
+        ui.card_footer("This is the footer"),
         full_screen=True,
     )
 )
