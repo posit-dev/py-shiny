43b4a27690a8394ab6a01f5c2cca636a4828057b
Barret Schloerke <barret@posit.co>
2024-07-24 15:30:56 -0400

    Update test_chat_append_user_msg.py

--- tests/playwright/shiny/components/chat/append_user_msg/test_chat_append_user_msg.py
+++ tests/playwright/shiny/components/chat/append_user_msg/test_chat_append_user_msg.py
@@ -14,7 +14,7 @@ def test_validate_chat_append_user_message(
 
     # Verify starting state
     expect(chat.loc).to_be_visible()
-    chat.expect_latest_message("A user message")
+    chat.expect_latest_message("A user message", timeout=30 * 1000)
 
     # Verify that the message state is as expected
     message_state = controller.OutputCode(page, "message_state")
