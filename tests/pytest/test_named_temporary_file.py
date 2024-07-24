8fac2e5fd86e735396a0a3752254968c5c4d6638
Barret Schloerke <barret@posit.co>
2024-07-24 10:30:50 -0400

    Assert all lines are seen and files are processed

--- tests/pytest/test_named_temporary_file.py
+++ tests/pytest/test_named_temporary_file.py
@@ -38,19 +38,21 @@ def test_named_temporary_file_is_not_used():
     """
 
     root_here = Path(__file__).parent.parent.parent
+    shiny_files = glob.glob(str(root_here / "shiny" / "**" / "*.py"), recursive=True)
+    tests_files = glob.glob(str(root_here / "tests" / "**" / "*.py"), recursive=True)
 
-    dir_content = [
-        *glob.glob("shiny/**/*.py", root_dir=root_here, recursive=True),
-        *glob.glob("tests/**/*.py", root_dir=root_here, recursive=True),
-    ]
+    assert len(shiny_files) > 0
+    assert len(tests_files) > 0
+
+    all_files = [*shiny_files, *tests_files]
 
     search_string = "NamedTemporaryFile("
 
-    bad_entries: list[tuple[str, int, str]] = []
+    bad_entries: list[tuple[Path, int, str]] = []
 
     # For every python file...
-    for p in dir_content:
-        path = Path(root_here, p)
+    for path in all_files:
+        path = Path(path)
         # Skip if dir
         if path.is_dir():
             continue
@@ -69,15 +71,25 @@ def test_named_temporary_file_is_not_used():
 
             # Split file contents by line
             lines = txt.split("\n")
-            known_lines = known_entries.get(str(p), set())
+            rel_path = path.relative_to(root_here)
+            known_lines = known_entries.get(str(rel_path), set())
+            seen_lines: set[str] = set()
 
             # If the search string is in the line
             # and the line is not in the known lines,
             # add it to the bad entries
             for i, line in enumerate(lines):
                 line = line.strip()
-                if search_string in line and (line not in known_lines):
-                    bad_entries.append((p, i + 1, line))
+                if search_string in line:
+                    seen_lines.add(line)
+                    if line not in known_lines:
+                        bad_entries.append((rel_path, i + 1, line))
+
+            if (len(known_lines) > 0) and (len(seen_lines) != len(known_lines)):
+                raise ValueError(
+                    f"Lines not found in {rel_path}: {known_lines - seen_lines}"
+                    "\nPlease remove them from the known_entries dictionary."
+                )
 
     assert (
         len(bad_entries) == 0
