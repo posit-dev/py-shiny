1d5a5c652c9e54acf1634a0732e6fdcf38ef9f71
Barret Schloerke <barret@posit.co>
2024-07-25 17:31:44 -0400

    Remove custom page object with browser console message tracking

--- tests/playwright/conftest.py
+++ tests/playwright/conftest.py
@@ -30,68 +30,6 @@ class ConsoleMessageInfo(TypedDict):
     location: SourceLocation
 
 
-@pytest.fixture
-def page_old(
-    request: pytest.FixtureRequest,
-    browser: BrowserContext,
-    is_chromium: bool,
-) -> Generator[Page, None, None]:
-    """
-    Create a new page.
-
-    Enhancements
-    * Set the size of the page to 1920 x 1080 (1080p) for each test.
-    * Add a console listener to the browser context and create a new page.
-
-    Parameters:
-        browser (BrowserContext): The browser context in which to create the new page.
-
-    Returns:
-        Page: The newly created page.
-
-    """
-    page = browser.new_page()
-
-    # Reset screen size to 1080p
-    page.set_viewport_size({"width": 1920, "height": 1080})
-
-    console_msgs: list[ConsoleMessageInfo] = []
-
-    def on_console_msg(msg: ConsoleMessage) -> None:
-        # Do not report missing favicon errors
-        if msg.location["url"].endswith("favicon.ico"):
-            return
-        if msg.type == "warning" and msg.text.startswith("DEPRECATED:"):
-            return
-        # console_msgs.append(msg.text)
-        console_msgs.append(
-            {
-                "type": msg.type,
-                "text": msg.text,
-                "location": msg.location,
-            }
-        )
-
-    # Only record console messages in Chromium
-    # GHA is running out of memory
-    if is_chromium:
-        page.on("console", on_console_msg)
-
-    yield page
-
-    if is_chromium:
-        page.remove_listener("console", on_console_msg)
-
-    if request.session.testsfailed:
-        if len(console_msgs) > 0:
-            print("+++++++++ Browser console log ++++++++")
-            for msg in console_msgs:
-                print(msg)
-            print("+++++++++ / Browser console log ++++++++")
-        else:
-            print("(No browser console messages captured.)")
-
-
 def create_example_fixture(
     example_name: str,
     example_file: str = "app.py",
