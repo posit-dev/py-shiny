eccf4a5ecb7247a5fb1e7465bc2c02c9c6669d81
Karan Gathani <karan.gathani@posit.co>
2024-07-30 19:11:46 +0530

    remove extra ui.nav_spacer

--- shiny/api-examples/nav_spacer/app-express.py
+++ shiny/api-examples/nav_spacer/app-express.py
@@ -4,7 +4,6 @@ with ui.navset_underline():
     with ui.nav_panel("Tab 1"):
         "Tab 1 content"
     ui.nav_spacer()
-    ui.nav_spacer()
     with ui.nav_panel("Tab 2"):
         "Tab 2 content"
     with ui.nav_panel("Tab 3"):
