4aadf36fb9b6fb9d7ceacf8c3e0c7c5f69b158d7
Karan Gathani <karan.gathani@posit.co>
2024-10-10 13:23:19 -0700

    iterate over backends

--- tests/playwright/shiny/components/data_frame/data_type/test_df_data_type.py
+++ tests/playwright/shiny/components/data_frame/data_type/test_df_data_type.py
@@ -5,66 +5,29 @@ from playwright.sync_api import Page
 from shiny.playwright import controller
 from shiny.run import ShinyAppProc
 
+backends = [
+    {"name": "pandas", "prefix": "pd", "df_original": "pd_df_original", "selected_row": "selected_pandas_row"},
+    {"name": "narwhals", "prefix": "nw", "df_original": "nw_df_original", "selected_row": "selected_nw_row"},
+    {"name": "pyarrow", "prefix": "pa", "df_original": "pa_df_original", "selected_row": "selected_pa_row"},
+    {"name": "polars", "prefix": "pl", "df_original": "pl_df_original", "selected_row": "selected_pl_row"},
+    {"name": "modin", "prefix": "mpd", "df_original": "mpd_df_original", "selected_row": "selected_mpd_row"},
+]
+
 
 def test_data_frame_data_type(
     page: Page,
     local_app: ShinyAppProc,
 ) -> None:
     page.goto(local_app.url)
+    # Iterate over the backends
+    for backend in backends:
+        # Perform output code tests
+        controller.OutputCode(page, f"{backend['prefix']}_type").expect_value(re.compile(backend["name"]))
+        controller.OutputCode(page, f"{backend['prefix']}_data").expect_value(re.compile(backend["name"]))
+        controller.OutputCode(page, f"{backend['prefix']}_data_view").expect_value(re.compile(backend["name"]))
+        controller.OutputCode(page, f"{backend['prefix']}_data_view_selected").expect_value(re.compile(backend["name"]))
 
-    #TODO-karan: Use for loop for iterating over the data types
-    # pandas tests
-    controller.OutputCode(page, "pd_type").expect_value(re.compile(r"pandas"))
-    controller.OutputCode(page, "pd_data").expect_value(re.compile(r"pandas"))
-    controller.OutputCode(page, "pd_data_view").expect_value(re.compile(r"pandas"))
-    controller.OutputCode(page, "pd_data_view_selected").expect_value(
-        re.compile(r"pandas")
-    )
-    # controller.OutputCode(page, "selected_pandas_row").expect_value(
-    #     re.compile(r"Empty DataFrame")
-    # )
-    controller.OutputDataFrame(page, "selected_pandas_row").expect_column_labels("")
-    controller.OutputDataFrame(page, "pd_df_original").cell_locator(0, 0).click()
-    controller.OutputCode(page, "selected_pandas_row").expect_value(
-        re.compile(r"PAL0708")
-    )
-
-    # narwhals tests
-    controller.OutputCode(page, "nw_type").expect_value(re.compile(r"narwhals"))
-    controller.OutputCode(page, "nw_data").expect_value(re.compile(r"narwhals"))
-    controller.OutputCode(page, "nw_data_view").expect_value(re.compile(r"narwhals"))
-    controller.OutputCode(page, "nw_data_view_selected").expect_value(
-        re.compile(r"narwhals")
-    )
-    controller.OutputDataFrame(page, "nw_df_original").cell_locator(0, 0).click()
-    controller.OutputCode(page, "selected_nw_row").expect_value(re.compile(r"PAL0708"))
-
-    # pyArrow tests
-    controller.OutputCode(page, "pa_type").expect_value(re.compile(r"pyarrow"))
-    controller.OutputCode(page, "pa_data").expect_value(re.compile(r"pyarrow"))
-    controller.OutputCode(page, "pa_data_view").expect_value(re.compile(r"pyarrow"))
-    controller.OutputCode(page, "pa_data_view_selected").expect_value(
-        re.compile(r"pyarrow")
-    )
-    controller.OutputDataFrame(page, "pa_df_original").cell_locator(0, 0).click()
-    controller.OutputCode(page, "selected_pa_row").expect_value(re.compile(r"PAL0708"))
-
-    # polars tests
-    controller.OutputCode(page, "pl_type").expect_value(re.compile(r"polars"))
-    controller.OutputCode(page, "pl_data").expect_value(re.compile(r"polars"))
-    controller.OutputCode(page, "pl_data_view").expect_value(re.compile(r"polars"))
-    controller.OutputCode(page, "pl_data_view_selected").expect_value(
-        re.compile(r"polars")
-    )
-    controller.OutputDataFrame(page, "pl_df_original").cell_locator(0, 0).click()
-    controller.OutputCode(page, "selected_pl_row").expect_value(re.compile(r"PAL0708"))
-
-    # modin tests
-    controller.OutputCode(page, "mpd_type").expect_value(re.compile(r"modin"))
-    controller.OutputCode(page, "mpd_data").expect_value(re.compile(r"modin"))
-    controller.OutputCode(page, "mpd_data_view").expect_value(re.compile(r"modin"))
-    controller.OutputCode(page, "mpd_data_view_selected").expect_value(
-        re.compile(r"modin")
-    )
-    controller.OutputDataFrame(page, "mpd_df_original").cell_locator(0, 0).click()
-    controller.OutputCode(page, "selected_mpd_row").expect_value(re.compile(r"PAL0708"))
+        # Perform output dataframe tests
+        controller.OutputDataFrame(page, backend["selected_row"]).expect_column_labels(["studyName", "Sample Number"])
+        controller.OutputDataFrame(page, backend["df_original"]).select_rows([1])
+        controller.OutputDataFrame(page, backend["selected_row"]).expect_nrow(1)
