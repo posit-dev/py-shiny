2ade16e51c88cb5dcd6d71b02caf90062356c30f
Carson <cpsievert1@gmail.com>
2025-02-10 11:58:13 -0600

    Rework playwright test to be Express; typing workarounds

--- tests/playwright/shiny/session/clientdata/app.py
+++ tests/playwright/shiny/session/clientdata/app.py
@@ -1,31 +1,21 @@
+# pyright: reportUnknownMemberType=false, reportUnknownVariableType=false
 import matplotlib.pyplot as plt
 import numpy as np
 
-from shiny import App, Inputs, Outputs, Session, render, ui
+from shiny.express import input, render, session, ui
 
-app_ui = ui.page_sidebar(
-    ui.sidebar(
-        ui.input_slider("obs", "Number of observations:", min=0, max=1000, value=500),
-        open="closed",
-    ),
-    ui.h3("clientData values"),
-    ui.output_text_verbatim("clientdatatext"),
-    ui.output_plot("myplot"),
-    title="Shiny Client Data",
-)
+with ui.sidebar(open="closed"):
+    ui.input_slider("obs", "Number of observations:", min=0, max=1000, value=500)
 
 
-def server(input: Inputs, output: Outputs, session: Session):
-    @render.text
-    def clientdatatext():
-        cdata = session.clientdata
-        return "\n".join([f"{name} = {cdata[name]()}" for name in reversed(dir(cdata))])
-
-    @render.plot
-    def myplot():
-        plt.figure()
-        plt.hist(np.random.normal(size=input.obs()))
-        plt.title("This is myplot")
+@render.code
+def clientdatatext():
+    cdata = session.clientdata
+    return "\n".join([f"{name} = {cdata[name]()}" for name in reversed(dir(cdata))])
 
 
-app = App(app_ui, server)
+@render.plot
+def myplot():
+    plt.figure()
+    plt.hist(np.random.normal(size=input.obs()))  # type: ignore
+    plt.title("This is myplot")
