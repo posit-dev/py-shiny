40cc6daad011248a831d73776a36f667e25ab143
Carson <cpsievert1@gmail.com>
2025-02-26 14:49:06 -0600

    Undo test_chat changes

--- tests/pytest/test_chat.py
+++ tests/pytest/test_chat.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import sys
 from datetime import datetime
 from typing import Union, cast, get_args, get_origin
 
@@ -384,8 +385,6 @@ def test_as_google_message():
     from google.generativeai.generative_models import (  # pyright: ignore[reportMissingTypeStubs]
         GenerativeModel,
     )
-
-    from shiny.ui._chat_provider_types import as_google_message
 
     generate_content = GenerativeModel.generate_content  # type: ignore
 
