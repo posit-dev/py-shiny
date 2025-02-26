5134b197f234834910de4f722869268482205f0b
Carson <cpsievert1@gmail.com>
2025-02-26 09:54:58 -0600

    Encourage async in response generation

--- shiny/templates/chat/llms/ollama/app.py
+++ shiny/templates/chat/llms/ollama/app.py
@@ -29,5 +29,5 @@ chat.ui()
 # Generate a response when the user submits a message
 @chat.on_user_submit
 async def handle_user_input(user_input: str):
-    response = chat_model.stream(user_input)
+    response = await chat_model.stream_async(user_input)
     await chat.append_message_stream(response)
