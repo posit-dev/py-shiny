b89bc9e9c4d3d3c20fbc3a09f4d7e29f32a3926b
Barret Schloerke <barret@posit.co>
2025-03-04 14:06:50 -0500

    Reduce comments

--- shiny/bookmark/_globals.py
+++ shiny/bookmark/_globals.py
@@ -5,20 +5,12 @@ from typing import Awaitable, Callable, Literal
 
 from ..types import MISSING, MISSING_TYPE
 
-# TODO: Barret: Q: Can we merge how bookmark dirs are saved / loaded?
+# TODO: Barret: Q: Can we merge how bookmark dirs are saved / loaded?... it's the same directory! However, the save will return a possibly new path. Restoring will return an existing path.
 BookmarkStateSaveDir = Callable[[str], Awaitable[Path]]
 BookmarkStateLoadDir = Callable[[str], Awaitable[Path]]
 bookmark_state_save_dir: BookmarkStateSaveDir | MISSING_TYPE = MISSING
 bookmark_state_load_dir: BookmarkStateLoadDir | MISSING_TYPE = MISSING
 
 BookmarkStore = Literal["url", "server", "disable"]
+# TODO: barret - Q: Should we have a `enable_bookmarking(store: BookmarkStore)` function?
 bookmark_store: BookmarkStore = "disable"
-
-
-# def bookmark_store_get() -> BookmarkStore:
-#     return bookmark_store
-
-
-# def bookmark_store_set(value: BookmarkStore) -> None:
-#     global bookmark_store
-#     bookmark_store = value
