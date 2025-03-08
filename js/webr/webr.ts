4c3af9b2141a0220ab1f6eca93f1b7fe2720b1f7
Winston Chang <winston@posit.co>
2025-03-08 12:38:40 -0600

    Stop using WebR Console

--- js/webr/webr.ts
+++ js/webr/webr.ts
@@ -1,26 +1,30 @@
 import { html } from "lit";
 
-import { Console } from "https://webr.r-wasm.org/latest/webr.mjs";
+import { RObject, WebR } from "https://webr.r-wasm.org/latest/webr.mjs";
+import { Message } from "webr/dist/webR/chan/message";
 import { LightElement } from "../utils/_utils";
+
+type CaptureRResult = {
+  result: unknown; // Should be JSONifiable
+  output: {
+    type: string;
+    data: any;
+  }[];
+  images: ImageBitmap[];
+};
 
 const WEBR_COMPONENT_TAG = "shiny-webr-component";
 
 class WebRComponent extends LightElement {
-  webRConsole: Console;
+  // webRConsole: Console;
+  webR: WebR;
+
+  private output!: HTMLElement;
+  private input!: HTMLInputElement;
 
   private outputId: string;
   private inputId: string;
   private runId: string;
-
-  private resolveEvalCode: ((value: string) => void) | null = null;
-
-  private enableCaptureOutput = false;
-  private capturedOutput: string[] = [];
-  private captureOutput(s: string) {
-    if (this.enableCaptureOutput) {
-      this.capturedOutput.push(s);
-    }
-  }
 
   constructor() {
     super();
@@ -28,22 +32,7 @@ class WebRComponent extends LightElement {
     this.inputId = this.id + "_input";
     this.runId = this.id + "_run";
 
-    this.webRConsole = new Console({
-      stdout: (line) => this.appendToOutEl(line + "\n"),
-      stderr: (line) => this.appendToOutEl(line + "\n"),
-      prompt: (p) => {
-        if (this.resolveEvalCode) {
-          this.resolveEvalCode(this.capturedOutput.join("\n"));
-          this.enableCaptureOutput = false;
-
-          // Reset the captured output
-          this.capturedOutput.length = 0;
-          this.resolveEvalCode = null;
-        }
-        this.appendToOutEl(p);
-      },
-    });
-    this.webRConsole.run();
+    this.webR = new WebR();
   }
 
   render() {
@@ -61,16 +50,20 @@ class WebRComponent extends LightElement {
   }
 
   firstUpdated() {
-    const input = document.getElementById(this.inputId) as HTMLInputElement;
+    this.output = document.getElementById(this.outputId) as HTMLElement;
+    this.input = document.getElementById(this.inputId) as HTMLInputElement;
+    this.webR.init().then(() => {
+      this.output.textContent = `webR ${this.webR.version}\n`;
+      this.prompt();
+    });
 
-    const sendInput = () => {
-      this.webRConsole.stdin(input.value);
-      this.appendToOutEl(input.value + "\n");
-      input.value = "";
+    const sendInput = async () => {
+      this.evalInConsole(this.input.value);
+      this.input.value = "";
     };
 
     /* Send input on Enter key */
-    input.addEventListener("keydown", (e) => {
+    this.input.addEventListener("keydown", (e) => {
       if (e.code === "Enter") sendInput();
     });
 
@@ -78,20 +71,54 @@ class WebRComponent extends LightElement {
     runButton.addEventListener("click", sendInput);
   }
 
-  evalR(code: string): Promise<string> {
-    this.webRConsole.stdin(code);
+  handleOutput(msg: Message) {
+    switch (msg.type) {
+      case "stdout":
+        this.appendToOutEl((msg.data as string) + "\n");
+        break;
+      case "stderr":
+        this.appendToOutEl((msg.data as string) + "\n");
+        break;
+      case "prompt":
+        console.log("prompt");
+        this.appendToOutEl(msg.data as string);
+        break;
+      case "canvas":
+        console.log("canvas");
+        break;
+      case "closed":
+        return;
+      default:
+        console.warn(`Unhandled output type for webR: ${msg.type}.`);
+    }
+  }
+
+  async evalInConsole(code: string): Promise<CaptureRResult> {
     this.appendToOutEl(code + "\n");
 
-    this.enableCaptureOutput = true;
-    return new Promise((resolve) => {
-      this.resolveEvalCode = resolve;
+    const shelter = await new this.webR.Shelter();
+    const result = await shelter.captureR(code, {
+      withAutoprint: true,
     });
+    result.output.forEach((msg) => this.handleOutput(msg));
+
+    shelter.purge();
+    this.prompt();
+
+    const newResult = {
+      ...result,
+      result: await result.result.toJs(),
+    };
+
+    return newResult;
+  }
+
+  prompt() {
+    this.appendToOutEl("> ");
   }
 
   private appendToOutEl(line: string) {
-    const outEl = document.getElementById(this.outputId) as HTMLElement;
-    outEl.append(line);
-    this.captureOutput(line);
+    this.output.append(line);
   }
 }
 
@@ -99,6 +126,6 @@ customElements.define(WEBR_COMPONENT_TAG, WebRComponent);
 
 window.Shiny.addCustomMessageHandler("eval_r", async function (message) {
   const el = document.getElementById(message.id) as WebRComponent;
-  const result = await el.evalR(message.code);
+  const result = await el.evalInConsole(message.code);
   window.Shiny.setInputValue!(message.id + "_result", result);
 });
