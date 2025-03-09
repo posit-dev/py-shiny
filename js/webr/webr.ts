716e03d5588b213d33eff8389ff339c7b777135c
Winston Chang <winston@posit.co>
2025-03-08 18:00:28 -0600

    Add plots to webr component

--- js/webr/webr.ts
+++ js/webr/webr.ts
@@ -1,7 +1,8 @@
 import { html } from "lit";
 
-import { RObject, WebR } from "https://webr.r-wasm.org/latest/webr.mjs";
+import { WebR } from "https://webr.r-wasm.org/latest/webr.mjs";
 import { Message } from "webr/dist/webR/chan/message";
+import { WebRDataJs } from "webr/dist/webR/robj";
 import { LightElement } from "../utils/_utils";
 
 type CaptureRResult = {
@@ -14,56 +15,74 @@ type CaptureRResult = {
 };
 
 const WEBR_COMPONENT_TAG = "shiny-webr-component";
+const PLOT_WIDTH = 400;
+const PLOT_HEIGHT = 300;
 
 class WebRComponent extends LightElement {
-  // webRConsole: Console;
   webR: WebR;
 
   private output!: HTMLElement;
   private input!: HTMLInputElement;
+  private plot!: HTMLCanvasElement;
 
   private outputId: string;
   private inputId: string;
   private runId: string;
+  private plotId: string;
 
   constructor() {
     super();
     this.outputId = this.id + "_output";
     this.inputId = this.id + "_input";
     this.runId = this.id + "_run";
+    this.plotId = this.id + "_plot";
 
     this.webR = new WebR();
   }
 
   render() {
     return html`<div>
-      <pre><code id="${this.outputId}">Loading webR, please wait...</code></pre>
-      <input
-        spellcheck="false"
-        autocomplete="off"
-        id="${this.inputId}"
-        type="text"
-        style="width: 100%;"
-      />
-      <button id="${this.runId}">Run</button>
+      <pre style="max-height: 500px"><code id="${this
+        .outputId}">Loading webR, please wait...</code></pre>
+      <div>
+        <input
+          spellcheck="false"
+          autocomplete="off"
+          id="${this.inputId}"
+          type="text"
+          style="width: 100%;"
+        />
+        <button id="${this.runId}">Run</button>
+      </div>
+      <div id="${this.plotId}"></div>
     </div>`;
   }
 
   firstUpdated() {
     this.output = document.getElementById(this.outputId) as HTMLElement;
     this.input = document.getElementById(this.inputId) as HTMLInputElement;
-    this.webR.init().then(() => {
+    this.plot = document.getElementById(this.plotId) as HTMLCanvasElement;
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    (async () => {
+      await this.webR.init();
       this.output.textContent = `webR ${this.webR.version}\n`;
-      this.prompt();
-    });
+      const result = await this.webR.flush();
+      result.forEach((msg) => this.handleOutput(msg));
+
+      // await this.webR.evalR(
+      //   `options(device=webr::canvas(${PLOT_WIDTH * 2}, ${PLOT_HEIGHT * 2}))`
+      // );
+    })();
 
     const sendInput = async () => {
-      this.evalInConsole(this.input.value);
+      await this.evalInConsole(this.input.value);
       this.input.value = "";
     };
 
     /* Send input on Enter key */
     this.input.addEventListener("keydown", (e) => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       if (e.code === "Enter") sendInput();
     });
 
@@ -84,7 +103,7 @@ class WebRComponent extends LightElement {
         this.appendToOutEl(msg.data as string);
         break;
       case "canvas":
-        console.log("canvas");
+        this.handleImage(msg.data.image);
         break;
       case "closed":
         return;
@@ -93,22 +112,51 @@ class WebRComponent extends LightElement {
     }
   }
 
+  handleImage(img: ImageBitmap) {
+    const canvas = document.createElement("canvas");
+    canvas.style.width = `${img.width / 2}px`;
+    canvas.style.height = `${img.height / 2}px`;
+    canvas.style.margin = "2px";
+    canvas.style.border = "1px solid black";
+    canvas.style.borderRadius = "3";
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d")!;
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    this.plot.insertBefore(canvas, this.plot.firstChild);
+  }
+
   async evalInConsole(code: string): Promise<CaptureRResult> {
     this.appendToOutEl(code + "\n");
 
     const shelter = await new this.webR.Shelter();
-    const result = await shelter.captureR(code, {
+    const res = await shelter.captureR(code, {
       withAutoprint: true,
+      captureGraphics: {
+        width: PLOT_WIDTH,
+        height: PLOT_HEIGHT,
+      },
     });
-    result.output.forEach((msg) => this.handleOutput(msg));
+    const moreResults = await this.webR.flush();
+    res.output.forEach((msg) => this.handleOutput(msg));
+    res.images.forEach((img) => this.handleImage(img));
+    console.log(res);
+    console.log(moreResults);
 
-    shelter.purge();
-    this.prompt();
-
-    const newResult = {
-      ...result,
-      result: await result.result.toJs(),
+    let resultJson: WebRDataJs = {
+      type: "null",
     };
+    try {
+      resultJson = await res.result.toJs();
+    } catch (e) {
+      console.warn(e);
+    }
+    const newResult = {
+      ...res,
+      result: resultJson,
+    };
+    await shelter.purge();
+    this.prompt();
 
     return newResult;
   }
@@ -119,6 +167,11 @@ class WebRComponent extends LightElement {
 
   private appendToOutEl(line: string) {
     this.output.append(line);
+    const parent = this.output.parentElement!;
+    parent.scrollTo({
+      top: parent.scrollHeight,
+      behavior: "smooth",
+    });
   }
 }
 
