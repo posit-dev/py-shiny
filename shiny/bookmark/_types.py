7ffe2a60261e3c9c9c2b427e94f0e7a0c641db6b
Barret Schloerke <barret@posit.co>
2025-03-13 10:10:05 -0400

    lints

--- shiny/bookmark/_types.py
+++ shiny/bookmark/_types.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Awaitable, Callable, Literal
+from typing import Awaitable, Callable, Literal, Union
 
 # Q: Can we merge how bookmark dirs are saved / loaded?... it's the same directory! However, the save will return a possibly new path. Restoring will return an existing path.
 # A: No. Keep them separate. The save function may need to create a new directory, while the load function will always return an existing directory.
 
-BookmarkDirFn = Callable[[str], Awaitable[Path]] | Callable[[str], Path]
+BookmarkDirFn = Union[Callable[[str], Awaitable[Path]], Callable[[str], Path]]
 BookmarkDirFnAsync = Callable[[str], Awaitable[Path]]
 
 BookmarkSaveDirFn = BookmarkDirFnAsync
