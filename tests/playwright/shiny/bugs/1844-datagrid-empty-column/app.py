6f05721e0483b0193afb905e7b4bce845ae92968
Laura <erdaradun.gaztea+github@gmail.com>
2025-03-31 11:18:46 +0200

    test: columns with empty names in DataGrid

---
+++ tests/playwright/shiny/bugs/1844-datagrid-empty-column/app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+from shiny import App, Inputs, reactive, render, ui
+
+app_ui = ui.page_fluid(
+    ui.output_data_frame("df1")
+)
+
+
+def server(input: Inputs):
+    df = reactive.Value(pd.DataFrame({
+        "": [1, 2],
+        "A": [4, 5],
+        " ": [7, 8]
+    }))
+
+    @render.data_frame
+    def df1():
+        return render.DataGrid(df())
+
+
+app = App(app_ui, server)
