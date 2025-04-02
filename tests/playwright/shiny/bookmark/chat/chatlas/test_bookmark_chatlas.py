3f6f65f170660ee856aea75703447e87d223d992
Barret Schloerke <barret@posit.co>
2025-04-02 15:45:12 -0400

    Tests

---
+++ tests/playwright/shiny/bookmark/chat/chatlas/test_bookmark_chatlas.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import Page
+
+from shiny.playwright.controller import Chat
+from shiny.run import ShinyAppProc
+
+
+def test_bookmark_chatlas(page: Page, local_app: ShinyAppProc):
+
+    page.goto(local_app.url)
+
+    assert "?" not in page.url
+
+    chat_controller = Chat(page, "chat")
+
+    chat_controller.expect_messages("Welcome!")
+
+    chat_controller.set_user_input("Testing")
+    chat_controller.send_user_input()
+
+    chat_controller.expect_messages("Welcome!\nTesting\nYou said to OpenAI: Testing")
+
+    assert "?" in page.url
+
+    page.reload()
+
+    chat_controller.expect_messages("Welcome!\nTesting\nYou said to OpenAI: Testing")
