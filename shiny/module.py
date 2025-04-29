94a90245bf33c23b9f6ed18e8a80c9629c24466f
Daniel Chen <chendaniely@users.noreply.github.com>
2025-04-28 17:09:40 -0700

    use :func: and :func: to show links

--- shiny/module.py
+++ shiny/module.py
@@ -51,7 +51,7 @@ def ui(fn: Callable[P, R]) -> Callable[Concatenate[str, P], R]:
     See Also
     --------
     * Shiny Modules documentation: <https://shiny.posit.co/py/docs/modules.html>
-    * shiny.module.server
+    * :func:`shiny.module.server`
     """
 
     def wrapper(id: Id, *args: P.args, **kwargs: P.kwargs) -> R:
@@ -91,7 +91,7 @@ def server(
     See Also
     --------
     * Shiny Modules documentation: <https://shiny.posit.co/py/docs/modules.html>
-    * shiny.module.ui
+    * :func:`shiny.module.ui`
     """
     from .session import require_active_session, session_context
 
