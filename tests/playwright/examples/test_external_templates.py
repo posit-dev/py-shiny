1dfd7120b99c6dff9e17cf29b67b409bb99fe725
Karan Gathani <karan.gathani@posit.co>
2025-05-15 11:37:17 -0700

    fix linting issues

--- tests/playwright/examples/test_external_templates.py
+++ tests/playwright/examples/test_external_templates.py
@@ -1,14 +1,17 @@
+from pathlib import Path
+
 import pytest
+from conftest import here_root
 from example_apps import get_apps, reruns, reruns_delay, validate_example
 from playwright.sync_api import Page
-from conftest import here_root
-from pathlib import Path
 
 if not Path(here_root / "py-shiny-templates").exists():
     pytest.skip(
         "./py-shiny-templates dir is not available. Skipping test.",
         allow_module_level=True,
     )
+
+
 @pytest.mark.only_browser("chromium")
 @pytest.mark.flaky(reruns=reruns, reruns_delay=reruns_delay)
 @pytest.mark.parametrize("ex_app_path", get_apps("py-shiny-templates"))
