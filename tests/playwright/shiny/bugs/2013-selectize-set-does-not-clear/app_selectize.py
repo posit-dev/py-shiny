222e962e035c2c7005ef22136e29fffd91b00c12
Karan Gathani <karan.gathani@posit.co>
2025-07-08 14:40:08 +0530

    linting issues

--- tests/playwright/shiny/bugs/2013-selectize-set-does-not-clear/app_selectize.py
+++ tests/playwright/shiny/bugs/2013-selectize-set-does-not-clear/app_selectize.py
@@ -1,10 +1,14 @@
 from shiny import App, ui
 
 app_ui = ui.page_fluid(
-    ui.input_selectize("test_selectize", "Select", ["Choice 1", "Choice 2"], multiple=True)
+    ui.input_selectize(
+        "test_selectize", "Select", ["Choice 1", "Choice 2"], multiple=True
+    )
 )
+
 
 def server(input, output, session):
     pass
+
 
 app = App(app_ui, server)
