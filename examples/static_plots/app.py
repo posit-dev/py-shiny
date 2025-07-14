76ca6536257224d6a27bd5ff0ebd3c6740d4d1ab
Karan Gathani <karan.gathani@posit.co>
2025-07-14 23:44:59 +0530

    Remove stat_smooth from ggplot example
    Eliminated the use and import of stat_smooth in the static_plots example app, simplifying the plot output to only include points and facets.

--- examples/static_plots/app.py
+++ examples/static_plots/app.py
@@ -78,7 +78,6 @@ def server(input: Inputs, output: Outputs, session: Session):
             facet_wrap,
             geom_point,
             ggplot,
-            stat_smooth,
             theme,
             theme_bw,
         )
@@ -89,7 +88,6 @@ def server(input: Inputs, output: Outputs, session: Session):
         return (
             ggplot(mtcars, aes(input.x(), input.y(), color=color_var))
             + geom_point()
-            + stat_smooth(method="lm")
             + facet_wrap("~gear")
             + theme_bw(base_size=16)
             + theme(legend_position="top")
