3322f29169fb0ae8796ea7e5da32326fd9519113
Karan <karangattu@users.noreply.github.com>
2025-07-24 02:18:14 +0530

    fix(InputSelectize): Fix set method to clear unwanted selections and maintain set order (#2024)
    Co-authored-by: Barret Schloerke <barret@posit.co>

---
+++ tests/playwright/shiny/bugs/2013-selectize-set-does-not-clear/test_app_selectize.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import Page
+
+from shiny.playwright import controller
+from shiny.pytest import create_app_fixture
+from shiny.run import ShinyAppProc
+
+app = create_app_fixture("app_selectize.py")
+
+
+def test_inputselectize(page: Page, app: ShinyAppProc):
+    page.goto(app.url)
+
+    input_selectize = controller.InputSelectize(page, "test_selectize")
+    output_text = controller.OutputText(page, "test_selectize_output")
+
+    input_selectize.set(["Choice 1", "Choice 2", "Choice 3"])
+    output_text.expect_value("Selected: Choice 1, Choice 2, Choice 3")
+    input_selectize.set(["Choice 2", "Choice 3"])
+    output_text.expect_value("Selected: Choice 2, Choice 3")
+    input_selectize.set(["Choice 2"])
+    output_text.expect_value("Selected: Choice 2")
+    input_selectize.set(["Choice 2", "Choice 3"])
+    output_text.expect_value("Selected: Choice 2, Choice 3")
+    input_selectize.set(["Choice 1", "Choice 2"])
+    output_text.expect_value("Selected: Choice 1, Choice 2")
+    input_selectize.set([])
+    output_text.expect_value("Selected: ")
+    input_selectize.set(["Choice 1", "Choice 3"])
+    output_text.expect_value("Selected: Choice 1, Choice 3")
