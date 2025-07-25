fd89a2c2e4602f80fef3840a9537e2ef67a2b212
Carson <cpsievert1@gmail.com>
2025-07-25 10:53:20 -0500

    Migrate and modernize panel_well() examples

--- shiny/api-examples/panel_absolute/app-core.py
+++ shiny/api-examples/panel_absolute/app-core.py
@@ -3,8 +3,9 @@ from shiny import App, Inputs, Outputs, Session, ui
 app_ui = ui.page_fluid(
     ui.panel_title("A basic absolute panel example", "Demo"),
     ui.panel_absolute(
-        ui.panel_well(
-            "Drag me around!", ui.input_slider("n", "N", min=0, max=100, value=20)
+        ui.card(
+            ui.card_header("Drag me around!"),
+            ui.input_slider("n", "N", min=0, max=100, value=20),
         ),
         draggable=True,
         width="300px",
