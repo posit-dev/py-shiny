84c8bf14394528898c70e2a5a5d107325030a453
Carson <cpsievert1@gmail.com>
2025-08-07 18:06:36 -0500

    Get docs building

--- shiny/ui/_chat.py
+++ shiny/ui/_chat.py
@@ -2,9 +2,26 @@ from shinychat import Chat, chat_ui
 from shinychat.express import Chat as ChatExpress
 from shinychat.types import ChatMessageDict
 
+from .._docstring import add_example
+
 __all__ = (
     "Chat",
     "ChatExpress",
     "chat_ui",
     "ChatMessageDict",
 )
+
+Chat = add_example(
+    app_file="app-core.py",
+    ex_dir="../shiny/api-examples/Chat",
+)(Chat)
+
+ChatExpress = add_example(
+    app_file="app-express.py",
+    ex_dir="../shiny/api-examples/Chat",
+)(ChatExpress)
+
+chat_ui = add_example(
+    app_file="app-core.py",
+    ex_dir="../shiny/api-examples/Chat",
+)(chat_ui)
--- shiny/ui/_markdown_stream.py
+++ shiny/ui/_markdown_stream.py
@@ -1,8 +1,25 @@
 from shinychat import MarkdownStream, output_markdown_stream
 from shinychat.express import MarkdownStream as ExpressMarkdownStream
 
+from .._docstring import add_example
+
 __all__ = (
     "output_markdown_stream",
     "MarkdownStream",
     "ExpressMarkdownStream",
 )
+
+MarkdownStream = add_example(
+    app_file="app-core.py",
+    ex_dir="../shiny/api-examples/MarkdownStream",
+)(MarkdownStream)
+
+ExpressMarkdownStream = add_example(
+    app_file="app-express.py",
+    ex_dir="../shiny/api-examples/MarkdownStream",
+)(ExpressMarkdownStream)
+
+output_markdown_stream = add_example(
+    app_file="app-core.py",
+    ex_dir="../shiny/api-examples/MarkdownStream",
+)(output_markdown_stream)
