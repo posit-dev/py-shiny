8fb5dd5cf70ad8899f4fa2ed749ba39d03a1b234
Liz Nelson <liz.nelson@posit.co>
2025-08-08 09:23:10 -0400

    placeholder test

--- tests/playwright/shiny/components/selectize/test_selectize.py
+++ tests/playwright/shiny/components/selectize/test_selectize.py
@@ -8,6 +8,8 @@ from shiny.run import ShinyAppProc
 def test_selectize(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
 
+    expect(page.get_by_placeholder("Search")).to_have_count(2)
+
     selectize_menu = controller.InputSelectize(page, "serverside-x")
     selectize_menu.expect_choices(["Foo 0", "Foo 1", "Foo 2"])
     selectize_menu.expect_multiple(True)
@@ -19,6 +21,3 @@ def test_selectize(page: Page, local_app: ShinyAppProc) -> None:
     selectize_menu2.expect_multiple(True)
     selectize_menu2.set(["Foo 0", "Foo 1"])
     selectize_menu2.expect_selected(["Foo 0", "Foo 1"])
-
-    expect(page.locator("css=input")).to_have_count(2)
-    expect(page.locator("css=input").first).to_have_attribute("placeholder", "Search")
