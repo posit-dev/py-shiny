be5387f25e2b4cafe77b8ac5ce52b06f0831b261
Carson <cpsievert1@gmail.com>
2025-08-21 10:45:42 -0500

    Pull in changes to shiny.js (via https://github.com/rstudio/shiny/pull/4276)

--- scripts/_pkg-sources.R
+++ scripts/_pkg-sources.R
@@ -1,6 +1,6 @@
 list(
   bslib = "rstudio/bslib@main",
-  shiny = "rstudio/shiny@main",
+  shiny = "rstudio/shiny@fix/update-selectize-default-plugins",
   sass = "sass",
   htmltools = "rstudio/htmltools@main"
 )
