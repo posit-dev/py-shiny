6488c37ee7e66c40ec3f37e9c18611d802a0216f
Liz Nelson <liz.nelson@posit.co>
2025-08-28 16:18:38 -0400

    Updating the js scripts

--- tests/playwright/shiny/bugs/2061-css-js-inclusion/app.py
+++ tests/playwright/shiny/bugs/2061-css-js-inclusion/app.py
@@ -3,12 +3,18 @@ from pathlib import Path
 from shiny import App, Inputs, Outputs, Session, ui
 
 js_file = Path(__file__).parent / "js" / "customjs.js"
+# We don't know what the temp dir will be called until we make it and that's what we need to reference
+# in the app (ex. we can't just reference where it is pre-copy)
+js2_file = (
+    ui.include_js(js_file, method="link_files").attrs["src"][:-11] + "customjs2.js"
+)
 css_file = Path(__file__).parent / "css" / "style.css"
 
 # Define the UI
 app_ui = ui.page_fluid(
     ui.include_css(css_file, method="link_files"),
     ui.include_js(js_file, method="link_files"),
+    ui.tags.script(src=str(js2_file)),
     ui.h1("Simple Shiny App with External CSS"),
     ui.div(
         ui.p("This is a simple Shiny app that demonstrates ui.include_css()"),
--- tests/playwright/shiny/bugs/2061-css-js-inclusion/test_css-js-inclusion.py
+++ tests/playwright/shiny/bugs/2061-css-js-inclusion/test_css-js-inclusion.py
@@ -6,4 +6,5 @@ from shiny.run import ShinyAppProc
 def test_inclusion(page: Page, local_app: ShinyAppProc) -> None:
     page.goto(local_app.url)
 
-    expect(page.locator("body > p")).to_have_text("Heyo!")
+    expect(page.locator("body > p").first).to_have_text("Heyo!")
+    expect(page.locator("body > p").last).to_have_text("Also here!")
