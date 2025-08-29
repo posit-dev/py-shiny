5418fe4f8c86718a80390e15747ed5ba0c9756cd
Liz Nelson <liz.nelson@posit.co>
2025-08-29 09:40:40 -0400

    updates

--- tests/playwright/shiny/bugs/2061-css-js-inclusion/app.py
+++ tests/playwright/shiny/bugs/2061-css-js-inclusion/app.py
@@ -2,19 +2,24 @@ from pathlib import Path
 
 from shiny import App, Inputs, Outputs, Session, ui
 
-js_file = Path(__file__).parent / "js" / "customjs.js"
-# We don't know what the temp dir will be called until we make it and that's what we need to reference
-# in the app (ex. we can't just reference where it is pre-copy)
-js2_file = (
-    ui.include_js(js_file, method="link_files").attrs["src"][:-11] + "customjs2.js"
+custom_css = ui.include_css(
+    Path(__file__).parent / "css" / "style.css",
+    method="link_files",
 )
-css_file = Path(__file__).parent / "css" / "style.css"
+
+custom_js = ui.include_js(
+    Path(__file__).parent / "js" / "customjs.js",
+    method="link_files",
+)
+
+# path where the JS file's parent directory is mounted
+href = custom_js.get_dependencies()[0].source_path_map()["href"]
 
 # Define the UI
 app_ui = ui.page_fluid(
-    ui.include_css(css_file, method="link_files"),
-    ui.include_js(js_file, method="link_files"),
-    ui.tags.script(src=str(js2_file)),
+    custom_css,
+    custom_js,
+    ui.tags.script(src=href + "/customjs2.js"),
     ui.h1("Simple Shiny App with External CSS"),
     ui.div(
         ui.p("This is a simple Shiny app that demonstrates ui.include_css()"),
