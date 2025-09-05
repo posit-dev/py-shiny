a5bd55c518877d96dbac249ce37d653a222b3a5d
Barret Schloerke <barret@posit.co>
2025-09-05 15:45:23 -0400

    Update comment

--- shiny/pytest/_generate/__init__.py
+++ shiny/pytest/_generate/__init__.py
@@ -1,5 +1,5 @@
 """
-This package is internal; public-facing imports should not rely on its location.
+This module is internal; public-facing imports should not rely on its location.
 """
 
 from ._main import ShinyTestGenerator
