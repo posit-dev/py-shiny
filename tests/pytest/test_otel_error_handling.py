74dc6c9aaaede6561c14906663c1795c988ae098
Barret Schloerke <barret@posit.co>
2026-02-16 14:51:59 -0500

    Update test_otel_error_handling.py

--- tests/pytest/test_otel_error_handling.py
+++ tests/pytest/test_otel_error_handling.py
@@ -13,7 +13,6 @@ Tests cover:
 
 import pytest
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 from opentelemetry.trace import StatusCode
 
